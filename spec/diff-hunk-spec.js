b767fb16f427af1af19148fb0475ef220696e036
Ben Ogle <ogle.ben@gmail.com>
2016-02-02 15:33:14 -0800

    Specs for header and line events

--- spec/diff-hunk-spec.js
+++ spec/diff-hunk-spec.js
@@ -3,14 +3,16 @@
 import DiffHunk from '../lib/diff-hunk'
 
 describe("DiffHunk", function() {
+  let diffHunk
+  beforeEach(function() {
+    diffHunk = DiffHunk.fromString(HunkStr)
+  })
+
   it("roundtrips toString and fromString", function() {
-    let hunk = HunkStr
-    let diffHunk = DiffHunk.fromString(hunk)
-    expect(diffHunk.toString()).toEqual(hunk)
+    expect(diffHunk.toString()).toEqual(HunkStr)
   })
 
   it("stages all lines with ::stage() and unstages all lines with ::unstage()", function() {
-    let diffHunk = DiffHunk.fromString(HunkStr)
     expect(diffHunk.getStageStatus()).toBe('unstaged')
 
     diffHunk.stage()
@@ -27,7 +29,6 @@ describe("DiffHunk", function() {
   })
 
   it("returns 'partial' from getStageStatus() when some of the lines are staged", function() {
-    let diffHunk = DiffHunk.fromString(HunkStr)
     expect(diffHunk.getStageStatus()).toBe('unstaged')
 
     diffHunk.getLines()[3].stage()
@@ -39,7 +40,6 @@ describe("DiffHunk", function() {
 
   it("emits one change event when the hunk is staged", function() {
     let changeHandler = jasmine.createSpy()
-    let diffHunk = DiffHunk.fromString(HunkStr)
     diffHunk.onDidChange(changeHandler)
 
     diffHunk.stage()
@@ -48,10 +48,21 @@ describe("DiffHunk", function() {
 
   it("emits a change event when a line is staged", function() {
     let changeHandler = jasmine.createSpy()
-    let diffHunk = DiffHunk.fromString(HunkStr)
     diffHunk.onDidChange(changeHandler)
 
     diffHunk.getLines()[3].stage()
+    expect(changeHandler).toHaveBeenCalled()
+  })
+
+  it("emits events when the header and lines change", function() {
+    let changeHandler = jasmine.createSpy()
+    diffHunk.onDidChange(changeHandler)
+
+    diffHunk.setHeader('@@ -85,9 +85,6 @@ someline ok yeah')
+    expect(changeHandler).toHaveBeenCalled()
+
+    changeHandler.reset()
+    diffHunk.setLines([])
     expect(changeHandler).toHaveBeenCalled()
   })
 })
