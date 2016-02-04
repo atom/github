ce043a92d96803d9c770683ab43260dca638e5a2
Ben Ogle <ogle.ben@gmail.com>
2016-02-03 17:06:28 -0800

    Add fileCache to FileList

--- spec/file-list-spec.js
+++ spec/file-list-spec.js
@@ -1,6 +1,7 @@
 /** @babel */
 
 import FileList from '../lib/file-list'
+import FileDiff from '../lib/file-diff'
 import {createFileDiffsFromPath} from './helpers'
 
 function createFileList(filePath) {
@@ -9,13 +10,45 @@ function createFileList(filePath) {
 }
 
 describe("FileList", function() {
+  let fileList
   it("emits a change event when a file is staged", function() {
-    let fileList = createFileList('fixtures/two-file-diff.txt')
+    fileList = createFileList('fixtures/two-file-diff.txt')
     let changeHandler = jasmine.createSpy()
     fileList.onDidChange(changeHandler)
 
     let fileDiff = fileList.getFiles()[0]
     fileDiff.stage()
     expect(changeHandler.callCount).toBe(1)
+  })
+
+  describe("the file cache", function() {
+    let fileDiffA, fileDiffB
+    beforeEach(function() {
+      fileDiffA = new FileDiff({
+        oldPathName: 'src/a.js',
+        newPathName: 'src/a.js'
+      })
+      fileDiffB = new FileDiff({
+        oldPathName: 'src/b.js',
+        newPathName: 'src/b.js'
+      })
+      fileList = new FileList([fileDiffA, fileDiffB])
+    })
+
+    it("", function() {
+      expect(fileList.getFileFromPathName(fileDiffA.getNewPathName())).toBe(fileDiffA)
+      expect(fileList.getFileFromPathName(fileDiffB.getNewPathName())).toBe(fileDiffB)
+
+      let fileDiffC = fileList.getOrCreateFileFromPathName('src/c.js')
+      expect(fileList.getFileFromPathName(fileDiffC.getNewPathName())).toBe(fileDiffC)
+    })
+
+    it("moves model to the new file key when a file is renamed", function() {
+      fileDiffA.setNewPathName('src/a-new.js')
+      fileList.setFiles([fileDiffA, fileDiffB])
+      expect(fileList.getFileFromPathName('src/a-new.js')).toBe(fileDiffA)
+      expect(fileList.getFileFromPathName('src/a.js')).toBeUndefined()
+      expect(fileList.getFileFromPathName(fileDiffB.getNewPathName())).toBe(fileDiffB)
+    })
   })
 })
