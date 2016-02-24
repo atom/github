640c7275bee026070150a7846d139706ce0f7419
joshaber <joshaber@gmail.com>
2016-02-24 10:32:28 -0500

    Test that committing commits.

--- spec/commit-box-view-model-spec.js
+++ spec/commit-box-view-model-spec.js
@@ -1,25 +1,58 @@
 /** @babel */
 
+import path from 'path'
+import fs from 'fs-plus'
 import GitService from '../lib/git-service'
 import CommitBoxViewModel from '../lib/commit-box-view-model'
 import {copyRepository} from './helpers'
-import {waitsForPromise} from './async-spec-helpers'
+import {waitsForPromise, it} from './async-spec-helpers'
 
 describe('CommitBoxViewModel', () => {
   let viewModel
+  let gitService
+  let repoPath
 
   beforeEach(() => {
-    const repoPath = copyRepository()
+    repoPath = copyRepository()
+
+    gitService = GitService.instance()
 
     atom.project.setPaths([repoPath])
+    gitService.repoPath = repoPath
 
-    viewModel = new CommitBoxViewModel(GitService.instance())
+    viewModel = new CommitBoxViewModel(gitService)
     waitsForPromise(() => viewModel.update())
   })
 
-  describe('getBranchName', () => {
-    it('is the current branch', () => {
+  describe('::getBranchName', () => {
+    it('returns the current branch', () => {
       expect(viewModel.getBranchName()).toBe('master')
+    })
+  })
+
+  describe('::commit', () => {
+    it('emits a user change event', async () => {
+      let changeHandler = jasmine.createSpy()
+      viewModel.onDidUserChange(changeHandler)
+
+      await viewModel.commit('hey there')
+
+      expect(changeHandler).toHaveBeenCalled()
+    })
+
+    it('commits the staged changes', async () => {
+      const newFileName = 'new-file.txt'
+      const newFile = path.join(repoPath, newFileName)
+      fs.writeFileSync(newFile, 'my fav file')
+
+      let statuses = await gitService.getStatuses()
+      expect(statuses[newFileName]).not.toBeUndefined()
+
+      await gitService.stagePath(newFileName)
+      await viewModel.commit('hey there')
+
+      statuses = await gitService.getStatuses()
+      expect(statuses[newFileName]).toBeUndefined()
     })
   })
 })
