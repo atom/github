cd197876d19af702c9f92ca5f08cc48bdcb3fa9c
joshaber <joshaber@gmail.com>
2016-03-01 15:15:46 -0500

    Spec for StatusBarViewModel.

---
+++ spec/status-bar-view-model-spec.js
@@ -0,0 +1,35 @@
+/** @babel */
+
+import path from 'path'
+import fs from 'fs-plus'
+import GitService from '../lib/git-service'
+import StatusBarViewModel from '../lib/status-bar-view-model'
+import {copyRepository} from './helpers'
+import {waitsForPromise, it} from './async-spec-helpers'
+
+describe('StatusBarViewModel', () => {
+  let viewModel
+  let gitService
+  let repoPath
+
+  beforeEach(() => {
+    repoPath = copyRepository()
+
+    gitService = GitService.instance()
+
+    atom.project.setPaths([repoPath])
+    gitService.repoPath = repoPath
+
+    fs.writeFileSync(path.join(repoPath, 'file1.txt'), '')
+    fs.writeFileSync(path.join(repoPath, 'file2.txt'), '')
+
+    viewModel = new StatusBarViewModel(gitService)
+    waitsForPromise(() => viewModel.update())
+  })
+
+  describe('::getChangedFileCount', () => {
+    it('returns the number of changed files', () => {
+      expect(viewModel.getChangedFileCount()).toBe(2)
+    })
+  })
+})
