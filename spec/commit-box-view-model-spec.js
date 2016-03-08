f9eeaae23a845bd4027c3089a21f98bf672cb3cb
joshaber <joshaber@gmail.com>
2016-03-08 17:17:18 -0500

    Test it.

--- spec/commit-box-view-model-spec.js
+++ spec/commit-box-view-model-spec.js
@@ -2,8 +2,10 @@
 
 import path from 'path'
 import fs from 'fs-plus'
+import {Point} from 'atom'
 import GitService from '../lib/git-service'
 import CommitBoxViewModel from '../lib/commit-box-view-model'
+import {SummaryPreferredLength} from '../lib/commit-box-view-model'
 import {copyRepository} from './helpers'
 import {waitsForPromise, it} from './async-spec-helpers'
 
@@ -50,6 +52,27 @@ describe('CommitBoxViewModel', () => {
 
       statuses = await gitService.getStatuses()
       expect(statuses[newFileName]).toBeUndefined()
+    })
+  })
+
+  describe('.calculateRemainingCharacters', () => {
+    const summary = 'hey kids!'
+    const description = 'Version control is good and good for you!'
+    const msg = `${summary}\n\n${description}`
+
+    describe('when the summary is being edited', () => {
+      it('counts down from 50', () => {
+        const remaining = viewModel.calculateRemainingCharacters(msg, new Point(0, 0))
+        expect(remaining).toBe(SummaryPreferredLength - summary.length)
+      })
+    })
+
+    describe('when the description is being edited', () => {
+      it('is infinite', () => {
+        const msg = 'hey kids!\n\nVersion control is good and good for you!'
+        const remaining = viewModel.calculateRemainingCharacters(msg, new Point(3, 0))
+        expect(isFinite(remaining)).toBe(false)
+      })
     })
   })
 })
