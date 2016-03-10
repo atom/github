560475476215d5d54df63e2a6f808a7611c30788
joshaber <joshaber@gmail.com>
2016-03-10 14:15:04 -0500

    Pull the git package class out into its own file.

---
+++ spec/git-package-spec.js
@@ -0,0 +1,66 @@
+/** @babel */
+
+import path from 'path'
+import fs from 'fs-plus'
+import DiffViewModel from '../lib/diff-view-model'
+import GitPackage from '../lib/git-package'
+import {copyRepository} from './helpers'
+import {it, beforeEach, afterEach} from './async-spec-helpers'
+
+function commit (fileListViewModel, msg) {
+  const fileList = fileListViewModel.fileList
+  const fileDiff = fileList.getFiles()[0]
+  console.log(fileList)
+  console.log(fileList.getFiles())
+  const changeHandler = jasmine.createSpy()
+  const commitHandler = jasmine.createSpy()
+  runs(() => {
+    fileList.onDidUserChange(changeHandler)
+    fileDiff.stage()
+  })
+  waitsFor('staging', () => changeHandler.callCount === 1)
+  runs(() => {
+    const commitViewModel = fileListViewModel.commitBoxViewModel
+    commitViewModel.onDidCommit(commitHandler)
+    commitViewModel.commit(msg)
+  })
+  waitsFor('committing', () => commitHandler.callCount === 1)
+}
+
+describe('GitPackage', function () {
+  let gitPackage
+  let repo
+
+  beforeEach(async () => {
+    jasmine.useRealClock()
+
+    repo = copyRepository()
+    atom.project.setPaths([repo])
+
+    gitPackage = new GitPackage()
+
+    await atom.packages.activatePackage('git')
+  })
+
+  afterEach(() => {
+    gitPackage.deactivate()
+  })
+
+  it('closes open diffs of files that were committed', async () => {
+    fs.writeFileSync(path.join(repo, 'README.md'), 'hey diddle diddle')
+    console.log(repo)
+
+    await gitPackage.update()
+    await atom.workspace.open('README.md')
+    await gitPackage.openDiffForActiveEditor()
+
+    const paneItem = atom.workspace.getActivePaneItem()
+    expect(paneItem instanceof DiffViewModel).toBe(true)
+
+    commit(gitPackage.getFileListViewModel(), 'hey')
+    waitsFor('the pane item to close', () => {
+      const paneItem = atom.workspace.getActivePaneItem()
+      return !(paneItem instanceof DiffViewModel)
+    })
+  })
+})
