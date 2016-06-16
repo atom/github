dd3456a8b650a84297684a6dd590e53de801d3ac
Nathan Sobo <nathan@github.com>
2016-06-16 16:33:34 -0600

    Get Mocha-based package test runner working

---
+++ test/staging-area.test.js
@@ -0,0 +1,7 @@
+describe('StagingArea', () => {
+  describe('getChangedFiles', () => {
+    it('returns an array of ChangedFile objects whose contents in the working copy differ from the repository HEAD', () => {
+      assert(true)
+    })
+  })
+})
