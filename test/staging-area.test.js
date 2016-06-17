c3f80f42121f28729e19e32675d12fcf98567826
Antonio Scandurra <as-cii@github.com>
2016-06-17 16:28:30 +0200

    Include hunks in ChangedFile objects

--- test/staging-area.test.js
+++ test/staging-area.test.js
@@ -9,7 +9,7 @@ describe('StagingArea', () => {
   describe('getChangedFiles', () => {
     it('returns an array of ChangedFile objects whose contents in the working copy differ from the repository HEAD', async () => {
       const workingDirPath = copyRepositoryDir(1)
-      fs.writeFileSync(path.join(workingDirPath, 'a.txt'), 'foo\nbar', 'utf8')
+      fs.writeFileSync(path.join(workingDirPath, 'a.txt'), 'qux\nfoo\nbar\n', 'utf8')
       fs.unlinkSync(path.join(workingDirPath, 'b.txt'))
       fs.renameSync(path.join(workingDirPath, 'c.txt'), path.join(workingDirPath, 'd.txt'))
       fs.writeFileSync(path.join(workingDirPath, 'e.txt'), 'qux', 'utf8')
@@ -17,21 +17,98 @@ describe('StagingArea', () => {
       const repo = await buildRepository(workingDirPath)
       const stage = repo.getStagingArea()
       assert.equal(stage.getChangedFiles().length, 0)
+
       await stage.refresh()
       assert.deepEqual(stage.getChangedFiles(), [
-        {oldName: 'a.txt', newName: 'a.txt', status: 'modified'},
-        {oldName: 'b.txt', newName: 'b.txt', status: 'deleted'},
-        {oldName: 'c.txt', newName: 'd.txt', status: 'renamed'},
-        {oldName: 'e.txt', newName: 'e.txt', status: 'created'}
+        {
+          oldName: 'a.txt',
+          newName: 'a.txt',
+          status: 'modified',
+          hunks: [
+            {
+              lines: [
+                {status: 'added', text: 'qux', oldLineNumber: -1, newLineNumber: 1},
+                {status: 'unchanged', text: 'foo', oldLineNumber: 1, newLineNumber: 2},
+                {status: 'added', text: 'bar', oldLineNumber: -1, newLineNumber: 3}
+              ]
+            }
+          ]
+        },
+        {
+          oldName: 'b.txt',
+          newName: 'b.txt',
+          status: 'deleted',
+          hunks: [
+            {
+              lines: [
+                {status: 'deleted', text: 'bar', oldLineNumber: 1, newLineNumber: -1},
+              ]
+            }
+          ]
+        },
+        {
+          oldName: 'c.txt',
+          newName: 'd.txt',
+          status: 'renamed',
+          hunks: []
+        },
+        {
+          oldName: 'e.txt',
+          newName: 'e.txt',
+          status: 'created',
+          hunks: [
+            {
+              lines: [
+                {status: 'added', text: 'qux', oldLineNumber: -1, newLineNumber: 1},
+                {status: 'unchanged', text: '\n\\ No newline at end of file', oldLineNumber: -1, newLineNumber: 1}
+              ]
+            }
+          ]
+        }
       ])
 
       fs.unlinkSync(path.join(workingDirPath, 'e.txt'))
       fs.unlinkSync(path.join(workingDirPath, 'd.txt'))
       await stage.refresh()
       assert.deepEqual(stage.getChangedFiles(), [
-        {oldName: 'a.txt', newName: 'a.txt', status: 'modified'},
-        {oldName: 'b.txt', newName: 'b.txt', status: 'deleted'},
-        {oldName: 'c.txt', newName: 'c.txt', status: 'deleted'}
+        {
+          oldName: 'a.txt',
+          newName: 'a.txt',
+          status: 'modified',
+          hunks: [
+            {
+              lines: [
+                {status: 'added', text: 'qux', oldLineNumber: -1, newLineNumber: 1},
+                {status: 'unchanged', text: 'foo', oldLineNumber: 1, newLineNumber: 2},
+                {status: 'added', text: 'bar', oldLineNumber: -1, newLineNumber: 3}
+              ]
+            }
+          ]
+        },
+        {
+          oldName: 'b.txt',
+          newName: 'b.txt',
+          status: 'deleted',
+          hunks: [
+            {
+              lines: [
+                {status: 'deleted', text: 'bar', oldLineNumber: 1, newLineNumber: -1},
+              ]
+            }
+          ]
+        },
+        {
+          oldName: 'c.txt',
+          newName: 'c.txt',
+          status: 'deleted',
+          hunks: [
+            {
+              lines: [
+                {status: 'deleted', text: 'baz', oldLineNumber: 1, newLineNumber: -1},
+              ]
+            }
+          ]
+        }
       ])
     })
   })
