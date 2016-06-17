beed7afc6bb0913e069b60b05ef420245b8f0c59
Antonio Scandurra <as-cii@github.com>
2016-06-17 12:57:34 +0200

    Implement a basic version of ChangedFile
    …eventually this object will contain state for the "internally staged"
    hunks and we will be able to call methods like `stage()` and
    `stageHunk()` on it.

--- test/staging-area.test.js
+++ test/staging-area.test.js
@@ -11,18 +11,28 @@ describe('StagingArea', () => {
       const workingDirPath = copyRepositoryDir(1)
       fs.writeFileSync(path.join(workingDirPath, 'a.txt'), 'foo\nbar', 'utf8')
       fs.unlinkSync(path.join(workingDirPath, 'b.txt'))
-      fs.writeFileSync(path.join(workingDirPath, 'c.txt'), 'baz', 'utf8')
+      fs.renameSync(path.join(workingDirPath, 'c.txt'), path.join(workingDirPath, 'd.txt'))
+      fs.writeFileSync(path.join(workingDirPath, 'e.txt'), 'qux', 'utf8')
 
-      let repo = await buildRepository(workingDirPath)
-      let stage = repo.getStagingArea()
-
+      const repo = await buildRepository(workingDirPath)
+      const stage = repo.getStagingArea()
+      assert.equal(stage.getChangedFiles().length, 0)
       await stage.refresh()
+      assert.deepEqual(stage.getChangedFiles(), [
+        {oldName: 'a.txt', newName: 'a.txt', status: 'modified'},
+        {oldName: 'b.txt', newName: 'b.txt', status: 'deleted'},
+        {oldName: 'c.txt', newName: 'd.txt', status: 'renamed'},
+        {oldName: 'e.txt', newName: 'e.txt', status: 'created'}
+      ])
 
-      // Return an array of ChangedFile objects representing a file with a diff
-      // against the HEAD tree. These objects will back various parts of our
-      // UI and maintain state about what is staged within Atom.
-
-      // stage.getChangedFiles()
+      fs.unlinkSync(path.join(workingDirPath, 'e.txt'))
+      fs.unlinkSync(path.join(workingDirPath, 'd.txt'))
+      await stage.refresh()
+      assert.deepEqual(stage.getChangedFiles(), [
+        {oldName: 'a.txt', newName: 'a.txt', status: 'modified'},
+        {oldName: 'b.txt', newName: 'b.txt', status: 'deleted'},
+        {oldName: 'c.txt', newName: 'c.txt', status: 'deleted'}
+      ])
     })
   })
 })
