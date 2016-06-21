4d19be0185a71fbd615d4ac9f83afd92c6389fd2
Antonio Scandurra <as-cii@github.com>
2016-06-21 17:49:01 +0200

    Add FileDiff.toString()

---
+++ test/file-diff.test.js
@@ -0,0 +1,42 @@
+/** @babel */
+
+import {copyRepositoryDir, buildRepository} from './helpers'
+import path from 'path'
+import fs from 'fs'
+import dedent from 'dedent-js'
+
+describe('FileDiff', () => {
+  describe('toString()', () =>{
+    it('converts the diff to the standard textual format', async () => {
+      const workdirPath = copyRepositoryDir(2)
+      const repository = await buildRepository(workdirPath)
+
+      const lines = fs.readFileSync(path.join(workdirPath, 'sample.js'), 'utf8').split('\n')
+      lines[0] = 'this is a modified line'
+      lines.splice(1, 0, 'this is a new line')
+      lines[11] = 'this is a modified line'
+      lines.splice(12, 1)
+      fs.writeFileSync(path.join(workdirPath, 'sample.js'), lines.join('\n'))
+
+      const [diff] = await repository.getUnstagedChanges()
+
+      assert.equal(diff.toString(), dedent`
+        @@ -1,4 +1,5 @@
+        -var quicksort = function () {
+        +this is a modified line
+        +this is a new line
+           var sort = function(items) {
+             if (items.length <= 1) return items;
+             var pivot = items.shift(), current, left = [], right = [];
+        @@ -8,6 +9,5 @@ var quicksort = function () {
+             }
+             return sort(left).concat(pivot).concat(sort(right));
+           };
+        -
+        -  return sort(Array.apply(this, arguments));
+        +this is a modified line
+         };
+      `)
+    })
+  })
+})
