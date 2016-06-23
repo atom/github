e165815aa77a5ce4d41ecd65704dd8cb79131e2b
Katrina Uychaco <kuychaco@github.com>
2016-06-22 22:53:07 -0700

    Call didSelectFileDiff when fileDiff is selected
    This is passed from GithubPackage and will be used to render the fileDiff 
    contents in a pane item

--- test/staging-component.test.js
+++ test/staging-component.test.js
@@ -3,6 +3,7 @@
 import {copyRepositoryDir, buildRepository} from './helpers'
 import path from 'path'
 import fs from 'fs'
+import sinon from 'sinon'
 
 import StagingComponent from '../lib/staging-component'
 
@@ -67,5 +68,28 @@ describe('StagingComponent', () => {
     selectedLists = component.element.querySelectorAll('.git-Panel-item.is-focused .is-header')
     assert.equal(selectedLists.length, 1)
     assert.equal(selectedLists[0].textContent, 'Staged Changes')
+  })
+
+  it('calls didSelectFileDiff when file is selected', async () => {
+    const didSelectFileDiff = sinon.spy()
+
+    const workdirPath = await copyRepositoryDir(1)
+    const repository = await buildRepository(workdirPath)
+    fs.writeFileSync(path.join(workdirPath, 'a.txt'), 'a change\n')
+    const component = new StagingComponent({repository, didSelectFileDiff})
+    await component.lastModelDataRefreshPromise
+
+    const {stagedChangesComponent, unstagedChangesComponent} = component.refs
+    const fileDiff = unstagedChangesComponent.fileDiffs[0]
+
+    assert(fileDiff)
+
+    unstagedChangesComponent.didSelectFileDiff(fileDiff)
+    assert.equal(didSelectFileDiff.callCount, 1)
+    assert.deepEqual(didSelectFileDiff.args[0], [fileDiff, 'unstaged'])
+
+    stagedChangesComponent.didSelectFileDiff(fileDiff)
+    assert.equal(didSelectFileDiff.callCount, 2)
+    assert.deepEqual(didSelectFileDiff.args[1], [fileDiff, 'staged'])
   })
 })
