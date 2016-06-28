8992953306a56d68a11331375a0f567c1e1bca02
Katrina Uychaco <kuychaco@github.com>
2016-06-27 21:43:09 -0700

    Pass selectedFilePatchIndex rather than FilePatch object to FilePatchListComponent

--- test/views/file-patch-list-component.test.js
+++ test/views/file-patch-list-component.test.js
@@ -15,49 +15,13 @@ describe('FilePatchListComponent', () => {
     // create a selectedFilePatch object that looks the same as filePatches[1] but not deep-equal
     //  fetched model data contains newly constructed file patch objects each time
     //  so the selectedFilePatch object may be different if model is updated
-    const selectedFilePatch = new FilePatch(null, 'b.txt', 1234, 1234, 'added')
 
-    const component = new FilePatchListComponent({filePatches, selectedFilePatch})
+    const component = new FilePatchListComponent({filePatches, selectedFilePatchIndex: 1})
     assert.equal(component.element.querySelector('.git-FilePatchListItem.modified .git-FilePatchListItem-path').textContent, 'a.txt')
     assert.equal(component.element.querySelector('.git-FilePatchListItem.added .git-FilePatchListItem-path').textContent, 'b.txt')
     assert.equal(component.element.querySelector('.git-FilePatchListItem.removed .git-FilePatchListItem-path').textContent, 'c.txt')
     assert.equal(component.element.querySelector('.git-FilePatchListItem.renamed .git-FilePatchListItem-path').textContent, 'd.txt → e.txt')
     assert.equal(component.element.querySelector('.is-selected .git-FilePatchListItem-path').textContent, 'b.txt')
-  })
-
-  describe('selectFilePatch(filePatch)', () => {
-    it('allows a file patch to be selected', async () => {
-      const filePatches = [
-        new FilePatch('a.txt', 'a.txt', 1234, 1234, 'modified'),
-        new FilePatch(null, 'b.txt', 1234, 1234, 'added'),
-        new FilePatch('c.txt', null, 1234, 1234, 'removed'),
-        new FilePatch('d.txt', 'e.txt', 1234, 1234, 'renamed')
-      ]
-
-      const component = new FilePatchListComponent({filePatches})
-      assert.equal(component.element.querySelector('.git-FilePatchListItem.modified .git-FilePatchListItem-path').textContent, 'a.txt')
-      assert.equal(component.element.querySelector('.git-FilePatchListItem.added .git-FilePatchListItem-path').textContent, 'b.txt')
-      assert.equal(component.element.querySelector('.git-FilePatchListItem.removed .git-FilePatchListItem-path').textContent, 'c.txt')
-      assert.equal(component.element.querySelector('.git-FilePatchListItem.renamed .git-FilePatchListItem-path').textContent, 'd.txt → e.txt')
-
-      await component.update({filePatches})
-      assert.equal(component.element.querySelector('.git-FilePatchListItem.modified .git-FilePatchListItem-path').textContent, 'a.txt')
-      assert.equal(component.element.querySelector('.git-FilePatchListItem.added .git-FilePatchListItem-path').textContent, 'b.txt')
-      assert.equal(component.element.querySelector('.git-FilePatchListItem.removed .git-FilePatchListItem-path').textContent, 'c.txt')
-      assert.equal(component.element.querySelector('.git-FilePatchListItem.renamed .git-FilePatchListItem-path').textContent, 'd.txt → e.txt')
-
-      await component.selectFilePatch(filePatches[1])
-      let selectedPatches = component.element.querySelectorAll('.git-FilePatchListItem.is-selected .git-FilePatchListItem-path')
-      assert.equal(selectedPatches.length, 1)
-      assert.deepEqual(component.selectedFilePatch, filePatches[1])
-      assert.equal(selectedPatches[0].textContent, 'b.txt')
-
-      await component.selectFilePatch(filePatches[3])
-      selectedPatches = component.element.querySelectorAll('.git-FilePatchListItem.is-selected .git-FilePatchListItem-path')
-      assert.equal(selectedPatches.length, 1)
-      assert.deepEqual(component.selectedFilePatch, filePatches[3])
-      assert.equal(selectedPatches[0].textContent, 'd.txt → e.txt')
-    })
   })
 
   describe('when a file patch is selected via single clicked', () => {
@@ -79,24 +43,6 @@ describe('FilePatchListComponent', () => {
 
       component.element.querySelector('.git-FilePatchListItem.renamed').dispatchEvent(new MouseEvent('click', {detail: 1}))
       assert.deepEqual(selectedPatches, [filePatches[0], filePatches[3]])
-    })
-
-    it('selects the file patch', () => {
-      const filePatches = [
-        new FilePatch('a.txt', 'a.txt', 1234, 1234, 'modified'),
-        new FilePatch(null, 'b.txt', 1234, 1234, 'added'),
-        new FilePatch('c.txt', null, 1234, 1234, 'removed'),
-        new FilePatch('d.txt', 'e.txt', 1234, 1234, 'renamed')
-      ]
-
-      const component = new FilePatchListComponent({filePatches})
-      assert.deepEqual(component.selectedFilePatch, filePatches[0])
-
-      component.element.querySelector('.git-FilePatchListItem.added').dispatchEvent(new MouseEvent('click', {detail: 1}))
-      assert.deepEqual(component.selectedFilePatch, filePatches[1])
-
-      component.element.querySelector('.git-FilePatchListItem.renamed').dispatchEvent(new MouseEvent('click', {detail: 1}))
-      assert.deepEqual(component.selectedFilePatch, filePatches[3])
     })
   })
 
