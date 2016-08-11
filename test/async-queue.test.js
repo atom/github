e6be1d42f092140c2a4399a387f646de8b104458
Michelle Tilley <binarymuse@github.com>
2016-08-10 17:14:04 -0700

    Add AsyncQueue for serializing async operations
    Signed-off-by: Katrina Uychaco <kuychaco@github.com>

---
+++ test/async-queue.test.js
@@ -0,0 +1,49 @@
+/** @babel */
+
+import AsyncQueue from '../lib/async-queue'
+
+const timer = (n) => {
+  return new Promise(resolve => {
+    setTimeout(resolve, 10 - n)
+  })
+}
+
+describe('AsyncQueue', () => {
+  it('runs items in the order queued', async () => {
+    const queue = new AsyncQueue()
+
+    let expectedEvents = []
+    let actualEvents = []
+    let promises = []
+    for (let i = 0; i < 10; i++) {
+      expectedEvents.push(i)
+      const promise = queue.push(() => timer(i)).then(() => actualEvents.push(i))
+      promises.push(promise)
+    }
+
+    await Promise.all(promises)
+    assert.deepEqual(expectedEvents, actualEvents)
+  })
+
+  it('continues running queued items when one fails', async () => {
+    const queue = new AsyncQueue()
+
+    let expectedEvents = []
+    let actualEvents = []
+    let promises = []
+    for (let i = 0; i < 10; i++) {
+      expectedEvents.push(i === 5 ? 'error' : i)
+      const promise = queue.push(() => {
+        if (i === 5) {
+          throw new Error('omg')
+        } else {
+          return timer(i)
+        }
+      }).then(() => actualEvents.push(i), () => actualEvents.push('error'))
+      promises.push(promise)
+    }
+
+    await Promise.all(promises)
+    assert.deepEqual(expectedEvents, actualEvents)
+  })
+})
