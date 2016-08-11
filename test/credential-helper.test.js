0f18c339210efb49f232615c1b97c464e3bff673
Michelle Tilley <binarymuse@github.com>
2016-08-10 17:12:39 -0700

    Add CredentialHelper to facilitate prompting for Git credentials
    Signed-off-by: Katrina Uychaco <kuychaco@github.com>

---
+++ test/credential-helper.test.js
@@ -0,0 +1,27 @@
+/** @babel */
+
+import {execFile} from 'child_process'
+
+import CredentialHelper from '../lib/credential-helper'
+
+describe('CredentialHelper', () => {
+  it('prompts for user input and writes the response to stdout', async () => {
+    const helper = new CredentialHelper()
+    const launchPath = await helper.start((question) => {
+      assert.equal(question, 'What... is your favorite color?\u0000')
+      return 'Green. I mean blue! AAAhhhh...'
+    })
+
+    await new Promise(resolve => {
+      const cp = execFile(launchPath, ['What... is your favorite color?'], (err, stdout, stderr) => {
+        assert.isNull(err)
+        assert.equal(stdout, 'Green. I mean blue! AAAhhhh...\n')
+        resolve()
+      })
+
+      cp.on('exit', () => console.log('exit'))
+    })
+
+    await helper.terminate()
+  })
+})
