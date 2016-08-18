47d340f25a5d5c2ffdac5dc68502a434b639175c
Katrina Uychaco <kuychaco@github.com>
2016-08-18 12:13:22 -0700

    :shirt: Fix linting errors in HunkView tests

--- test/views/hunk-view.test.js
+++ test/views/hunk-view.test.js
@@ -1,7 +1,6 @@
 /** @babel */
 
 import sinon from 'sinon'
-import etch from 'etch'
 
 import Hunk from '../../lib/models/hunk'
 import HunkLine from '../../lib/models/hunk-line'
@@ -15,9 +14,9 @@ describe('HunkView', () => {
       new HunkLine('line-3', 'removed', 7, -1),
       new HunkLine('line-4', 'added', -1, 6)
     ])
-    const view = new HunkView({hunk: hunk1, selectedLines: new Set})
+    const view = new HunkView({hunk: hunk1, selectedLines: new Set()})
     const element = view.element
-    var [line1, line2, line3, line4] = Array.from(element.querySelectorAll('.git-HunkView-line'))
+    let [line1, line2, line3, line4] = Array.from(element.querySelectorAll('.git-HunkView-line'))
 
     assert.equal(view.refs.header.textContent, hunk1.getHeader())
     assertHunkLineElementEqual(
@@ -41,9 +40,11 @@ describe('HunkView', () => {
       new HunkLine('line-1', 'removed', 8, -1),
       new HunkLine('line-2', 'added', -1, 8)
     ])
-    var [line1, line2] = Array.from(element.querySelectorAll('.git-HunkView-line'))
+    const lines = Array.from(element.querySelectorAll('.git-HunkView-line'))
+    line1 = lines[0]
+    line2 = lines[1]
 
-    await view.update({hunk: hunk2, selectedLines: new Set})
+    await view.update({hunk: hunk2, selectedLines: new Set()})
 
     assert.equal(view.refs.header.textContent, hunk2.getHeader())
     assertHunkLineElementEqual(
@@ -68,10 +69,10 @@ describe('HunkView', () => {
 
   it('adds the is-selected class based on the isSelected property', async () => {
     const hunk = new Hunk(5, 5, 2, 1, [])
-    const view = new HunkView({hunk, selectedLines: new Set, isSelected: true})
+    const view = new HunkView({hunk, selectedLines: new Set(), isSelected: true})
     assert(view.element.classList.contains('is-selected'))
 
-    await view.update({hunk: hunk, selectedLines: new Set, isSelected: false})
+    await view.update({hunk: hunk, selectedLines: new Set(), isSelected: false})
     assert(!view.element.classList.contains('is-selected'))
   })
 
@@ -93,17 +94,17 @@ describe('HunkView', () => {
   it('calls the didClickStageButton handler when the staging button is clicked', async () => {
     const hunk = new Hunk(5, 5, 2, 1, [new HunkLine('line-1', 'unchanged', 5, 5)])
     const didClickStageButton1 = sinon.spy()
-    const view = new HunkView({hunk, selectedLines: new Set, didClickStageButton: didClickStageButton1})
+    const view = new HunkView({hunk, selectedLines: new Set(), didClickStageButton: didClickStageButton1})
     view.refs.stageButton.dispatchEvent(new MouseEvent('click'))
     assert(didClickStageButton1.calledOnce)
 
     const didClickStageButton2 = sinon.spy()
-    await view.update({didClickStageButton: didClickStageButton2, hunk, selectedLines: new Set})
+    await view.update({didClickStageButton: didClickStageButton2, hunk, selectedLines: new Set()})
     view.refs.stageButton.dispatchEvent(new MouseEvent('click'))
     assert(didClickStageButton2.calledOnce)
   })
 
-  it('calls the selectLines handler when lines are selected with the mouse', async () => {
+  it('calls the selectLine handler when lines are selected with the mouse', async () => {
     const hunk = new Hunk(1234, 1234, 1234, 1234, [
       new HunkLine('line-1', 'added', 1234, 1234),
       new HunkLine('line-2', 'added', 1234, 1234),
@@ -114,46 +115,47 @@ describe('HunkView', () => {
     ])
 
     const selectLines1 = sinon.spy()
-    const view = new HunkView({hunk, selectedLines: new Set, selectLines: selectLines1})
+    const view = new HunkView({hunk, selectedLines: new Set(), selectLines: selectLines1})
     const element = view.element
-    const [line1, line2, line3, line4, line5, line6] = Array.from(element.querySelectorAll('.git-HunkView-line'))
+    // const [line1, line2, line3, line4, lines[4], line6] = Array.from(element.querySelectorAll('.git-HunkView-line'))
+    const lines = Array.from(element.querySelectorAll('.git-HunkView-line'))
 
     // clicking lines
-    line5.dispatchEvent(new MouseEvent('mousedown'))
-    line5.dispatchEvent(new MouseEvent('mouseup'))
+    lines[4].dispatchEvent(new MouseEvent('mousedown'))
+    lines[4].dispatchEvent(new MouseEvent('mouseup'))
     assert.deepEqual(Array.from(selectLines1.args[0][0]), hunk.getLines().slice(4, 5))
 
     selectLines1.reset()
-    line1.dispatchEvent(new MouseEvent('mousedown'))
-    line1.dispatchEvent(new MouseEvent('mouseup'))
+    lines[0].dispatchEvent(new MouseEvent('mousedown'))
+    lines[0].dispatchEvent(new MouseEvent('mouseup'))
     assert.deepEqual(Array.from(selectLines1.args[0][0]), hunk.getLines().slice(0, 1))
 
     // ensure updating the view with a different selectLines handler works
     const selectLines2 = sinon.spy()
-    await view.update({hunk, selectedLines: new Set, selectLines: selectLines2})
+    await view.update({hunk, selectedLines: new Set(), selectLines: selectLines2})
 
     // start dragging
-    line2.dispatchEvent(new MouseEvent('mousedown'))
+    lines[1].dispatchEvent(new MouseEvent('mousedown'))
     assert.deepEqual(Array.from(selectLines2.args[0][0]), hunk.getLines().slice(1, 2))
     // dragging the mouse within the same line is idempotent
     selectLines2.reset()
-    line2.dispatchEvent(new MouseEvent('mousemove'))
+    lines[1].dispatchEvent(new MouseEvent('mousemove'))
     assert.deepEqual(Array.from(selectLines2.args[0][0]), hunk.getLines().slice(1, 2))
     // dragging the mouse to the next adjacent lines selects them
     selectLines2.reset()
-    line3.dispatchEvent(new MouseEvent('mousemove'))
+    lines[2].dispatchEvent(new MouseEvent('mousemove'))
     assert.deepEqual(Array.from(selectLines2.args[0][0]), hunk.getLines().slice(1, 3))
     selectLines2.reset()
-    line6.dispatchEvent(new MouseEvent('mousemove'))
+    lines[5].dispatchEvent(new MouseEvent('mousemove'))
     assert.deepEqual(Array.from(selectLines2.args[0][0]), hunk.getLines().slice(1, 6))
     // dragging the mouse to a line before the first selected one selects the adjacent lines in the middle
     selectLines2.reset()
-    line1.dispatchEvent(new MouseEvent('mousemove'))
+    lines[0].dispatchEvent(new MouseEvent('mousemove'))
     assert.deepEqual(Array.from(selectLines2.args[0][0]), hunk.getLines().slice(0, 2))
     // stop dragging (outside the view)
     selectLines2.reset()
     window.dispatchEvent(new MouseEvent('mouseup'))
-    line2.dispatchEvent(new MouseEvent('mousemove'))
+    lines[1].dispatchEvent(new MouseEvent('mousemove'))
     assert(!selectLines2.called)
   })
 
