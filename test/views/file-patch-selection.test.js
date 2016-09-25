916c21c1d4f71096a52a003aebbdba84acf3ada2
Nathan Sobo <nathan@github.com>
2016-09-24 19:27:13 -0600

    Allow multiple hunk line selection ranges

--- test/views/file-patch-selection.test.js
+++ test/views/file-patch-selection.test.js
@@ -4,7 +4,7 @@ import FilePatchSelection from '../../lib/views/file-patch-selection'
 import Hunk from '../../lib/models/hunk'
 import HunkLine from '../../lib/models/hunk-line'
 
-describe.only('FilePatchSelection', () => {
+describe('FilePatchSelection', () => {
   describe('line selection', () => {
     it('starts a new selection with selectLine and expands an existing selection with selectToLine',  () => {
       const hunks = [
@@ -53,6 +53,50 @@ describe.only('FilePatchSelection', () => {
       selection.selectLine(hunks[1], hunks[1].lines[2])
       assert.deepEqual(selection.getSelectedLines(), [
         hunks[1].lines[2]
+      ])
+    })
+
+    it('adds a new selection if the `add` option is specified and always expands the most recent selection', function () {
+      const hunks = [
+        new Hunk(1, 1, 1, 3, [
+          new HunkLine('line-1', 'added', -1, 1),
+          new HunkLine('line-2', 'added', -1, 2),
+          new HunkLine('line-3', 'unchanged', 1, 3)
+        ]),
+        new Hunk(5, 7, 5, 4, [
+          new HunkLine('line-4', 'unchanged', 5, 7),
+          new HunkLine('line-5', 'deleted', 6, -1),
+          new HunkLine('line-6', 'deleted', 7, -1),
+          new HunkLine('line-7', 'added', -1, 8),
+          new HunkLine('line-8', 'added', -1, 9),
+          new HunkLine('line-9', 'added', -1, 10),
+          new HunkLine('line-10', 'deleted', 8, -1),
+          new HunkLine('line-11', 'deleted', 9, -1)
+        ])
+      ]
+      const selection = new FilePatchSelection(hunks)
+
+      selection.selectLine(hunks[0], hunks[0].lines[1])
+      selection.selectToLine(hunks[1], hunks[1].lines[1])
+
+      selection.selectLine(hunks[1], hunks[1].lines[3], true)
+      selection.selectToLine(hunks[1], hunks[1].lines[4])
+
+      assert.deepEqual(selection.getSelectedLines(), [
+        hunks[0].lines[1],
+        hunks[1].lines[1],
+        hunks[1].lines[3],
+        hunks[1].lines[4]
+      ])
+
+      selection.selectToLine(hunks[0], hunks[0].lines[0])
+
+      assert.deepEqual(selection.getSelectedLines(), [
+        hunks[0].lines[0],
+        hunks[0].lines[1],
+        hunks[1].lines[1],
+        hunks[1].lines[2],
+        hunks[1].lines[3]
       ])
     })
   })
