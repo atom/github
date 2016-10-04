dd81d047d36ef1b9ebd2c753fc9d9d7ef402ae15
Nathan Sobo <nathan@github.com>
2016-10-04 14:33:31 -0600

    Auto-scroll to selected hunks or lines in FilePatchView

--- test/views/file-patch-view.test.js
+++ test/views/file-patch-view.test.js
@@ -89,6 +89,40 @@ describe('FilePatchView', () => {
     assert(hunkView1.props.selectedLines.has(hunks[1].lines[3]))
   })
 
+  it('scrolls off-screen lines and hunks into view when they are selected', async function () {
+    const hunks = [
+      new Hunk(1, 1, 2, 4, [
+        new HunkLine('line-1', 'unchanged', 1, 1),
+        new HunkLine('line-2', 'added', -1, 2),
+        new HunkLine('line-3', 'added', -1, 3),
+        new HunkLine('line-4', 'unchanged', 2, 4)
+      ]),
+      new Hunk(5, 7, 1, 4, [
+        new HunkLine('line-5', 'unchanged', 5, 7),
+        new HunkLine('line-6', 'added', -1, 8),
+        new HunkLine('line-7', 'added', -1, 9),
+        new HunkLine('line-8', 'added', -1, 10)
+      ])
+    ]
+    const filePatchView = new FilePatchView({hunks})
+    document.body.appendChild(filePatchView.element)
+    filePatchView.element.style.overflow = 'scroll'
+    filePatchView.element.style.height = '100px'
+
+    filePatchView.togglePatchSelectionMode()
+    filePatchView.selectNext()
+    await new Promise(resolve => filePatchView.element.addEventListener('scroll', resolve))
+    assert.isAbove(filePatchView.element.scrollTop, 0)
+    const initScrollTop = filePatchView.element.scrollTop
+
+    filePatchView.togglePatchSelectionMode()
+    filePatchView.selectNext()
+    await new Promise(resolve => filePatchView.element.addEventListener('scroll', resolve))
+    assert.isAbove(filePatchView.element.scrollTop, initScrollTop)
+
+    filePatchView.element.remove()
+  })
+
   it('assigns the appropriate stage button label on hunks based on the stagingStatus and selection mode', async () => {
     const hunk = new Hunk(1, 1, 1, 2, [new HunkLine('line-1', 'added', -1, 1)])
     let hunkView
