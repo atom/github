218040b4e264060c44562812ddb386f653d4e5d1
Nathan Sobo <nathan@github.com>
2016-10-24 14:49:39 -0600

    Implement selectItem and preserveTail option on selectNext/PreviousItem
    Signed-off-by: Katrina Uychaco <kuychaco@github.com>

--- test/views/composite-list-selection.test.js
+++ test/views/composite-list-selection.test.js
@@ -4,6 +4,27 @@ import CompositeListSelection from '../../lib/views/composite-list-selection'
 import {assertEqualSets} from '../helpers'
 
 describe('CompositeListSelection', () => {
+  it('allows specific items to be selected, but does not select across lists', function () {
+    const selection = new CompositeListSelection({
+      unstagedChanges: ['a', 'b'],
+      mergeConflicts: ['c'],
+      stagedChanges: ['d', 'e', 'f'],
+    })
+
+    selection.selectItem('e')
+    assert.equal(selection.getActiveListKey(), 'stagedChanges')
+    assertEqualSets(selection.getSelectedItems(), new Set(['e']))
+    selection.selectItem('f', true)
+    assert.equal(selection.getActiveListKey(), 'stagedChanges')
+    assertEqualSets(selection.getSelectedItems(), new Set(['e', 'f']))
+    selection.selectItem('d', true)
+    assert.equal(selection.getActiveListKey(), 'stagedChanges')
+    assertEqualSets(selection.getSelectedItems(), new Set(['d', 'e']))
+    selection.selectItem('c', true)
+    assert.equal(selection.getActiveListKey(), 'stagedChanges')
+    assertEqualSets(selection.getSelectedItems(), new Set(['d', 'e']))
+  })
+
   it('allows the next and previous item to be selected', function () {
     const selection = new CompositeListSelection({
       unstagedChanges: ['a', 'b'],
@@ -53,5 +74,31 @@ describe('CompositeListSelection', () => {
     selection.selectPreviousItem()
     assert.equal(selection.getActiveListKey(), 'unstagedChanges')
     assertEqualSets(selection.getSelectedItems(), new Set(['a']))
+  })
+
+  it('allows the selection to be expanded to the next or previous item', function () {
+    const selection = new CompositeListSelection({
+      unstagedChanges: ['a', 'b'],
+      mergeConflicts: ['c'],
+      stagedChanges: ['d', 'e'],
+    })
+
+    assert.equal(selection.getActiveListKey(), 'unstagedChanges')
+    assertEqualSets(selection.getSelectedItems(), new Set(['a']))
+
+    selection.selectNextItem(true)
+    assert.equal(selection.getActiveListKey(), 'unstagedChanges')
+    assertEqualSets(selection.getSelectedItems(), new Set(['a', 'b']))
+
+    // Does not expand selections across lists
+    selection.selectNextItem(true)
+    assert.equal(selection.getActiveListKey(), 'unstagedChanges')
+    assertEqualSets(selection.getSelectedItems(), new Set(['a', 'b']))
+
+    selection.selectItem('e')
+    selection.selectPreviousItem(true)
+    selection.selectPreviousItem(true)
+    assert.equal(selection.getActiveListKey(), 'stagedChanges')
+    assertEqualSets(selection.getSelectedItems(), new Set(['d', 'e']))
   })
 })
