ee60023d78cd2397f4c44f1e7ec5922bc8830b34
Nathan Sobo <nathan@github.com>
2016-10-28 20:53:37 -0600

    Remove repository interaction from StagingView test

--- test/views/staging-view.test.js
+++ test/views/staging-view.test.js
@@ -1,21 +1,17 @@
 /** @babel */
 
-import {cloneRepository, buildRepository} from '../helpers'
-import path from 'path'
-import fs from 'fs'
 import sinon from 'sinon'
-
 import StagingView from '../../lib/views/staging-view'
+import FilePatch from '../../lib/models/file-patch'
 
 describe('StagingView', () => {
   describe('staging and unstaging files', () => {
     it('renders staged and unstaged files', async () => {
-      const workdirPath = await cloneRepository('three-files')
-      const repository = await buildRepository(workdirPath)
-      fs.writeFileSync(path.join(workdirPath, 'a.txt'), 'a change\n')
-      fs.unlinkSync(path.join(workdirPath, 'b.txt'))
-      const filePatches = await repository.getUnstagedChanges()
-      const view = new StagingView({repository, stagedChanges: [], unstagedChanges: filePatches})
+      const filePatches = [
+        new FilePatch('a.txt', 'a.txt', 'modified', []),
+        new FilePatch('b.txt', null, 'deleted', [])
+      ]
+      const view = new StagingView({unstagedChanges: filePatches, stagedChanges: []})
       const {refs} = view
       function textContentOfChildren (element) {
         return Array.from(element.children).map(child => child.textContent)
@@ -24,27 +20,26 @@ describe('StagingView', () => {
       assert.deepEqual(textContentOfChildren(refs.unstagedChanges), ['a.txt', 'b.txt'])
       assert.deepEqual(textContentOfChildren(refs.stagedChanges), [])
 
-      await view.update({repository, stagedChanges: [filePatches[1]], unstagedChanges: [filePatches[0]]})
+      await view.update({unstagedChanges: [filePatches[0]], stagedChanges: [filePatches[1]]})
       assert.deepEqual(textContentOfChildren(refs.unstagedChanges), ['a.txt'])
       assert.deepEqual(textContentOfChildren(refs.stagedChanges), ['b.txt'])
     })
 
     describe('confirmSelectedItems()', () => {
       it('calls stageFilePatch or unstageFilePatch depending on the current staging state of the toggled file patch', async () => {
-        const workdirPath = await cloneRepository('three-files')
-        const repository = await buildRepository(workdirPath)
-        fs.writeFileSync(path.join(workdirPath, 'a.txt'), 'a change\n')
-        fs.unlinkSync(path.join(workdirPath, 'b.txt'))
-        const filePatches = await repository.getUnstagedChanges()
+        const filePatches = [
+          new FilePatch('a.txt', 'a.txt', 'modified', []),
+          new FilePatch('b.txt', null, 'deleted', [])
+        ]
         const stageFiles = sinon.spy()
         const unstageFiles = sinon.spy()
-        const view = new StagingView({repository, stagedChanges: [], unstagedChanges: filePatches, stageFiles, unstageFiles})
+        const view = new StagingView({unstagedChanges: filePatches, stagedChanges: [], stageFiles, unstageFiles})
 
         view.mousedownOnItem({detail: 1}, filePatches[1])
         view.confirmSelectedItems()
         assert.isTrue(stageFiles.calledWith(['b.txt']))
 
-        await view.update({repository, stagedChanges: [filePatches[1]], unstagedChanges: [filePatches[0]], stageFiles, unstageFiles})
+        await view.update({unstagedChanges: [filePatches[0]], stagedChanges: [filePatches[1]], stageFiles, unstageFiles})
         view.mousedownOnItem({detail: 1}, filePatches[1])
         view.confirmSelectedItems()
         assert.isTrue(unstageFiles.calledWith(['b.txt']))
@@ -54,19 +49,17 @@ describe('StagingView', () => {
 
   describe('merge conflicts list', () => {
     it('is visible only when conflicted paths are passed', async () => {
-      const workdirPath = await cloneRepository('three-files')
-      const repository = await buildRepository(workdirPath)
-      const view = new StagingView({repository, stagedChanges: [], unstagedChanges: []})
+      const view = new StagingView({unstagedChanges: [], stagedChanges: []})
 
       assert.isUndefined(view.refs.mergeConflicts)
 
-      const mergeConflict = {
+      const mergeConflicts = [{
         getPath: () => 'conflicted-path',
         getFileStatus: () => 'modified',
         getOursStatus: () => 'deleted',
         getTheirsStatus: () => 'modified'
-      }
-      await view.update({repository, mergeConflicts: [mergeConflict], stagedChanges: [], unstagedChanges: []})
+      }]
+      await view.update({unstagedChanges: [], mergeConflicts, stagedChanges: []})
       assert.isDefined(view.refs.mergeConflicts)
     })
   })
