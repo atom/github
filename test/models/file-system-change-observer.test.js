f9f8552fa0e1d437e22416b6caf1750ced2765f9
Katrina Uychaco <kuychaco@gmail.com>
2016-10-31 17:26:29 -0700

    Add FileSystemChangeObserver tests to verify updates after file changes

--- test/models/file-system-change-observer.test.js
+++ test/models/file-system-change-observer.test.js
@@ -5,14 +5,16 @@ import path from 'path'
 import temp from 'temp'
 import sinon from 'sinon'
 
-import {cloneRepository, buildRepository} from '../helpers'
+import {cloneRepository, buildRepository, setUpLocalAndRemoteRepositories} from '../helpers'
 
 import FileSystemChangeObserver from '../../lib/models/file-system-change-observer'
 
 describe('FileSystemChangeObserver', async () => {
-  it('emits an event when the currently active directory changes', async function () {
+  beforeEach(function () {
     this.timeout(5000) // increase the timeout because we're interacting with file system events.
+  })
 
+  it('emits an event when the currently active directory changes', async () => {
     const workdirPath1 = await cloneRepository('three-files')
     const repository1 = await buildRepository(workdirPath1)
     const workdirPath2 = await cloneRepository('three-files')
@@ -48,6 +50,96 @@ describe('FileSystemChangeObserver', async () => {
     fs.writeFileSync(path.join(workdirPath1, 'd.txt'), 'a change\n')
     await Promise.race([changeObserver.lastFileChangePromise, timeout(500)])
     assert(!changeSpy.called)
+  })
+
+  it('emits an event when a project file is modified, created, or deleted', async () => {
+    const workdirPath = await cloneRepository('three-files')
+    const repository = await buildRepository(workdirPath)
+    const changeSpy = sinon.spy()
+    const changeObserver = new FileSystemChangeObserver()
+    changeObserver.onDidChange(changeSpy)
+    await changeObserver.start()
+
+    await changeObserver.setActiveRepository(repository)
+    fs.writeFileSync(path.join(workdirPath, 'a.txt'), 'a change\n')
+    await changeObserver.lastFileChangePromise
+    assert(changeSpy.calledOnce)
+
+    changeSpy.reset()
+    fs.writeFileSync(path.join(workdirPath, 'new-file.txt'), 'a change\n')
+    await changeObserver.lastFileChangePromise
+    assert(changeSpy.calledOnce)
+
+    changeSpy.reset()
+    fs.unlinkSync(path.join(workdirPath, 'a.txt'))
+    await changeObserver.lastFileChangePromise
+    assert(changeSpy.calledOnce)
+  })
+
+  it('emits an event when a file is staged or unstaged', async () => {
+    const workdirPath = await cloneRepository('three-files')
+    const repository = await buildRepository(workdirPath)
+    const changeSpy = sinon.spy()
+    const changeObserver = new FileSystemChangeObserver()
+    changeObserver.onDidChange(changeSpy)
+    await changeObserver.start()
+    await changeObserver.setActiveRepository(repository)
+
+    fs.writeFileSync(path.join(workdirPath, 'a.txt'), 'a change\n')
+    await repository.git.exec(['add', 'a.txt'])
+    await changeObserver.lastFileChangePromise
+    assert(changeSpy.calledOnce)
+
+    changeSpy.reset()
+    await repository.git.exec(['reset', 'a.txt'])
+    await changeObserver.lastFileChangePromise
+    assert(changeSpy.calledOnce)
+  })
+
+  it('emits an event when a branch is checked out', async () => {
+    const workdirPath = await cloneRepository('three-files')
+    const repository = await buildRepository(workdirPath)
+    const changeSpy = sinon.spy()
+    const changeObserver = new FileSystemChangeObserver()
+    changeObserver.onDidChange(changeSpy)
+    await changeObserver.start()
+    await changeObserver.setActiveRepository(repository)
+
+    await repository.git.exec(['checkout', '-b', 'new-branch'])
+    await changeObserver.lastFileChangePromise
+    assert(changeSpy.calledOnce)
+  })
+
+  it('emits an event when a new tracking branch is added after pushing', async () => {
+    const {localRepoPath} = await setUpLocalAndRemoteRepositories()
+    const repository = await buildRepository(localRepoPath)
+    const changeSpy = sinon.spy()
+    const changeObserver = new FileSystemChangeObserver()
+    changeObserver.onDidChange(changeSpy)
+    await changeObserver.start()
+    await changeObserver.setActiveRepository(repository)
+
+    await repository.git.exec(['checkout', '-b', 'new-branch'])
+    await changeObserver.lastFileChangePromise
+
+    changeSpy.reset()
+    await repository.git.exec(['push', '--set-upstream'])
+    await changeObserver.lastFileChangePromise
+    assert(changeSpy.calledOnce)
+  })
+
+  it('emits an event when commits have been fetched', async () => {
+    const {localRepoPath} = await setUpLocalAndRemoteRepositories({remoteAhead: true})
+    const repository = await buildRepository(localRepoPath)
+    const changeSpy = sinon.spy()
+    const changeObserver = new FileSystemChangeObserver()
+    changeObserver.onDidChange(changeSpy)
+    await changeObserver.start()
+    await changeObserver.setActiveRepository(repository)
+
+    await repository.git.exec(['fetch'])
+    await changeObserver.lastFileChangePromise
+    assert(changeSpy.calledOnce)
   })
 
   function timeout(ms) {
