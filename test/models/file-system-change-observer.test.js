abafbbf2b09a8a33f4da6fcc6fe3d035be958d92
Katrina Uychaco <kuychaco@gmail.com>
2016-11-01 02:12:07 -0700

    :art: assert -> assert.isTrue

--- test/models/file-system-change-observer.test.js
+++ test/models/file-system-change-observer.test.js
@@ -28,28 +28,28 @@ describe('FileSystemChangeObserver', async () => {
     fs.writeFileSync(path.join(workdirPath1, 'a.txt'), 'a change\n')
     fs.writeFileSync(path.join(workdirPath2, 'a.txt'), 'a change\n')
     await changeObserver.lastFileChangePromise
-    assert(changeSpy.calledOnce)
+    assert.isTrue(changeSpy.calledOnce)
 
     changeSpy.reset()
     await changeObserver.setActiveRepository(repository2)
     fs.writeFileSync(path.join(workdirPath1, 'b.txt'), 'a change\n')
     fs.writeFileSync(path.join(workdirPath2, 'b.txt'), 'a change\n')
     await changeObserver.lastFileChangePromise
-    assert(changeSpy.calledOnce)
+    assert.isTrue(changeSpy.calledOnce)
 
     changeSpy.reset()
     await changeObserver.setActiveRepository(null)
     fs.writeFileSync(path.join(workdirPath1, 'c.txt'), 'a change\n')
     fs.writeFileSync(path.join(workdirPath2, 'c.txt'), 'a change\n')
     await Promise.race([changeObserver.lastFileChangePromise, timeout(500)])
-    assert(!changeSpy.called)
+    assert.isTrue(!changeSpy.called)
 
     changeSpy.reset()
     await changeObserver.setActiveRepository(repository1)
     await changeObserver.stop()
     fs.writeFileSync(path.join(workdirPath1, 'd.txt'), 'a change\n')
     await Promise.race([changeObserver.lastFileChangePromise, timeout(500)])
-    assert(!changeSpy.called)
+    assert.isTrue(!changeSpy.called)
   })
 
   it('emits an event when a project file is modified, created, or deleted', async () => {
@@ -63,17 +63,17 @@ describe('FileSystemChangeObserver', async () => {
     await changeObserver.setActiveRepository(repository)
     fs.writeFileSync(path.join(workdirPath, 'a.txt'), 'a change\n')
     await changeObserver.lastFileChangePromise
-    assert(changeSpy.calledOnce)
+    assert.isTrue(changeSpy.calledOnce)
 
     changeSpy.reset()
     fs.writeFileSync(path.join(workdirPath, 'new-file.txt'), 'a change\n')
     await changeObserver.lastFileChangePromise
-    assert(changeSpy.calledOnce)
+    assert.isTrue(changeSpy.calledOnce)
 
     changeSpy.reset()
     fs.unlinkSync(path.join(workdirPath, 'a.txt'))
     await changeObserver.lastFileChangePromise
-    assert(changeSpy.calledOnce)
+    assert.isTrue(changeSpy.calledOnce)
   })
 
   it('emits an event when a file is staged or unstaged', async () => {
@@ -88,12 +88,12 @@ describe('FileSystemChangeObserver', async () => {
     fs.writeFileSync(path.join(workdirPath, 'a.txt'), 'a change\n')
     await repository.git.exec(['add', 'a.txt'])
     await changeObserver.lastFileChangePromise
-    assert(changeSpy.calledOnce)
+    assert.isTrue(changeSpy.calledOnce)
 
     changeSpy.reset()
     await repository.git.exec(['reset', 'a.txt'])
     await changeObserver.lastFileChangePromise
-    assert(changeSpy.calledOnce)
+    assert.isTrue(changeSpy.calledOnce)
   })
 
   it('emits an event when a branch is checked out', async () => {
@@ -107,7 +107,7 @@ describe('FileSystemChangeObserver', async () => {
 
     await repository.git.exec(['checkout', '-b', 'new-branch'])
     await changeObserver.lastFileChangePromise
-    assert(changeSpy.calledOnce)
+    assert.isTrue(changeSpy.calledOnce)
   })
 
   it('emits an event when a new tracking branch is added after pushing', async () => {
@@ -125,7 +125,7 @@ describe('FileSystemChangeObserver', async () => {
     changeSpy.reset()
     await repository.git.exec(['push', '--set-upstream'])
     await changeObserver.lastFileChangePromise
-    assert(changeSpy.calledOnce)
+    assert.isTrue(changeSpy.calledOnce)
   })
 
   it('emits an event when commits have been fetched', async () => {
@@ -139,7 +139,7 @@ describe('FileSystemChangeObserver', async () => {
 
     await repository.git.exec(['fetch'])
     await changeObserver.lastFileChangePromise
-    assert(changeSpy.calledOnce)
+    assert.isTrue(changeSpy.calledOnce)
   })
 
   function timeout(ms) {
