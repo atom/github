38f1e9a11f643961d628200d64689930065a6eeb
Michelle Tilley <binarymuse@github.com>
2017-01-05 10:01:53 -0800

    Support setting timeout with assert.async(ms).method()

--- assert-async-plugin.js
+++ assert-async-plugin.js
@@ -1,22 +1,33 @@
 module.exports = function({types: t, template}) {
   const generator = template(`
-    until(fail => {
+    UNTIL_FUNC(fail => {
       try {
         ASSERT_CALL;
         return true;
       } catch (err) {
         return fail(err);
       }
-    })
+    }, UNTIL_ARGS)
   `);
 
   return {
     name: 'assert-async',
     visitor: {
       CallExpression(path) {
+        // assuming `assert.async.something(arg1, arg2)`
+
+        // assert.async.something
         const callee = path.node.callee;
         if (!t.isMemberExpression(callee)) { return; }
-        const calleeObject = callee.object;
+        // assert.async
+        let calleeObject = callee.object;
+        let asyncArgs = [];
+        // support `assert.async(200).something(arg1, arg2)` for setting timeout
+        // in this case, `calleeObject` is actually `assert.async(200)` not `assert.async`
+        if (t.isCallExpression(calleeObject)) {
+          asyncArgs = calleeObject.arguments; // [200]
+          calleeObject = calleeObject.callee; // assert.async
+        }
         if (!t.isMemberExpression(calleeObject)) { return; }
 
         const calleeObjectObject = calleeObject.object;
@@ -40,7 +51,11 @@ module.exports = function({types: t, template}) {
         // assert.something(arg1, arg2)
         const assertExpression = t.callExpression(assertDotMethod, callArgs);
 
-        path.replaceWith(generator({ASSERT_CALL: assertExpression}));
+        path.replaceWith(generator({
+          ASSERT_CALL: assertExpression,
+          UNTIL_ARGS: asyncArgs,
+          UNTIL_FUNC: t.identifier('until'),
+        }));
       },
     },
   };
