8bda5e277c84f0a5be1a0a11bd3a43718104e7a8
Ash Wilson <smashwilson@github.com>
2017-01-11 15:24:50 -0500

    Increase default test-until timeout to 5000ms

--- test/runner.js
+++ test/runner.js
@@ -2,8 +2,13 @@ import {createRunner} from 'atom-mocha-test-runner';
 import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
 
+import until from 'test-until';
+
 chai.use(chaiAsPromised);
 global.assert = chai.assert;
+
+// Give tests that rely on filesystem event delivery lots of breathing room.
+until.setDefaultTimeout(5000);
 
 module.exports = createRunner({
   reporter: 'spec',
