8219e78b6b46261eead64aab51a0febd372db3df
Michelle Tilley <binarymuse@github.com>
2017-01-10 17:25:21 -0800

    Add the failing assertion code as assertion failure messages

--- test/assert-transforms.test.js
+++ test/assert-transforms.test.js
@@ -29,6 +29,18 @@ describe('assert.async', function() {
     }
 
     assert.match(caught.message, /^async\(50ms\):/);
+    assert.match(caught.message, /assert\.isTrue\(val\)/);
     assert.match(caught.message, /false.*true/);
+  });
+
+  it('includes the assert line in the exception, even for non-async assertions', function() {
+    let caught = null;
+    try {
+      assert.equal(1, 2);
+    } catch (err) {
+      caught = err;
+    }
+
+    assert.match(caught.message, /assert\.equal\(1, 2\)/);
   });
 });
