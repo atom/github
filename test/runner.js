aa3fd22e98271f76f90e75364c733c671ac8df23
Ash Wilson <smashwilson@github.com>
2017-01-11 19:52:10 -0500

    Use the custom reporter on AppVeyor builds

--- test/runner.js
+++ test/runner.js
@@ -5,7 +5,9 @@ import chaiAsPromised from 'chai-as-promised';
 chai.use(chaiAsPromised);
 global.assert = chai.assert;
 
+const reporter = process.env.APPVEYOR_API_URL ? 'mocha-appveyor-reporter' : 'spec';
+
 module.exports = createRunner({
-  reporter: 'spec',
+  reporter,
   overrideTestPaths: [/spec$/, /test/],
 });
