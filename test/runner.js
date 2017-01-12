64a63358e4744f7d4d9de2187a377f068ac8e287
Ash Wilson <smashwilson@github.com>
2017-01-11 20:48:51 -0500

    Let's try passing the reporter _this_ way

--- test/runner.js
+++ test/runner.js
@@ -1,11 +1,12 @@
 import {createRunner} from 'atom-mocha-test-runner';
 import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
+import AppVeyorReporter from 'mocha-appveyor-reporter';
 
 chai.use(chaiAsPromised);
 global.assert = chai.assert;
 
-const reporter = process.env.APPVEYOR_API_URL ? 'mocha-appveyor-reporter' : 'spec';
+const reporter = process.env.APPVEYOR_API_URL ? AppVeyorReporter : 'spec';
 
 module.exports = createRunner({
   reporter,
