a9e49d5e56a7fcf54fa77b12392cca5609885a9c
Michelle Tilley <binarymuse@github.com>
2017-01-14 21:53:04 -0800

    ohhhh maybe this will help

--- test/runner.js
+++ test/runner.js
@@ -1,14 +1,15 @@
 import {createRunner} from 'atom-mocha-test-runner';
 import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
-import AppVeyorReporter from 'mocha-appveyor-reporter';
 
 chai.use(chaiAsPromised);
 global.assert = chai.assert;
 
-const reporter = process.env.APPVEYOR_API_URL ? AppVeyorReporter : 'spec';
-
 module.exports = createRunner({
-  reporter,
+  'spec',
   overrideTestPaths: [/spec$/, /test/],
+}, mocha => {
+  if (process.env.APPVEYOR_API_URL) {
+    mocha.reporter(require('mocha-appveyor-reporter'));
+  }
 });
