f09b8d27ea4cd882e74a39f176e152d1ba41b5ee
Katrina Uychaco <kuychaco@gmail.com>
2017-02-21 00:27:27 -0800

    Implement undo discard for Unstaged Changes list

--- test/controllers/git-controller.test.js
+++ test/controllers/git-controller.test.js
@@ -444,7 +444,7 @@ describe('GitController', function() {
       });
     });
 
-    describe('undoLastDiscardForFilePath(filePath)', () => {
+    describe('undoLastDiscard({partial, filePath})', () => {
       let unstagedFilePatch, repository, absFilePath, wrapper;
       beforeEach(async () => {
         const workdirPath = await cloneRepository('multi-line-file');
@@ -475,9 +475,9 @@ describe('GitController', function() {
         const contents3 = fs.readFileSync(absFilePath, 'utf8');
         assert.notEqual(contents2, contents3);
 
-        await wrapper.instance().undoLastDiscardForFilePath('sample.js');
+        await wrapper.instance().undoLastDiscard({partial: true, filePath: 'sample.js'});
         await assert.async.equal(fs.readFileSync(absFilePath, 'utf8'), contents2);
-        await wrapper.instance().undoLastDiscardForFilePath('sample.js');
+        await wrapper.instance().undoLastDiscard({partial: true, filePath: 'sample.js'});
         await assert.async.equal(fs.readFileSync(absFilePath, 'utf8'), contents1);
       });
 
@@ -497,7 +497,7 @@ describe('GitController', function() {
         await repository.refresh();
         unstagedFilePatch = await repository.getFilePatchForPath('sample.js');
         wrapper.setState({filePatch: unstagedFilePatch});
-        await wrapper.instance().undoLastDiscardForFilePath('sample.js');
+        await wrapper.instance().undoLastDiscard({partial: true, filePath: 'sample.js'});
         const notificationArgs = notificationManager.addError.args[0];
         assert.equal(notificationArgs[0], 'Cannot undo last discard.');
         assert.match(notificationArgs[1].description, /You have unsaved changes./);
@@ -518,7 +518,7 @@ describe('GitController', function() {
           await repository.refresh();
           unstagedFilePatch = await repository.getFilePatchForPath('sample.js');
           wrapper.setState({filePatch: unstagedFilePatch});
-          await wrapper.instance().undoLastDiscardForFilePath('sample.js');
+          await wrapper.instance().undoLastDiscard({partial: true, filePath: 'sample.js'});
 
           await assert.async.equal(fs.readFileSync(absFilePath, 'utf8'), contents1 + change);
         });
@@ -539,7 +539,7 @@ describe('GitController', function() {
 
           // click 'Cancel'
           confirm.returns(2);
-          await wrapper.instance().undoLastDiscardForFilePath('sample.js');
+          await wrapper.instance().undoLastDiscard({partial: true, filePath: 'sample.js'});
           assert.equal(confirm.callCount, 1);
           const confirmArg = confirm.args[0][0];
           assert.match(confirmArg.message, /Undoing will result in conflicts/);
@@ -547,16 +547,16 @@ describe('GitController', function() {
 
           // click 'Open in new buffer'
           confirm.returns(1);
-          await wrapper.instance().undoLastDiscardForFilePath('sample.js');
+          await wrapper.instance().undoLastDiscard({partial: true, filePath: 'sample.js'});
           assert.equal(confirm.callCount, 2);
           const activeEditor = workspace.getActiveTextEditor();
-          assert.isNull(activeEditor.getFileName());
+          assert.match(activeEditor.getFileName(), /sample.js-/);
           assert.isTrue(activeEditor.getText().includes('<<<<<<<'));
           assert.isTrue(activeEditor.getText().includes('>>>>>>>'));
 
           // click 'Proceed and resolve conflicts'
           confirm.returns(0);
-          await wrapper.instance().undoLastDiscardForFilePath('sample.js');
+          await wrapper.instance().undoLastDiscard({partial: true, filePath: 'sample.js'});
           assert.equal(confirm.callCount, 3);
           await assert.async.isTrue(fs.readFileSync(absFilePath, 'utf8').includes('<<<<<<<'));
           await assert.async.isTrue(fs.readFileSync(absFilePath, 'utf8').includes('>>>>>>>'));
@@ -576,7 +576,7 @@ describe('GitController', function() {
 
         sinon.stub(notificationManager, 'addError');
         assert.equal(repository.getPartialDiscardUndoHistoryForPath('sample.js').length, 2);
-        await wrapper.instance().undoLastDiscardForFilePath('sample.js');
+        await wrapper.instance().undoLastDiscard({partial: true, filePath: 'sample.js'});
         const notificationArgs = notificationManager.addError.args[0];
         assert.equal(notificationArgs[0], 'Discard history has expired.');
         assert.match(notificationArgs[1].description, /Stale discard history has been deleted./);
