6652a0c3699ddffc1872f262587a7001f866cf9c
Ash Wilson <smashwilson@github.com>
2017-02-23 15:12:03 -0500

    Initialize GithubPackage properly

--- test/github-package.test.js
+++ test/github-package.test.js
@@ -331,7 +331,9 @@ describe('GithubPackage', function() {
       assert.isDefined(payload.resolutionProgressByPath[workdirMergeConflict]);
       assert.isUndefined(payload.resolutionProgressByPath[workdirNoConflict]);
 
-      const githubPackage1 = new GithubPackage(workspace, project, commandRegistry, notificationManager, config);
+      const githubPackage1 = new GithubPackage(
+        workspace, project, commandRegistry, notificationManager, tooltips, config, confirm,
+      );
       await githubPackage1.activate(payload);
       await githubPackage1.getInitialModelsPromise();
 
