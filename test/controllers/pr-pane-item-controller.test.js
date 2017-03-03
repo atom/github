000f9422535fe203b9908807e3c6a08c0bfb4e1a
Michelle Tilley <binarymuse@github.com>
2017-03-03 15:21:36 -0800

    Change PrPaneItemController URI format and parsing

--- test/controllers/pr-pane-item-controller.test.js
+++ test/controllers/pr-pane-item-controller.test.js
@@ -8,32 +8,21 @@ describe('PrPaneItemController', function() {
     });
 
     it('returns an item given a valid PR URL', function() {
-      const item = PrPaneItemController.opener('atom-github://pull-request/atom/github/123');
+      const item = PrPaneItemController.opener('atom-github://pull-request/api.github.com/atom/github/123');
       assert.deepEqual(PrPaneItemController.create.getCall(0).args[0], {
+        host: 'api.github.com',
         owner: 'atom',
         repo: 'github',
         prNumber: 123,
-        options: {host: undefined, enterprise: false},
-      });
-      assert.equal(item, paneItem);
-    });
-
-    it('can accept info about a GHE endpoint', function() {
-      const item = PrPaneItemController.opener('atom-github://pull-request/atom/github/123?host=ghe.io&enterprise=true');
-      assert.deepEqual(PrPaneItemController.create.getCall(0).args[0], {
-        owner: 'atom',
-        repo: 'github',
-        prNumber: 123,
-        options: {host: 'ghe.io', enterprise: true},
       });
       assert.equal(item, paneItem);
     });
 
     [
-      ['returns null if a segment is missing', 'atom-github://pull-request/atom/github'],
-      ['returns null if the PR number is not a number', 'atom-github://pull-request/atom/github/asdf'],
-      ['returns null if the host is not pull-request', 'atom-github://pr/atom/github/123'],
-      ['returns null if the protocol is not atom-github', 'github://pull-request/atom/github/123'],
+      ['returns null if a segment is missing', 'atom-github://pull-request/atom/github/123'],
+      ['returns null if the PR number is not a number', 'atom-github://pull-request/api.github.com/atom/github/asdf'],
+      ['returns null if the host is not pull-request', 'atom-github://pr/api.github.com/atom/github/123'],
+      ['returns null if the protocol is not atom-github', 'github://pull-request/api.github.com/atom/github/123'],
     ].forEach(([description, uri]) => {
       it(description, function() {
         const item = PrPaneItemController.opener(uri);
