e51bbf671b0c6bc670f54204dacab391c7bce13d
Ash Wilson <smashwilson@github.com>
2017-03-06 14:04:39 -0500

    Let's see if this fixes it on AppVeyor

--- test/git-prompt-server.test.js
+++ test/git-prompt-server.test.js
@@ -1,45 +1,36 @@
 import {execFile} from 'child_process';
 
-import semver from 'semver';
-
-const {major, minor, patch} = semver.parse(atom.appVersion);
-const atomVersion = `${major}.${minor}.${patch}`;
-const requiredVersion = '>=1.14.0';
-
 import GitPromptServer from '../lib/git-prompt-server';
 
-// Will not pass on Appveyor
-if (process.platform !== 'win32' && semver.satisfies(atomVersion, requiredVersion)) {
-  describe('GitPromptServer', function() {
-    it('prompts for user input and writes the response to stdout', async function() {
-      this.timeout(10000);
+describe('GitPromptServer', function() {
+  it('prompts for user input and writes the response to stdout', async function() {
+    this.timeout(10000);
 
-      const server = new GitPromptServer();
-      const {helper, socket, electron} = await server.start(question => {
-        assert.equal(question, 'What... is your favorite color?\u0000');
-        return 'Green. I mean blue! AAAhhhh...';
-      });
-
-      let err, stdout;
-      await new Promise((resolve, reject) => {
-        execFile(electron,
-          [helper, socket, 'What... is your favorite color?'],
-          {
-            env: {
-              ELECTRON_RUN_AS_NODE: 1,
-              ELECTRON_NO_ATTACH_CONSOLE: 1,
-            },
-          }, (_err, _stdout, _stderr) => {
-            err = _err;
-            stdout = _stdout;
-            resolve();
-          });
-      });
-
-      assert.ifError(err);
-      assert.equal(stdout, 'Green. I mean blue! AAAhhhh...\n');
-
-      await server.terminate();
+    const server = new GitPromptServer();
+    const {helper, socket, electron} = await server.start(question => {
+      assert.equal(question, 'What... is your favorite color?\u0000');
+      return 'Green. I mean blue! AAAhhhh...';
     });
+
+    let err, stdout;
+    await new Promise((resolve, reject) => {
+      execFile(electron,
+        [helper, socket, 'What... is your favorite color?'],
+        {
+          env: {
+            ELECTRON_RUN_AS_NODE: 1,
+            ELECTRON_NO_ATTACH_CONSOLE: 1,
+          },
+        }, (_err, _stdout, _stderr) => {
+          err = _err;
+          stdout = _stdout;
+          resolve();
+        });
+    });
+
+    assert.ifError(err);
+    assert.equal(stdout, 'Green. I mean blue! AAAhhhh...\n');
+
+    await server.terminate();
   });
-}
+});
