575c8a4a0120f1fe707cd9c79aab647a2bb6f0f6
Ash Wilson <smashwilson@github.com>
2017-03-09 08:31:49 -0500

    Test both prompt protocols

--- test/git-prompt-server.test.js
+++ test/git-prompt-server.test.js
@@ -3,44 +3,90 @@ import {execFile} from 'child_process';
 import GitPromptServer from '../lib/git-prompt-server';
 
 describe('GitPromptServer', function() {
-  it('prompts for user input and writes the response to stdout', async function() {
-    this.timeout(10000);
+  describe('credential helper', function() {
+    it('prompts for user input and writes JSON to stdout', async function() {
+      this.timeout(10000);
 
-    const server = new GitPromptServer();
-    const {helper, socket, electron} = await server.start(query => {
-      assert.equal(query.prompt, 'Please enter your credentials for https://what-is-your-favorite-color.com');
-      assert.isTrue(query.includeUsername);
-      return {
-        username: 'old-man-from-scene-24',
-        password: 'Green. I mean blue! AAAhhhh...',
-      };
-    });
+      const server = new GitPromptServer();
+      const {credentialHelper, socket, electron} = await server.start(query => {
+        assert.equal(query.prompt, 'Please enter your credentials for https://what-is-your-favorite-color.com');
+        assert.isTrue(query.includeUsername);
+        return {
+          username: 'old-man-from-scene-24',
+          password: 'Green. I mean blue! AAAhhhh...',
+        };
+      });
 
-    let err, stdout;
-    await new Promise((resolve, reject) => {
-      const child = execFile(electron,
-        [helper, socket, 'get'],
-        {
-          env: {
-            ELECTRON_RUN_AS_NODE: 1,
-            ELECTRON_NO_ATTACH_CONSOLE: 1,
+      let err, stdout;
+      await new Promise((resolve, reject) => {
+        const child = execFile(
+          credentialHelper.launcher,
+          ['get'],
+          {
+            env: {
+              ATOM_GITHUB_ELECTRON_PATH: electron,
+              ATOM_GITHUB_SOCK_PATH: socket,
+              ATOM_GITHUB_CREDENTIAL_PATH: credentialHelper.script,
+            },
           },
-        }, (_err, _stdout, _stderr) => {
-          err = _err;
-          stdout = _stdout;
-          resolve();
-        });
+          (_err, _stdout, _stderr) => {
+            err = _err;
+            stdout = _stdout;
+            resolve();
+          },
+        );
 
-      child.stdin.write('protocol=https\n');
-      child.stdin.write('host=what-is-your-favorite-color.com\n');
-      child.stdin.end('\n');
+        child.stdin.write('protocol=https\n');
+        child.stdin.write('host=what-is-your-favorite-color.com\n');
+        child.stdin.end('\n');
+      });
+
+      assert.ifError(err);
+      assert.equal(stdout,
+        'protocol=https\nhost=what-is-your-favorite-color.com\n' +
+        'username=old-man-from-scene-24\npassword=Green. I mean blue! AAAhhhh...\n');
+
+      await server.terminate();
     });
+  });
 
-    assert.ifError(err);
-    assert.equal(stdout,
-      'protocol=https\nhost=what-is-your-favorite-color.com\n' +
-      'username=old-man-from-scene-24\npassword=Green. I mean blue! AAAhhhh...\n');
+  describe('askpass helper', function() {
+    it('prompts for user input and writes the response to stdout', async function() {
+      this.timeout(10000);
 
-    await server.terminate();
+      const server = new GitPromptServer();
+      const {askPass, socket, electron} = await server.start(query => {
+        assert.equal(query.prompt, 'Please enter your password for "updog"');
+        assert.isFalse(query.includeUsername);
+        return {
+          password: "What's 'updog'?",
+        };
+      });
+
+      let err, stdout;
+      await new Promise((resolve, reject) => {
+        execFile(
+          askPass.launcher,
+          ['Please enter your password for "updog"'],
+          {
+            env: {
+              ATOM_GITHUB_ELECTRON_PATH: electron,
+              ATOM_GITHUB_SOCK_PATH: socket,
+              ATOM_GITHUB_ASKPASS_PATH: askPass.script,
+            },
+          },
+          (_err, _stdout, _stderr) => {
+            err = _err;
+            stdout = _stdout;
+            resolve();
+          },
+        );
+      });
+
+      assert.ifError(err);
+      assert.equal(stdout, "What's 'updog'?");
+
+      await server.terminate();
+    });
   });
 });
