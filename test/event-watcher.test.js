50815ea36578c7bd6736ae2f72af952ad23e2cb4
Ash Wilson <smashwilson@github.com>
2017-03-27 14:12:29 -0400

    Ensure getXyzPromise() and resolveXyzPromise() methods align

--- test/event-watcher.test.js
+++ test/event-watcher.test.js
@@ -41,4 +41,65 @@ describe('EventWatcher', function() {
     watcher.rejectPromise('testing', new Error('oh shit'));
     await assert.isRejected(promise, /oh shit/);
   });
+
+  describe('function pairs', function() {
+    const baseNames = Object.getOwnPropertyNames(EventWatcher.prototype)
+      .map(methodName => /^get(.+)Promise$/.exec(methodName))
+      .filter(match => match !== null)
+      .map(match => match[1]);
+    let functionPairs;
+
+    beforeEach(function() {
+      functionPairs = baseNames.map(baseName => {
+        return {
+          baseName,
+          getter: watcher[`get${baseName}Promise`].bind(watcher),
+          resolver: watcher[`resolve${baseName}Promise`].bind(watcher),
+        };
+      });
+    });
+
+    baseNames.forEach(baseName => {
+      it(`resolves the correct Promise for ${baseName}`, async function() {
+        const allPromises = [];
+        const positiveResults = [];
+        const negativeResults = [];
+
+        let positiveResolver = null;
+        const negativeResolvers = [];
+
+        for (let i = 0; i < functionPairs.length; i++) {
+          const functionPair = functionPairs[i];
+
+          if (functionPair.baseName === baseName) {
+            const positivePromise = functionPair.getter().then(payload => {
+              positiveResults.push(payload);
+            });
+            allPromises.push(positivePromise);
+
+            positiveResolver = functionPair.resolver;
+          } else {
+            const negativePromise = functionPair.getter().then(payload => {
+              negativeResults.push(payload);
+            });
+            allPromises.push(negativePromise);
+
+            negativeResolvers.push(functionPair.resolver);
+          }
+        }
+
+        // Resolve positive resolvers with "yes" and negative resolvers with "no"
+        positiveResolver('yes');
+        negativeResolvers.forEach(resolver => resolver('no'));
+
+        await Promise.all(allPromises);
+
+        assert.lengthOf(positiveResults, 1);
+        assert.isTrue(positiveResults.every(result => result === 'yes'));
+
+        assert.lengthOf(negativeResults, baseNames.length - 1);
+        assert.isTrue(negativeResults.every(result => result === 'no'));
+      });
+    });
+  });
 });
