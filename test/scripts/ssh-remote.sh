7f7bb0a38bae723d4faed92e9425e42129ca22ed
Ash Wilson <smashwilson@github.com>
2017-05-02 11:34:53 -0400

    Consume the "0000" reply from git

--- test/scripts/ssh-remote.sh
+++ test/scripts/ssh-remote.sh
@@ -9,3 +9,6 @@ fi
 printf '005a66d11860af6d28eb38349ef83de475597cb0e8b4 HEAD\0multi_ack symref=HEAD:refs/heads/master\n'
 printf '003f66d11860af6d28eb38349ef83de475597cb0e8b4 refs/heads/master\n'
 printf '0000'
+
+# Consume the git process' 0000 response
+read || true
