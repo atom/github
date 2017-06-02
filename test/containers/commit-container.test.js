e5fd369d025e53ce15d26eded4ac8e8fad6125e1
Ahmed Tajelsir <ah.tajelsir@gmail.com>
2017-06-03 00:15:03 +0300

    More tests and fix eslint warnings

--- test/containers/commit-container.test.js
+++ test/containers/commit-container.test.js
@@ -9,26 +9,26 @@ describe('CommitContainer', function() {
       author: {
         name: 'author_name', avatarUrl: '',
         user: {
-          login: 'author_login'
-        }
+          login: 'author_login',
+        },
       },
       committer: {
         name: 'committer_name', avatarUrl: '',
         user: {
-          login: 'committer_login'
-        }
+          login: 'committer_login',
+        },
       },
-      oid: 'ff22',
+      oid: 'e6c80aa37dc6f7a5e5491e0ed6e00ec2c812b1a5',
       message: 'commit message',
       messageHeadlineHTML: '<h1>html</h1>',
     };
     const app = <Commit item={item} />;
     const instance = shallow(app);
     assert.isTrue(
-      instance.containsMatchingElement(<img title='author_login' />)
+      instance.containsMatchingElement(<img title="author_login" />),
     );
     assert.isTrue(
-      instance.containsMatchingElement(<img title='committer_login' />)
+      instance.containsMatchingElement(<img title="committer_login" />),
     );
   });
 
@@ -42,7 +42,7 @@ describe('CommitContainer', function() {
         name: 'committer_name', avatarUrl: '',
         user: null,
       },
-      oid: 'ff22',
+      oid: 'e6c80aa37dc6f7a5e5491e0ed6e00ec2c812b1a5',
       authoredByCommitter: false,
       message: 'commit message',
       messageHeadlineHTML: '<h1>html</h1>',
@@ -50,14 +50,14 @@ describe('CommitContainer', function() {
     const app = <Commit item={item} />;
     const instance = shallow(app);
     assert.isTrue(
-      instance.containsMatchingElement(<img title='author_name' />)
+      instance.containsMatchingElement(<img title="author_name" />),
     );
     assert.isTrue(
-      instance.containsMatchingElement(<img title='committer_name' />)
+      instance.containsMatchingElement(<img title="committer_name" />),
     );
   });
 
-  it('ignores committer when it authored by the same person', function(){
+  it('ignores committer when it authored by the same person', function() {
     const item = {
       author: {
         name: 'author_name', avatarUrl: '',
@@ -69,7 +69,7 @@ describe('CommitContainer', function() {
         name: 'author_name', avatarUrl: '',
         user: null,
       },
-      oid: 'ff22',
+      oid: 'e6c80aa37dc6f7a5e5491e0ed6e00ec2c812b1a5',
       authoredByCommitter: true,
       message: 'commit message',
       messageHeadlineHTML: '<h1>html</h1>',
@@ -77,14 +77,14 @@ describe('CommitContainer', function() {
     const app = <Commit item={item} />;
     const instance = shallow(app);
     assert.isTrue(
-      instance.containsMatchingElement(<img title='author_login' />)
+      instance.containsMatchingElement(<img title="author_login" />),
     );
     assert.isFalse(
-      instance.containsMatchingElement(<img title='committer_name' />)
+      instance.containsMatchingElement(<img title="committer_name" />),
     );
   });
 
-  it('ignores committer when it authored by GitHub', function(){
+  it('ignores committer when it authored by GitHub', function() {
     const item = {
       author: {
         name: 'author_name', avatarUrl: '',
@@ -96,7 +96,7 @@ describe('CommitContainer', function() {
         name: 'GitHub', avatarUrl: '',
         user: null,
       },
-      oid: 'ff22',
+      oid: 'e6c80aa37dc6f7a5e5491e0ed6e00ec2c812b1a5',
       authoredByCommitter: false,
       message: 'commit message',
       messageHeadlineHTML: '<h1>html</h1>',
@@ -104,14 +104,43 @@ describe('CommitContainer', function() {
     const app = <Commit item={item} />;
     const instance = shallow(app);
     assert.isTrue(
-      instance.containsMatchingElement(<img title='author_login' />)
+      instance.containsMatchingElement(<img title="author_login" />),
     );
     assert.isFalse(
-      instance.containsMatchingElement(<img title='GitHub' />)
+      instance.containsMatchingElement(<img title="GitHub" />),
     );
   });
 
-  it('shows the full commit message as tooltip', function(){
+  it('renders avatar URLs', function() {
+    const item = {
+      author: {
+        name: 'author_name', avatarUrl: 'URL1',
+        user: {
+          login: 'author_login',
+        },
+      },
+      committer: {
+        name: 'GitHub', avatarUrl: 'URL2',
+        user: {
+          login: 'committer_login',
+        },
+      },
+      oid: 'e6c80aa37dc6f7a5e5491e0ed6e00ec2c812b1a5',
+      authoredByCommitter: false,
+      message: 'commit message',
+      messageHeadlineHTML: '<h1>html</h1>',
+    };
+    const app = <Commit item={item} />;
+    const instance = shallow(app);
+    assert.isTrue(
+      instance.containsMatchingElement(<img src="URL1" />),
+    );
+    assert.isTrue(
+      instance.containsMatchingElement(<img src="URL2" />),
+    );
+  });
+
+  it('shows the full commit message as tooltip', function() {
     const item = {
       author: {
         name: 'author_name', avatarUrl: '',
@@ -123,7 +152,7 @@ describe('CommitContainer', function() {
         name: 'author_name', avatarUrl: '',
         user: null,
       },
-      oid: 'ff22',
+      oid: 'e6c80aa37dc6f7a5e5491e0ed6e00ec2c812b1a5',
       authoredByCommitter: true,
       message: 'full message',
       messageHeadlineHTML: '<h1>html</h1>',
@@ -131,11 +160,11 @@ describe('CommitContainer', function() {
     const app = <Commit item={item} />;
     const instance = shallow(app);
     assert.isTrue(
-      instance.containsMatchingElement(<span title='full message' />)
+      instance.containsMatchingElement(<span title="full message" />),
     );
   });
 
-  it('renders commit message headline', function(){
+  it('renders commit message headline', function() {
     const item = {
       author: {
         name: 'author_name', avatarUrl: '',
@@ -147,7 +176,7 @@ describe('CommitContainer', function() {
         name: 'author_name', avatarUrl: '',
         user: null,
       },
-      oid: 'ff22',
+      oid: 'e6c80aa37dc6f7a5e5491e0ed6e00ec2c812b1a5',
       authoredByCommitter: true,
       message: 'full message',
       messageHeadlineHTML: '<h1>inner HTML</h1>',
@@ -157,4 +186,25 @@ describe('CommitContainer', function() {
     assert.match(instance.html(), /<h1>inner HTML<\/h1>/);
   });
 
+  it('renders commit sha', function() {
+    const item = {
+      author: {
+        name: 'author_name', avatarUrl: '',
+        user: {
+          login: 'author_login',
+        },
+      },
+      committer: {
+        name: 'author_name', avatarUrl: '',
+        user: null,
+      },
+      oid: 'e6c80aa37dc6f7a5e5491e0ed6e00ec2c812b1a5',
+      authoredByCommitter: true,
+      message: 'full message',
+      messageHeadlineHTML: '<h1>inner HTML</h1>',
+    };
+    const app = <Commit item={item} />;
+    const instance = shallow(app);
+    assert.match(instance.text(), /e6c80aa3/);
+  });
 });
