46f4f782073e7a9f730c6f40c35cbd86678936e7
Ash Wilson <smashwilson@github.com>
2017-06-08 14:12:30 -0400

    Reorganize function calls

--- bin/gpg-pinentry.js
+++ bin/gpg-pinentry.js
@@ -14,58 +14,41 @@ const INFO = {
   pid: process.pid,
 };
 
-/*
- * Emit diagnostic messages to stderr if GIT_TRACE is set to a non-empty value.
- */
-function log(message) {
-  if (!diagnosticsEnabled) {
-    return;
+async function main() {
+  let exitCode = 1;
+  try {
+    process.stdout.write('OK Your orders please\n');
+    exitCode = await parseStdin();
+  } catch (err) {
+    log(`Failed with error:\n${err}`);
+  } finally {
+    await cleanup();
+    process.exit(exitCode);
   }
-
-  if (!logStream) {
-    const logFile = path.join(process.env.ATOM_GITHUB_TMP, 'gpg-pinentry.log');
-    logStream = fs.createWriteStream(logFile, {defaultEncoding: 'utf8'});
-  }
-
-  logStream.write(message + '\n');
 }
 
-/*
- * Request a dialog in Atom by writing a null-delimited JSON query to the socket.
- */
-function dialog(state) {
-  const payload = {
-    error: state.error,
-    prompt: state.prompt,
-    includeUsername: false,
-    pid: process.pid,
-  };
-
+function parseStdin() {
   return new Promise((resolve, reject) => {
-    log('Requesting dialog through Atom socket.');
+    const rl = readline.createInterface({input: process.stdin});
+    const state = {
+      desc: 'Please enter the passphrase for your default GPG signing key.',
+      error: null,
+      exit: null,
+    };
 
-    const socket = net.connect(socketPath, () => {
-      log('Connection established.');
-
-      const parts = [];
-
-      socket.on('data', data => parts.push(data));
-      socket.on('end', () => {
-        log('Atom socket stream terminated.');
-        try {
-          const reply = JSON.parse(parts.join(''));
-          resolve(reply.password);
-        } catch (e) {
-          log(`Unable to parse reply from Atom:\n${parts.join('')}`);
-          reject(e);
-        }
-      });
-
-      log('Sending payload.');
-      socket.write(JSON.stringify(payload) + '\u0000', 'utf8');
-      log('Payload sent.');
+    rl.on('line', async line => {
+      Object.assign(state, await handleCommand(line, state));
+      if (state.exit !== null) {
+        resolve(state.exit);
+      }
     });
-    socket.setEncoding('utf8');
+
+    rl.on('close', () => {
+      if (state.exit !== null) {
+        state.exit = 1;
+        resolve(state.exit);
+      }
+    });
   });
 }
 
@@ -125,47 +108,64 @@ function handleCommand(line, state) {
   return handler(param, state);
 }
 
-function parseStdin() {
+/*
+ * Request a dialog in Atom by writing a null-delimited JSON query to the socket.
+ */
+function dialog(state) {
+  const payload = {
+    error: state.error,
+    prompt: state.prompt,
+    includeUsername: false,
+    pid: process.pid,
+  };
+
   return new Promise((resolve, reject) => {
-    const rl = readline.createInterface({input: process.stdin});
-    const state = {
-      desc: 'Please enter the passphrase for your default GPG signing key.',
-      error: null,
-      exit: null,
-    };
+    log('Requesting dialog through Atom socket.');
 
-    rl.on('line', async line => {
-      Object.assign(state, await handleCommand(line, state));
-      if (state.exit !== null) {
-        resolve(state.exit);
-      }
-    });
+    const socket = net.connect(socketPath, () => {
+      log('Connection established.');
 
-    rl.on('close', () => {
-      if (state.exit !== null) {
-        state.exit = 1;
-        resolve(state.exit);
-      }
+      const parts = [];
+
+      socket.on('data', data => parts.push(data));
+      socket.on('end', () => {
+        log('Atom socket stream terminated.');
+        try {
+          const reply = JSON.parse(parts.join(''));
+          resolve(reply.password);
+        } catch (e) {
+          log(`Unable to parse reply from Atom:\n${parts.join('')}`);
+          reject(e);
+        }
+      });
+
+      log('Sending payload.');
+      socket.write(JSON.stringify(payload) + '\u0000', 'utf8');
+      log('Payload sent.');
     });
+    socket.setEncoding('utf8');
   });
+}
+
+/*
+ * Emit diagnostic messages to stderr if GIT_TRACE is set to a non-empty value.
+ */
+function log(message) {
+  if (!diagnosticsEnabled) {
+    return;
+  }
+
+  if (!logStream) {
+    const logFile = path.join(process.env.ATOM_GITHUB_TMP, 'gpg-pinentry.log');
+    logStream = fs.createWriteStream(logFile, {defaultEncoding: 'utf8'});
+  }
+
+  logStream.write(message + '\n');
 }
 
 async function cleanup() {
   if (logStream) {
     await new Promise(resolve => logStream.end('\n', 'utf8', resolve));
-  }
-}
-
-async function main() {
-  let exitCode = 1;
-  try {
-    process.stdout.write('OK Your orders please\n');
-    exitCode = await parseStdin();
-  } catch (err) {
-    log(`Failed with error:\n${err}`);
-  } finally {
-    await cleanup();
-    process.exit(exitCode);
   }
 }
 
