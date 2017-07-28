a809346e9678d6e64c90c164d6d98f7696952172
Ash Wilson <smashwilson@github.com>
2017-07-27 23:20:38 -0700

    :fire: console.log

--- test/runner.js
+++ test/runner.js
@@ -5,8 +5,6 @@ import path from 'path';
 
 import until from 'test-until';
 
-console.log('Are we even getting here');
-
 chai.use(chaiAsPromised);
 global.assert = chai.assert;
 
@@ -25,15 +23,10 @@ module.exports = createRunner({
   reporter: process.env.MOCHA_REPORTER || 'spec',
   overrideTestPaths: [/spec$/, /test/],
 }, mocha => {
-  console.log('Configuring mocha runner');
   mocha.timeout(parseInt(process.env.MOCHA_TIMEOUT || '5000', 10));
   if (process.env.APPVEYOR_API_URL) {
-    console.log('AppVeyor detected?');
     mocha.reporter(require('mocha-appveyor-reporter'));
-  }
-
-  if (process.env.CIRCLECI === 'true') {
-    console.log('CircleCI detected.');
+  } else if (process.env.CIRCLECI === 'true') {
     mocha.reporter(require('mocha-junit-and-console-reporter'), {
       mochaFile: path.join(process.env.CIRCLE_TEST_REPORTS, 'mocha', 'test-results.xml'),
     });
