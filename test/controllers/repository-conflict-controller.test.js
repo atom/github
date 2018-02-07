8199d6581faaef7d39fca039304c84bcf27fd96b
Ash Wilson <smashwilson@github.com>
2018-02-07 15:15:31 -0500

    Unit test to ensure ResolutionConflictController can be disabled

--- test/controllers/repository-conflict-controller.test.js
+++ test/controllers/repository-conflict-controller.test.js
@@ -67,4 +67,26 @@ describe('RepositoryConflictController', () => {
       await assert.async.equal(wrapper.find(EditorConflictController).length, 2);
     });
   });
+
+  describe('with the configuration option disabled', function() {
+    beforeEach(function() {
+      atomEnv.config.set('github.graphicalConflictResolution', false);
+    });
+
+    it('renders no children', async function() {
+      const workdirPath = await cloneRepository('merge-conflict');
+      const repository = await buildRepository(workdirPath);
+
+      await assert.isRejected(repository.git.merge('origin/branch'));
+
+      await Promise.all(['modified-on-both-ours.txt', 'modified-on-both-theirs.txt'].map(basename => {
+        return workspace.open(path.join(workdirPath, basename));
+      }));
+
+      app = React.cloneElement(app, {repository});
+      const wrapper = mount(app);
+
+      await assert.async.lengthOf(wrapper.find(EditorConflictController), 0);
+    });
+  });
 });
