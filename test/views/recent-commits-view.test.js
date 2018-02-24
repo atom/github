5412f7202e77e065cc085410bbe25eadf037e3dd
Katrina Uychaco <kuychaco@gmail.com>
2018-02-23 18:08:23 -0800

    Stub recent commits component hierarchy and add tests

---
+++ test/views/recent-commits-view.test.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+
+import RecentCommitsView from '../../lib/views/recent-commits-view';
+
+describe('RecentCommitsView', function() {
+  let app;
+
+  beforeEach(function() {
+    app = <RecentCommitsView commits={[]} isLoading={false} />;
+  });
+
+  it('shows a placeholder while commits are empty and loading', function() {
+    app = React.cloneElement(app, {commits: [], isLoading: true});
+    const wrapper = shallow(app);
+
+    assert.isFalse(wrapper.find('RecentCommitView').exists());
+    assert.strictEqual(wrapper.find('.github-RecentCommit-message').text(), 'Recent commits');
+  });
+
+  it('shows a prompting message while commits are empty and not loading', function() {
+    app = React.cloneElement(app, {commits: [], isLoading: false});
+    const wrapper = shallow(app);
+
+    assert.isFalse(wrapper.find('RecentCommitView').exists());
+    assert.strictEqual(wrapper.find('.github-RecentCommit-message').text(), 'Make your first commit');
+  });
+
+  it('renders a RecentCommitView for each commit', function() {
+    const commits = ['1', '2', '3'].map(sha => {
+      return {
+        getSha() { return sha; },
+      };
+    });
+
+    app = React.cloneElement(app, {commits});
+    const wrapper = shallow(app);
+
+    assert.deepEqual(wrapper.find('RecentCommitView').map(w => w.prop('commit')), commits);
+  });
+
+
+});
