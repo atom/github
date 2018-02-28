ea46fb3f76b23c13447bcfd8f9b9917d388c5a47
Katrina Uychaco <kuychaco@gmail.com>
2018-02-28 11:59:44 -0800

    WIP test

--- test/views/recent-commits-view.test.js
+++ test/views/recent-commits-view.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import {shallow} from 'enzyme';
+import {shallow, mount} from 'enzyme';
 
 import RecentCommitsView from '../../lib/views/recent-commits-view';
+import Commit from '../../lib/models/commit';
 
-describe('RecentCommitsView', function() {
+describe.only('RecentCommitsView', function() {
   let app;
 
   beforeEach(function() {
@@ -39,5 +40,20 @@ describe('RecentCommitsView', function() {
     assert.deepEqual(wrapper.find('RecentCommitView').map(w => w.prop('commit')), commits);
   });
 
-  it('renders an avatar corresponding to the GitHub user who authored the commit');
+  it.only('renders an avatar corresponding to the GitHub user who authored the commit', function() {
+    const commits = ['one@x.com', 'two@y.com', 'thr&ee@z.com'].map(authorEmail => {
+      return new Commit({sha: '1111111111', authorEmail, authorDate: 0, message: 'x'});
+    });
+
+    app = React.cloneElement(app, {commits});
+    const wrapper = mount(app);
+    assert.deepEqual(
+      wrapper.find('img.github-RecentCommit-avatar').map(w => w.prop('src')),
+      [
+        'https://avatars.githubusercontent.com/u/e?email=thr%26ee%40z.com',
+        'https://avatars.githubusercontent.com/u/e?email=two%40y.com',
+        'https://avatars.githubusercontent.com/u/e?email=one%40x.com',
+      ],
+    );
+  });
 });
