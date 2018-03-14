59ed70709d9f6318b36a8217d8b76dd073afab43
Katrina Uychaco <kuychaco@gmail.com>
2018-03-14 23:11:02 +0100

    Add basic UserStore

--- test/models/user-store.test.js
+++ test/models/user-store.test.js
@@ -1,108 +1,53 @@
 import UserStore from '../../lib/models/user-store';
 
+import {cloneRepository, buildRepository} from '../helpers';
+
 describe('UserStore', function() {
   describe('populate', function() {
     it('loads store with users in repo', async function() {
-      const repo = await buildRepository('three-files');
-      const store = new UserStore({repository: repo});
+      const workdirPath = await cloneRepository('multiple-commits');
+      const repository = await buildRepository(workdirPath);
+      const store = new UserStore({repository});
+
+      assert.deepEqual(store.getUsers(), []);
+
       await store.populate();
-    })
-  })
+      const users = store.getUsers();
+      assert.deepEqual(users, [
+        {
+          email: 'kuychaco@github.com',
+          name: 'Katrina Uychaco',
+        },
+      ]);
+    });
+  });
 
   describe('addUsers', function() {
-    it('adds specified user', function() {
+    it('adds specified user', async function() {
+      const workdirPath = await cloneRepository('multiple-commits');
+      const repository = await buildRepository(workdirPath);
+      const store = new UserStore({repository});
 
-    })
-  })
-  it('', async function() {
-    const rawUsers = {
-      'kuychaco@github.com': 'Katrina Uychaco',
-      'smashwilson@github.com': 'Ash Wilson'
-    }
+      await store.populate();
+      store.addUsers({
+        'mona@lisa.com': 'Mona Lisa',
+        'hubot@github.com': 'Hubot Robot',
+      });
 
-
-    const store = new UserStore()
-    store.addUsers(rawUsers)
-    const users = store.getUsers()
-    assert.deepEqual(users, {
-      'kuychaco@github.com': {
-        name: 'Katrina Uychaco',
-        emails: ['kuychaco@github.com'], // for mailmap support
-        github: {
-          handle: '@kuychaco',
-          avatarUrl: 'githubusercontent...'
-        }
-      }
-    })
-  })
-})
-
-/*
-
-const repo = await buildRepository('three-files');
-const store = new UserStore({repository: repo});
-await store.populate();
-
-// User:
-
-class User {
-  constructor ({email, fullname, githubLogin}) {
-    //
-  }
-
-  isGitHubUser() {
-    return !!this.githubLogin;
-  }
-
-  displayName() {
-    if (this.githubLogin) {
-      return `@${this.githubLogin}`
-    } else {
-      return `${this.name} <${this.email}>`
-    }
-  }
-
-  coAuthorTrailer() {
-    return `Co-authored-by: ${this.email}`
-  }
-}
-
-// UserStore:
-
-class UserStore {
-  constructor ({repository}) {
-    this.repository = repository
-  }
-
-  populate () {
-    this.repository.onDidChangeState(({oldState, newState}) => {
-      if (!oldState.isLoading() && newState.isPresent()) {
-        this.kickOffLocalShit();
-      }
-    }
-    })
-  }
-
-  addUsersFromGraphQL(response) {
-    // gets called in query renderer callback
-    this.addMentionable();
-  }
-
-  async kickOffGraphQLShit() {
-    // Find GitHub repo(s) from remotes
-    // Hit GraphQL API
-    this.addMentionable(username);
-  }
-
-  async kickOffLocalShit() {
-    // Run git commands to get authors/committers
-    this.addMentionable(username);
-  }
-
-  mentionableUsers() {
-    // returns [User]
-  }
-}
-}
-
-*/
+      assert.deepEqual(store.getUsers(), [
+        {
+          name: 'Hubot Robot',
+          email: 'hubot@github.com',
+        },
+        {
+          name: 'Katrina Uychaco',
+          email: 'kuychaco@github.com',
+        },
+        {
+          name: 'Mona Lisa',
+          email: 'mona@lisa.com',
+        },
+      ]);
+    });
+  });
+});
