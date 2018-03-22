8a6ca824d02812efb771f631d4f0c63c16942e99
Katrina Uychaco <kuychaco@gmail.com>
2018-03-21 17:34:30 -0700

    Add User Store test for refetching data when HEAD changes
    Co-Authored-By: Tilde Ann Thurium <annthurium@github.com>

--- test/models/user-store.test.js
+++ test/models/user-store.test.js
@@ -1,3 +1,5 @@
+import dedent from 'dedent-js'
+
 import UserStore from '../../lib/models/user-store';
 
 import {cloneRepository, buildRepository} from '../helpers';
@@ -20,7 +22,7 @@ describe('UserStore', function() {
   });
 
   describe('addUsers', function() {
-    it('adds specified user', async function() {
+    it('adds specified users and does not overwrite existing users', async function() {
       const workdirPath = await cloneRepository('multiple-commits');
       const repository = await buildRepository(workdirPath);
       const store = new UserStore({repository});
@@ -47,5 +49,40 @@ describe('UserStore', function() {
         },
       ]);
     });
+  });
+
+  it('refetches users when HEAD changes', async function() {
+    const workdirPath = await cloneRepository('multiple-commits');
+    const repository = await buildRepository(workdirPath);
+    await repository.checkout('new-branch', {createNew: true});
+    await repository.commit('commit 1', {allowEmpty: true});
+    await repository.commit('commit 2', {allowEmpty: true});
+    await repository.checkout('master');
+
+    const store = new UserStore({repository});
+    await assert.async.deepEqual(store.getUsers(), [
+      {
+        email: 'kuychaco@github.com',
+        name: 'Katrina Uychaco',
+      },
+    ]);
+
+    sinon.spy(store, 'addUsers')
+
+    // Head changes due to new commit
+    await repository.commit(dedent`
+      New commit
+
+      Co-authored-by: New Author <new-author@email.com>
+    `, {allowEmpty: true});
+
+    await assert.async.equal(store.addUsers.callCount, 1)
+    assert.isOk(store.getUsers().find(user => {
+      return user.name === 'New Author' && user.email === 'new-author@email.com';
+    }));
+
+    // Change head due to branch checkout
+    await repository.checkout('new-branch')
+    await assert.async.equal(store.addUsers.callCount, 2)
   });
 });
