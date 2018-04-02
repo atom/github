b4a695906ab66564cebf12b8e0db14c3503e9667
Katrina Uychaco <kuychaco@gmail.com>
2018-04-02 16:37:53 -0700

    Implement CoAuthorDialog and allow users to add new authors
    Co-authored-by: Tilde Ann Thurium <annthurium@github.com>

---
+++ test/views/co-author-dialog.test.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import {mount} from 'enzyme';
+
+import CoAuthorDialog from '../../lib/views/co-author-dialog';
+
+describe('CoAuthorDialog', function() {
+  let atomEnv;
+  let app, wrapper, didSubmit, didCancel;
+
+  beforeEach(function() {
+    atomEnv = global.buildAtomEnvironment();
+
+    didSubmit = sinon.stub();
+    didCancel = sinon.stub();
+
+    app = (
+      <CoAuthorDialog
+        commandRegistry={atomEnv.commands}
+        onSubmit={didSubmit}
+        onCancel={didCancel}
+      />
+    );
+  });
+
+  afterEach(function() {
+    atomEnv.destroy();
+  });
+
+  const setTextIn = function(selector, text) {
+    wrapper.find(selector).simulate('change', {target: {value: text}});
+  };
+
+  describe('confirm', function() {
+    it('submits current co author name and email', function() {
+      wrapper = mount(app);
+      const name = 'Coauthor Name';
+      const email = 'foo@bar.com';
+
+      setTextIn('.github-CoAuthorDialog-Name', name);
+      setTextIn('.github-CoAuthorDialog-Email', email);
+
+
+      wrapper.find('.btn-primary').simulate('click');
+
+      assert.deepEqual(didSubmit.firstCall.args[0], {
+        name,
+        email,
+      });
+    });
+  });
+
+  describe('cancel', function() {
+    it('calls cancel prop when cancel is clicked', function() {
+      wrapper = mount(app);
+      wrapper.find('.github-CancelButton').simulate('click');
+      assert.isTrue(didCancel.called);
+    });
+
+    it('calls cancel prop when `core:cancel` is triggered', function() {
+      wrapper = mount(app);
+      atomEnv.commands.dispatch(wrapper.find('.github-NewCoAuthor').getNode(), 'core:cancel');
+      assert.isTrue(didCancel.called);
+    });
+  });
+});
