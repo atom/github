bf0501f98b7edfb82b2bcdd9fffd2423506822e0
Ash Wilson <smashwilson@github.com>
2018-04-03 09:54:06 -0400

    Tests for an unpublished branch

--- test/containers/pr-selection-by-branch-container.test.js
+++ test/containers/pr-selection-by-branch-container.test.js
@@ -4,17 +4,23 @@ import {shallow} from 'enzyme';
 import {PrSelectionByBranch} from '../../lib/containers/pr-selection-by-branch-container';
 
 describe('PrSelectionByBranch', function() {
-  let onSelectPr, onUnpinPr;
+  let onSelectPr, onUnpinPr, onPushBranch, onCreatePr;
   let app;
 
   beforeEach(function() {
     onSelectPr = sinon.spy();
     onUnpinPr = sinon.spy();
+    onPushBranch = sinon.spy();
+    onCreatePr = sinon.spy();
 
     app = (
       <PrSelectionByBranch
         onSelectPr={onSelectPr}
         onUnpinPr={onUnpinPr}
+        onPushBranch={onPushBranch}
+        onCreatePr={onCreatePr}
+        aheadCount={0}
+        isUnpublished={true}
       />
     );
   });
@@ -44,9 +50,37 @@ describe('PrSelectionByBranch', function() {
     });
 
     describe('with no remote tracking branch', function() {
-      it('shows a button to publish your branch');
+      beforeEach(function() {
+        app = React.cloneElement(app, {
+          isUnpublished: true,
+        });
+      });
 
-      it('disables a button to open a new pull request');
+      it('shows a button to publish your branch', function() {
+        const wrapper = shallow(app);
+
+        const button = wrapper.find('.github-PrSelectionByBranch-push');
+        assert.isTrue(button.exists());
+        button.simulate('click');
+        assert.isTrue(onPushBranch.called);
+      });
+
+      it('shows a link to search again', function() {
+        this.skip();
+        const wrapper = shallow(app);
+
+        const link = wrapper.find('.github-PrSelectionByBranch-searchAgain');
+        assert.isTrue(link.exists());
+      });
+
+      it('disables a button to open a new pull request', function() {
+        const wrapper = shallow(app);
+
+        const button = wrapper.find('.github-PrSelectionByBranch-createPr');
+        assert.isTrue(button.exists());
+        assert.isTrue(button.prop('disabled'));
+        button.simulate('click');
+      });
     });
 
     describe('with unpushed commits', function() {
