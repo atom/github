8c042a4fe89fde875f1b168dc5c2b624f94c54df
Ash Wilson <smashwilson@github.com>
2018-04-10 10:58:19 -0400

    Query the BranchSet for our upstream status

--- test/containers/pr-selection-by-branch-container.test.js
+++ test/containers/pr-selection-by-branch-container.test.js
@@ -3,13 +3,17 @@ import {shallow} from 'enzyme';
 
 import Remote from '../../lib/models/remote';
 import Branch, {nullBranch} from '../../lib/models/branch';
+import BranchSet from '../../lib/models/branch-set';
 import {PrSelectionByBranch} from '../../lib/containers/pr-selection-by-branch-container';
 
 describe('PrSelectionByBranch', function() {
-  let onSelectPr, onUnpinPr, onCreatePr, onSearchAgain;
+  let branches, remote, onSelectPr, onUnpinPr, onCreatePr, onSearchAgain;
   let app;
 
   beforeEach(function() {
+    branches = new BranchSet();
+    remote = new Remote('origin', 'git@github.com:atom/github.git');
+
     onSelectPr = sinon.spy();
     onUnpinPr = sinon.spy();
     onCreatePr = sinon.spy();
@@ -21,13 +25,41 @@ describe('PrSelectionByBranch', function() {
         onUnpinPr={onUnpinPr}
         onCreatePr={onCreatePr}
         onSearchAgain={onSearchAgain}
-        remote={new Remote('origin', 'git@github.com:atom/github.git')}
+        remote={remote}
         aheadCount={null}
+        branches={branches}
         upstreamBranch={nullBranch}
         pushInProgress={false}
       />
     );
   });
+
+  // Populate the `branches` prop with a set of Branches that triggers the happy path: HEAD on a feature branch that
+  // has been moved from the repository's main branch.
+  function setUpFeatureBranch() {
+    const mainUpstream = Branch.createRemoteTracking('refs/remotes/origin/master', 'origin', 'refs/heads/master');
+    const main = new Branch(
+      'master',
+      mainUpstream,
+      mainUpstream,
+      false,
+      {sha: 'ac133c710d2f789c36799bddffe88b10551c6484'},
+    );
+    branches.add(main);
+    const featureUpstream = Branch.createRemoteTracking(
+      'refs/remotes/origin/feature',
+      'origin',
+      'refs/heads/feature',
+    );
+    const feature = new Branch(
+      'feature',
+      featureUpstream,
+      featureUpstream,
+      true,
+      {sha: 'ece5f33141b84077cbd68bfa09283d73d18433e5'},
+    );
+    branches.add(feature);
+  }
 
   describe('with no pull request', function() {
     beforeEach(function() {
@@ -79,10 +111,8 @@ describe('PrSelectionByBranch', function() {
 
     describe('with unpushed commits', function() {
       beforeEach(function() {
-        app = React.cloneElement(app, {
-          aheadCount: 3,
-          upstreamBranch: new Branch('feature'),
-        });
+        setUpFeatureBranch();
+        app = React.cloneElement(app, {aheadCount: 3});
       });
 
       it('shows a button to push your commits and create a PR', function() {
@@ -98,9 +128,9 @@ describe('PrSelectionByBranch', function() {
 
     describe('while pushing is in progress', function() {
       beforeEach(function() {
+        setUpFeatureBranch();
         app = React.cloneElement(app, {
           aheadCount: 3,
-          upstreamBranch: new Branch('feature'),
           pushInProgress: true,
         });
       });
@@ -116,10 +146,8 @@ describe('PrSelectionByBranch', function() {
 
     describe('when fully pushed', function() {
       beforeEach(function() {
-        app = React.cloneElement(app, {
-          aheadCount: 0,
-          upstreamBranch: new Branch('origin/feature'),
-        });
+        setUpFeatureBranch();
+        app = React.cloneElement(app, {aheadCount: 0});
       });
 
       it('shows a button to open a new pull request', function() {
@@ -157,6 +185,51 @@ describe('PrSelectionByBranch', function() {
       it('prompts you to create a branch', function() {
         const wrapper = shallow(app);
         assert.strictEqual(wrapper.find('.github-CreatePr strong').text(), 'Create a new branch');
+      });
+    });
+
+    describe('when just branched from the main branch', function() {
+      beforeEach(function() {
+        // "main" and "feature" are still on the same SHA
+        const main = new Branch(
+          'main',
+          Branch.createRemoteTracking('refs/remotes/elsewhere/main', 'elsewhere', 'refs/heads/develop'),
+          Branch.createRemoteTracking('refs/remotes/origin/main', 'origin', 'refs/heads/develop'),
+          false,
+          {sha: 'ece5f33141b84077cbd68bfa09283d73d18433e5'},
+        );
+        branches.add(main);
+
+        const feature = new Branch(
+          'feature',
+          nullBranch,
+          nullBranch,
+          true,
+          {sha: 'ece5f33141b84077cbd68bfa09283d73d18433e5'},
+        );
+        branches.add(feature);
+
+        app = React.cloneElement(app, {
+          repository: {
+            defaultBranchRef: {
+              name: 'develop',
+            },
+            pullRequests: {
+              totalCount: 0,
+              edges: [],
+            },
+          },
+        });
+      });
+
+      it('does not show the new pull request button', function() {
+        const wrapper = shallow(app);
+        assert.isFalse(wrapper.find('.github-PrSelectionByBranch-createPr').exists());
+      });
+
+      it('prompts you to commit', function() {
+        const wrapper = shallow(app);
+        assert.strictEqual(wrapper.find('.github-CreatePr strong').text(), 'Make some commits');
       });
     });
   });
