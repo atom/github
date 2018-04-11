388d8dd66aa2b68fa80469ff6891ebebad91c42e
Ash Wilson <smashwilson@github.com>
2018-04-11 15:30:49 -0400

    CommitView :white_check_mark:

--- test/views/commit-view.test.js
+++ test/views/commit-view.test.js
@@ -62,39 +62,38 @@ describe('CommitView', function() {
 
   it('displays the remaining characters limit based on which line is being edited', function() {
     const wrapper = mount(app);
-    const remaining = wrapper.find('.github-CommitView-remaining-characters');
-    assert.strictEqual(remaining.text(), '72');
+    assert.strictEqual(wrapper.find('.github-CommitView-remaining-characters').text(), '72');
 
     wrapper.setProps({message: 'abcde fghij'});
-    assert.strictEqual(remaining.text(), '61');
-    assert.isFalse(remaining.hasClass('is-error'));
-    assert.isFalse(remaining.hasClass('is-warning'));
+    assert.strictEqual(wrapper.find('.github-CommitView-remaining-characters').text(), '61');
+    assert.isFalse(wrapper.find('.github-CommitView-remaining-characters').hasClass('is-error'));
+    assert.isFalse(wrapper.find('.github-CommitView-remaining-characters').hasClass('is-warning'));
 
     wrapper.setProps({message: '\nklmno'});
-    assert.strictEqual(remaining.text(), '∞');
-    assert.isFalse(remaining.hasClass('is-error'));
-    assert.isFalse(remaining.hasClass('is-warning'));
+    assert.strictEqual(wrapper.find('.github-CommitView-remaining-characters').text(), '∞');
+    assert.isFalse(wrapper.find('.github-CommitView-remaining-characters').hasClass('is-error'));
+    assert.isFalse(wrapper.find('.github-CommitView-remaining-characters').hasClass('is-warning'));
 
     wrapper.setProps({message: 'abcde\npqrst'});
-    assert.strictEqual(remaining.text(), '∞');
-    assert.isFalse(remaining.hasClass('is-error'));
-    assert.isFalse(remaining.hasClass('is-warning'));
+    assert.strictEqual(wrapper.find('.github-CommitView-remaining-characters').text(), '∞');
+    assert.isFalse(wrapper.find('.github-CommitView-remaining-characters').hasClass('is-error'));
+    assert.isFalse(wrapper.find('.github-CommitView-remaining-characters').hasClass('is-warning'));
 
-    wrapper.find('atom-text-editor').getNode().getModel().setCursorBufferPosition([0, 3]);
+    wrapper.find('atom-text-editor').getDOMNode().getModel().setCursorBufferPosition([0, 3]);
     wrapper.update();
-    assert.strictEqual(remaining.text(), '67');
-    assert.isFalse(remaining.hasClass('is-error'));
-    assert.isFalse(remaining.hasClass('is-warning'));
+    assert.strictEqual(wrapper.find('.github-CommitView-remaining-characters').text(), '67');
+    assert.isFalse(wrapper.find('.github-CommitView-remaining-characters').hasClass('is-error'));
+    assert.isFalse(wrapper.find('.github-CommitView-remaining-characters').hasClass('is-warning'));
 
     wrapper.setProps({stagedChangesExist: true, maximumCharacterLimit: 50});
-    assert.strictEqual(remaining.text(), '45');
-    assert.isFalse(remaining.hasClass('is-error'));
-    assert.isFalse(remaining.hasClass('is-warning'));
+    assert.strictEqual(wrapper.find('.github-CommitView-remaining-characters').text(), '45');
+    assert.isFalse(wrapper.find('.github-CommitView-remaining-characters').hasClass('is-error'));
+    assert.isFalse(wrapper.find('.github-CommitView-remaining-characters').hasClass('is-warning'));
 
     wrapper.setProps({message: 'a'.repeat(41)});
-    assert.strictEqual(remaining.text(), '9');
-    assert.isFalse(remaining.hasClass('is-error'));
-    assert.isTrue(remaining.hasClass('is-warning'));
+    assert.strictEqual(wrapper.find('.github-CommitView-remaining-characters').text(), '9');
+    assert.isFalse(wrapper.find('.github-CommitView-remaining-characters').hasClass('is-error'));
+    assert.isTrue(wrapper.find('.github-CommitView-remaining-characters').hasClass('is-warning'));
 
     wrapper.setProps({message: 'a'.repeat(58)});
     assert.strictEqual(wrapper.find('.github-CommitView-remaining-characters').text(), '-8');
@@ -103,7 +102,7 @@ describe('CommitView', function() {
   });
 
   describe('the commit button', function() {
-    let wrapper, commitButton;
+    let wrapper;
 
     beforeEach(async function() {
       const workdirPath = await cloneRepository('three-files');
@@ -116,53 +115,51 @@ describe('CommitView', function() {
         message: 'something',
       });
       wrapper = mount(app);
-
-      commitButton = wrapper.find('.github-CommitView-commit');
     });
 
     it('is disabled when no changes are staged', function() {
       wrapper.setProps({stagedChangesExist: false});
-      assert.isTrue(commitButton.prop('disabled'));
+      assert.isTrue(wrapper.find('.github-CommitView-commit').prop('disabled'));
 
       wrapper.setProps({stagedChangesExist: true});
-      assert.isFalse(commitButton.prop('disabled'));
+      assert.isFalse(wrapper.find('.github-CommitView-commit').prop('disabled'));
     });
 
     it('is disabled when there are merge conflicts', function() {
       wrapper.setProps({mergeConflictsExist: true});
-      assert.isTrue(commitButton.prop('disabled'));
+      assert.isTrue(wrapper.find('.github-CommitView-commit').prop('disabled'));
 
       wrapper.setProps({mergeConflictsExist: false});
-      assert.isFalse(commitButton.prop('disabled'));
+      assert.isFalse(wrapper.find('.github-CommitView-commit').prop('disabled'));
     });
 
     it('is disabled when the commit message is empty', function() {
       wrapper.setProps({message: ''});
-      assert.isTrue(commitButton.prop('disabled'));
+      assert.isTrue(wrapper.find('.github-CommitView-commit').prop('disabled'));
 
       wrapper.setProps({message: 'Not empty'});
-      assert.isFalse(commitButton.prop('disabled'));
+      assert.isFalse(wrapper.find('.github-CommitView-commit').prop('disabled'));
     });
 
     it('displays the current branch name', function() {
       const currentBranch = new Branch('aw-do-the-stuff');
       wrapper.setProps({currentBranch});
-      assert.strictEqual(commitButton.text(), 'Commit to aw-do-the-stuff');
+      assert.strictEqual(wrapper.find('.github-CommitView-commit').text(), 'Commit to aw-do-the-stuff');
     });
 
     it('indicates when a commit will be detached', function() {
       const currentBranch = Branch.createDetached('master~3');
       wrapper.setProps({currentBranch});
-      assert.strictEqual(commitButton.text(), 'Create detached commit');
+      assert.strictEqual(wrapper.find('.github-CommitView-commit').text(), 'Create detached commit');
     });
 
     it('displays a progress message while committing', function() {
       wrapper.setState({showWorking: true});
-      assert.strictEqual(commitButton.text(), 'Working...');
+      assert.strictEqual(wrapper.find('.github-CommitView-commit').text(), 'Working...');
     });
 
     it('falls back to "commit" with no current branch', function() {
-      assert.strictEqual(commitButton.text(), 'Commit');
+      assert.strictEqual(wrapper.find('.github-CommitView-commit').text(), 'Commit');
     });
   });
 
@@ -177,16 +174,15 @@ describe('CommitView', function() {
       app = React.cloneElement(app, {stagedChangesExist: true, prepareToCommit, commit, message: 'Something'});
       wrapper = mount(app);
 
-      editorElement = wrapper.find('atom-text-editor').getNode();
+      editorElement = wrapper.find('atom-text-editor').getDOMNode();
       sinon.spy(editorElement, 'focus');
       editor = editorElement.getModel();
 
       // Perform an extra render to ensure the editor text is reflected in the commit button enablement.
       // The controller accomplishes this by re-rendering on Repository update.
-      wrapper.update();
+      wrapper.setProps({});
 
       commitButton = wrapper.find('.github-CommitView-commit');
-
       workspaceElement = atomEnv.views.getView(atomEnv.workspace);
     });
 
@@ -196,6 +192,7 @@ describe('CommitView', function() {
       });
 
       it('calls props.commit(message) when the commit button is clicked', async function() {
+        wrapper.update();
         commitButton.simulate('click');
 
         await assert.async.isTrue(commit.calledWith('Something'));
