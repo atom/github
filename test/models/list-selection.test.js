19e7f46f80cac44358134f91f5e221b7816028bf
Ash Wilson <smashwilson@github.com>
2018-04-25 09:24:38 -0400

    Make ListSelection immutable

--- test/models/list-selection.test.js
+++ test/models/list-selection.test.js
@@ -1,35 +1,33 @@
 import ListSelection from '../../lib/models/list-selection';
 import {assertEqualSets} from '../helpers';
 
-// This class is mostly tested via CompositeListSelection and
-// FilePatchSelection. This file contains unit tests that are more convenient to
-// write directly against this class.
+// This class is mostly tested via CompositeListSelection and FilePatchSelection. This file contains unit tests that are
+// more convenient to write directly against this class.
 describe('ListSelection', function() {
   describe('coalesce', function() {
     it('correctly handles adding and subtracting a single item (regression)', function() {
-      const selection = new ListSelection({items: ['a', 'b', 'c']});
-      selection.selectLastItem(true);
-      selection.coalesce();
+      let selection = new ListSelection({items: ['a', 'b', 'c']});
+      selection = selection.selectLastItem(true);
+      selection = selection.coalesce();
       assertEqualSets(selection.getSelectedItems(), new Set(['a', 'b', 'c']));
-      selection.addOrSubtractSelection('b');
-      selection.coalesce();
+      selection = selection.addOrSubtractSelection('b');
+      selection = selection.coalesce();
       assertEqualSets(selection.getSelectedItems(), new Set(['a', 'c']));
-      selection.addOrSubtractSelection('b');
-      global.debug = true;
-      selection.coalesce();
+      selection = selection.addOrSubtractSelection('b');
+      selection = selection.coalesce();
       assertEqualSets(selection.getSelectedItems(), new Set(['a', 'b', 'c']));
     });
   });
 
   describe('selectItem', () => {
     // https://github.com/atom/github/issues/467
-    it('selects an item when there are no selections', () => {
-      const selection = new ListSelection({items: ['a', 'b', 'c']});
-      selection.addOrSubtractSelection('a');
-      selection.coalesce();
-      assert.equal(selection.getSelectedItems().size, 0);
-      selection.selectItem('a', true);
-      assert.equal(selection.getSelectedItems().size, 1);
+    it.only('selects an item when there are no selections', () => {
+      let selection = new ListSelection({items: ['a', 'b', 'c']});
+      selection = selection.addOrSubtractSelection('a');
+      selection = selection.coalesce();
+      assert.strictEqual(selection.getSelectedItems().size, 0);
+      selection = selection.selectItem('a', true);
+      assert.strictEqual(selection.getSelectedItems().size, 1);
     });
   });
 });
