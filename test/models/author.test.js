19f9d9f24b864c51533cfd8023006224150d7a42
Ash Wilson <smashwilson@github.com>
2018-05-22 10:02:33 -0400

    Sort and match Authors

--- test/models/author.test.js
+++ test/models/author.test.js
@@ -1,4 +1,4 @@
-import Author, {NO_REPLY_GITHUB_EMAIL} from '../../lib/models/author';
+import Author, {nullAuthor, NO_REPLY_GITHUB_EMAIL} from '../../lib/models/author';
 
 describe('Author', function() {
   it('recognizes the no-reply GitHub email address', function() {
@@ -15,5 +15,17 @@ describe('Author', function() {
 
     const a1 = new Author('other@bar.com', 'Nah');
     assert.isFalse(a1.hasLogin());
+  });
+
+  it('implements matching by email address', function() {
+    const a0 = new Author('same@same.com', 'Zero');
+    const a1 = new Author('same@same.com', 'One');
+    const a2 = new Author('same@same.com', 'Two', 'two');
+    const a3 = new Author('different@same.com', 'Three');
+
+    assert.isTrue(a0.matches(a1));
+    assert.isTrue(a0.matches(a2));
+    assert.isFalse(a0.matches(a3));
+    assert.isFalse(a0.matches(nullAuthor));
   });
 });
