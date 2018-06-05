deb790af9da1eb44e75928beb1ce4c7b30cffe8f
Ash Wilson <smashwilson@github.com>
2018-06-05 11:37:22 -0400

    Start the Marker type

---
+++ test/atom/marker.test.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import {mount} from 'enzyme';
+
+import Marker from '../../lib/atom/marker';
+import AtomTextEditor from '../../lib/atom/atom-text-editor';
+
+describe('Marker', function() {
+  let atomEnv, editor, markerID;
+
+  beforeEach(async function() {
+    atomEnv = global.buildAtomEnvironment();
+    editor = await atomEnv.workspace.open(__filename);
+  });
+
+  function setMarkerID(id) {
+    markerID = id;
+  }
+
+  it('adds its marker on mount with default properties', function() {
+    mount(
+      <Marker editor={editor} bufferRange={[[0, 0], [10, 0]]} handleID={setMarkerID} />,
+    );
+
+    const marker = editor.getMarker(markerID);
+    assert.isTrue(marker.getBufferRange().isEqual([[0, 0], [10, 0]]));
+    assert.strictEqual(marker.bufferMarker.invalidate, 'overlap');
+    assert.isFalse(marker.isReversed());
+    assert.isFalse(marker.bufferMarker.layer.maintainHistory);
+  });
+
+  it('configures its marker');
+
+  it('prefers marking a MarkerLayer to a TextEditor');
+
+  it('removes its layer on unmount');
+
+  it('marks an editor from a parent node');
+
+  it('marks a marker layer from a parent node');
+});
