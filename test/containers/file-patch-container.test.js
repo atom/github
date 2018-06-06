b6b92f937424b0ae48771cb33ecd07d826677c60
Ash Wilson <smashwilson@github.com>
2018-06-06 16:05:34 -0400

    FilePatchContainer tests

---
+++ test/containers/file-patch-container.test.js
@@ -0,0 +1,65 @@
+import path from 'path';
+import fs from 'fs-extra';
+import React from 'react';
+import {mount} from 'enzyme';
+
+import FilePatchContainer from '../../lib/containers/file-patch-container';
+import {cloneRepository, buildRepository} from '../helpers';
+
+describe('FilePatchContainer', function() {
+  let atomEnv, repository;
+
+  beforeEach(async function() {
+    atomEnv = global.buildAtomEnvironment();
+
+    const workdirPath = await cloneRepository();
+    repository = await buildRepository(workdirPath);
+    sinon.spy(repository, 'getFilePatchForPath');
+    sinon.spy(repository, 'isPartiallyStaged');
+
+    // a.txt: unstaged changes
+    await fs.writeFile(path.join(workdirPath, 'a.txt'), 'changed\n');
+
+    // b.txt: staged changes
+    await fs.writeFile(path.join(workdirPath, 'b.txt'), 'changed\n');
+    await repository.stageFiles(['b.txt']);
+
+    // c.txt: untouched
+  });
+
+  afterEach(function() {
+    atomEnv.destroy();
+  });
+
+  function buildApp(overrideProps = {}) {
+    const props = {
+      repository,
+      stagingStatus: 'unstaged',
+      relPath: 'a.txt',
+      ...overrideProps,
+    };
+
+    return <FilePatchContainer {...props} />;
+  }
+
+  it('renders a loading spinner before file patch data arrives', function() {
+    const wrapper = mount(buildApp());
+    assert.isTrue(wrapper.find('LoadingView').exists());
+  });
+
+  it('renders a message for an empty patch', async function() {
+    const wrapper = mount(buildApp({relPath: 'c.txt'}));
+    await assert.async.isTrue(wrapper.update().find('span.icon-info').exists());
+  });
+
+  it('renders a FilePatchView', async function() {
+    const wrapper = mount(buildApp({relPath: 'a.txt', stagingStatus: 'unstaged'}));
+    await assert.async.isTrue(wrapper.update().find('FilePatchView').exists());
+  });
+
+  it('passes unrecognized props to the FilePatchView', async function() {
+    const extra = Symbol('extra');
+    const wrapper = mount(buildApp({relPath: 'a.txt', stagingStatus: 'unstaged', extra}));
+    await assert.async.strictEqual(wrapper.update().find('FilePatchView').prop('extra'), extra);
+  });
+});
