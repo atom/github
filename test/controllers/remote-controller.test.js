8499d670f32fa47d3a5b03003ebe7631519898fe
Ash Wilson <smashwilson@github.com>
2018-06-14 13:35:24 -0400

    Remove RemoteContainer functionality from RemoteController

--- test/controllers/remote-controller.test.js
+++ test/controllers/remote-controller.test.js
@@ -1,42 +1,19 @@
 import React from 'react';
-import {mount} from 'enzyme';
+import {shallow} from 'enzyme';
 
-import GithubLoginModel from '../../lib/models/github-login-model';
 import BranchSet from '../../lib/models/branch-set';
 import Branch, {nullBranch} from '../../lib/models/branch';
 import Remote from '../../lib/models/remote';
-import {expectRelayQuery} from '../../lib/relay-network-layer-manager';
-import {InMemoryStrategy, UNAUTHENTICATED, INSUFFICIENT} from '../../lib/shared/keytar-strategy';
 import RemoteController from '../../lib/controllers/remote-controller';
 
 describe('RemoteController', function() {
-  let loginModel, remote, branchSet, currentBranch;
+  let remote, branchSet, currentBranch;
 
   beforeEach(function() {
-    loginModel = new GithubLoginModel(InMemoryStrategy);
-    sinon.stub(loginModel, 'getToken').returns(Promise.resolve('1234'));
-
     remote = new Remote('origin', 'git@github.com:atom/github');
     currentBranch = new Branch('master', nullBranch, nullBranch, true);
     branchSet = new BranchSet();
     branchSet.add(currentBranch);
-
-    expectRelayQuery({
-      name: 'issueishListContainerQuery',
-      variables: {query: 'repo:atom/github type:pr state:open'},
-    }, {
-      repository: {
-        defaultBranchRef: {
-          prefix: 'refs/heads',
-          name: 'master',
-        },
-        pullRequests: {
-          totalCount: 0,
-          edges: [],
-        },
-        id: '1',
-      },
-    });
   });
 
   function createApp(props = {}) {
@@ -44,8 +21,10 @@ describe('RemoteController', function() {
 
     return (
       <RemoteController
-        loginModel={loginModel}
         host="https://api.github.com"
+        token="1234"
+
+        repository={null}
 
         remote={remote}
         branches={branchSet}
@@ -60,41 +39,8 @@ describe('RemoteController', function() {
     );
   }
 
-  it('renders a loading message while fetching the token', function() {
-    const wrapper = mount(createApp());
-    assert.isTrue(wrapper.find('LoadingView').exists());
-  });
-
-  it('shows the login view if unauthenticated', async function() {
-    loginModel.getToken.restore();
-    sinon.stub(loginModel, 'getToken').returns(Promise.resolve(UNAUTHENTICATED));
-
-    const wrapper = mount(createApp());
-
-    await assert.async.isTrue(wrapper.update().find('GithubLoginView').exists());
-    assert.strictEqual(
-      wrapper.find('GithubLoginView').find('p').text(),
-      'Log in to GitHub to access PR information and more!',
-    );
-  });
-
-  it('shows the login view if more scopes are required', async function() {
-    loginModel.getToken.restore();
-    sinon.stub(loginModel, 'getToken').returns(Promise.resolve(INSUFFICIENT));
-
-    const wrapper = mount(createApp());
-
-    await assert.async.isTrue(wrapper.update().find('GithubLoginView').exists());
-    assert.strictEqual(
-      wrapper.find('GithubLoginView').find('p').text(),
-      'Your token no longer has sufficient authorizations. Please re-authenticate and generate a new one.',
-    );
-  });
-
-  it('renders issueish searches if authenticated', async function() {
-    const wrapper = mount(createApp());
-
-    await assert.async.isTrue(wrapper.update().find('IssueishSearchController').exists());
+  it('renders issueish searches', function() {
+    const wrapper = shallow(createApp());
 
     const controller = wrapper.update().find('IssueishSearchController');
     assert.strictEqual(controller.prop('token'), '1234');
