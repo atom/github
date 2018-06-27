97c4feca90654d406806644decbb844943a8e634
Ash Wilson <smashwilson@github.com>
2018-06-27 14:25:17 -0400

    Test the IssueishDetailController

--- test/fixtures/props/issueish-pane-props.js
+++ test/fixtures/props/issueish-pane-props.js
@@ -23,3 +23,84 @@ export function issueishDetailContainerProps(overrides = {}) {
     ...overrides,
   };
 }
+
+export function issueishDetailControllerProps(opts, overrides = {}) {
+  const o = {
+    repositoryName: 'repository',
+    ownerLogin: 'owner',
+
+    omitIssueish: false,
+    issueishNumber: 1,
+    issueishOverrides: {},
+
+    ...opts,
+  };
+
+  return {
+    repository: {
+      name: o.repositoryName,
+      owner: {
+        login: o.ownerLogin,
+      },
+      issueish: o.omitIssueish ? null : issueishDetailViewProps(opts, o.issueishOverrides).issueish,
+    },
+    issueishNumber: o.issueishNumber,
+
+    onTitleChange: () => {},
+    switchToIssueish: () => {},
+
+    ...overrides,
+  };
+}
+
+export function issueishDetailViewProps(opts, overrides = {}) {
+  const o = {
+    issueishKind: 'PullRequest',
+
+    repositoryName: 'repository',
+    ownerLogin: 'owner',
+
+    issueishTitle: 'title',
+    issueishBodyHTML: '<p>body</p>',
+    issueishAuthorLogin: 'author',
+    issueishAuthorAvatarURL: 'https://avatars3.githubusercontent.com/u/000?v=4',
+    issueishNumber: 1,
+    issueishState: 'OPEN',
+
+    relayRefetch: () => {},
+    ...opts,
+  };
+
+  return {
+    relay: {
+      refetch: o.relayRefetch,
+    },
+
+    repository: {
+      id: 'repository0',
+      name: o.repositoryName,
+      owner: {
+        login: o.ownerLogin,
+      },
+    },
+
+    issueish: {
+      id: 'pr0',
+      __typename: o.issueishKind,
+      title: o.issueishTitle,
+      bodyHTML: o.issueishBodyHTML,
+      number: o.issueishNumber,
+      state: o.issueishState,
+      author: {
+        login: o.issueishAuthorLogin,
+        avatarUrl: o.issueishAuthorAvatarURL,
+        url: `https://github.com/${o.issueishAuthorLogin}`,
+      },
+      reactionGroups: [],
+    },
+
+    switchToIssueish: () => {},
+
+    ...overrides,
+  };
+}
