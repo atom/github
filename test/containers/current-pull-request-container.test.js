5b8f2263eaa88d2695bd50e5c5f6ef9e327a5cbd
Ash Wilson <smashwilson@github.com>
2018-07-02 15:45:59 -0400

    PRs need distinct IDs

--- test/containers/current-pull-request-container.test.js
+++ test/containers/current-pull-request-container.test.js
@@ -159,12 +159,12 @@ describe('CurrentPullRequestContainer', function() {
     assert.deepEqual(controller.prop('results').map(result => result.number), [10]);
   });
 
-  it.only('filters out pull requests opened on different repositories', async function() {
+  it('filters out pull requests opened on different repositories', async function() {
     const repository = createRepositoryResult({id: 'upstream-repo'});
 
     const {resolve, promise} = useResults(
-      {number: 11, repositoryID: 'upstream-repo'},
-      {number: 22, repositoryID: 'someones-fork'},
+      {id: 'pr0', number: 11, repositoryID: 'upstream-repo'},
+      {id: 'pr1', number: 22, repositoryID: 'someones-fork'},
     );
 
     const wrapper = mount(buildApp({repository}));
@@ -172,8 +172,8 @@ describe('CurrentPullRequestContainer', function() {
     await promise;
     wrapper.update();
 
-    const numbers = wrapper.find('IssueishListView').prop('issueishes').map(i => i.number());
-    assert.deepEqual(numbers, ['#11']);
+    const numbers = wrapper.find('IssueishListView').prop('issueishes').map(i => i.getNumber());
+    assert.deepEqual(numbers, [11]);
   });
 
   it('performs the query again when a remote operation completes', async function() {
