3d8d723804eb0587e93d9beee20a454cb0b0c02a
Tilde Ann Thurium <annthurium@github.com>
2018-07-03 18:30:33 -0700

    improve reporterProxy tests

--- test/reporter-proxy.test.js
+++ test/reporter-proxy.test.js
@@ -3,12 +3,33 @@ const pjson = require('../package.json');
 
 const version = pjson.version;
 
+class FakeReporter {
+  addCustomEvent() {}
+
+  addTiming() {}
+
+  incrementCounter() {}
+}
+
+const fakeReporter = new FakeReporter();
+
 describe('reporterProxy', function() {
+  const event = {coAuthorCount: 2};
+  const eventType = 'commits';
+
+  const timingEventType = 'load';
+  const durationInMilliseconds = 42;
+
+  const counterName = 'push';
+  afterEach(function() {
+    // let's not leak state between tests, dawg.
+    reporterProxy.events = [];
+    reporterProxy.timings = [];
+    reporterProxy.counters = [];
+  })
+
   describe('before reporter has been set', function() {
     it('adds event to queue when addEvent is called', function() {
-      const event = {coAuthorCount: 2};
-      const eventType = 'commits';
-      const expectedEvent = { eventType, event };
       addEvent(eventType, event);
 
       const events = reporterProxy.events;
@@ -19,26 +40,89 @@ describe('reporterProxy', function() {
     });
 
     it('adds timing to queue when addTiming is called', function() {
-      const eventType = 'load';
-      const durationInMilliseconds = 42;
-      addTiming(eventType, durationInMilliseconds);
+      addTiming(timingEventType, durationInMilliseconds);
 
       const timings = reporterProxy.timings;
       assert.deepEqual(timings.length, 1);
       const timing = timings[0];
-      assert.deepEqual(timing.eventType, eventType);
+
+      assert.deepEqual(timing.eventType, timingEventType);
       assert.deepEqual(timing.durationInMilliseconds, durationInMilliseconds);
-      console.log(timing.metadata);
       assert.deepEqual(timing.metadata, { gitHubPackageVersion: version });
     })
 
     it('adds counter to queue when incrementCounter is called', function() {
-      const counterName = "commits";
       incrementCounter(counterName);
 
       const counters = reporterProxy.counters;
       assert.deepEqual(counters.length, 1);
       assert.deepEqual(counters[0], counterName);
+    });
+  });
+  describe('after reporter has been set', function() {
+    let addCustomEventStub, addTimingStub, incrementCounterStub;
+    beforeEach(function() {
+      addCustomEventStub = sinon.stub(fakeReporter, 'addCustomEvent');
+      addTimingStub = sinon.stub(fakeReporter, 'addTiming');
+      incrementCounterStub = sinon.stub(fakeReporter, 'incrementCounter');
+    })
+    it('empties all queues', function() {
+      addEvent(eventType, event);
+      addTiming(timingEventType, durationInMilliseconds);
+      incrementCounter(counterName);
+
+      assert.isFalse(addCustomEventStub.called);
+      assert.isFalse(addTimingStub.called);
+      assert.isFalse(incrementCounterStub.called);
+
+      assert.deepEqual(reporterProxy.events.length, 1);
+      assert.deepEqual(reporterProxy.timings.length, 1);
+      assert.deepEqual(reporterProxy.counters.length, 1);
+
+      reporterProxy.setReporter(fakeReporter);
+
+      const addCustomEventArgs = addCustomEventStub.lastCall.args;
+      assert.deepEqual(addCustomEventArgs[0], eventType);
+      assert.deepEqual(addCustomEventArgs[1], {coAuthorCount: 2, gitHubPackageVersion: version});
+
+      const addTimingArgs = addTimingStub.lastCall.args;
+      assert.deepEqual(addTimingArgs[0], timingEventType);
+      assert.deepEqual(addTimingArgs[1], durationInMilliseconds);
+      assert.deepEqual(addTimingArgs[2], { gitHubPackageVersion: version });
+
+      assert.deepEqual(incrementCounterStub.lastCall.args, [counterName]);
+    });
+    it('calls addCustomEvent directly, bypassing queue', function() {
+      assert.isFalse(addCustomEventStub.called);
+      reporterProxy.setReporter(fakeReporter);
+
+      addEvent(eventType, event);
+      assert.deepEqual(reporterProxy.events.length, 0);
+
+      const addCustomEventArgs = addCustomEventStub.lastCall.args;
+      assert.deepEqual(addCustomEventArgs[0], eventType);
+      assert.deepEqual(addCustomEventArgs[1], {coAuthorCount: 2, gitHubPackageVersion: version});
+    });
+    it('calls addTiming directly, bypassing queue', function() {
+      assert.isFalse(addTimingStub.called);
+      reporterProxy.setReporter(fakeReporter);
+
+      addTiming(timingEventType, durationInMilliseconds);
+      assert.deepEqual(reporterProxy.timings.length, 0);
+
+      const addTimingArgs = addTimingStub.lastCall.args;
+      assert.deepEqual(addTimingArgs[0], timingEventType);
+      assert.deepEqual(addTimingArgs[1], durationInMilliseconds);
+      assert.deepEqual(addTimingArgs[2], { gitHubPackageVersion: version });
+    });
+    it('calls incrementCounter directly, bypassing queue', function() {
+      assert.isFalse(incrementCounterStub.called);
+      reporterProxy.setReporter(fakeReporter);
+
+      incrementCounter(counterName);
+      assert.deepEqual(reporterProxy.counters.length, 0);
+
+      assert.deepEqual(incrementCounterStub.lastCall.args, [counterName]);
     });
   });
 });
