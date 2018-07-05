7d829f0caa26132673f8536159851f978622c2e9
Ash Wilson <smashwilson@github.com>
2018-07-05 13:01:06 -0400

    Filter a RemoteSet by a predicate function

--- test/models/remote-set.test.js
+++ test/models/remote-set.test.js
@@ -35,4 +35,13 @@ describe('RemoteSet', function() {
     const set = new RemoteSet(remotes);
     assert.deepEqual(Array.from(set), remotes);
   });
+
+  it('filters remotes by a predicate', function() {
+    const set0 = new RemoteSet(remotes);
+    const set1 = set0.filter(remote => remote.getName() === 'upstream');
+
+    assert.notStrictEqual(set0, set1);
+    assert.isTrue(set1.withName('upstream').isPresent());
+    assert.isFalse(set1.withName('origin1').isPresent());
+  });
 });
