6d15c4e7813f4bc823f659248b7c4ce9d3f0a984
Ash Wilson <smashwilson@github.com>
2018-07-05 11:51:42 -0400

    Use RemoteSet to store... sets of Remotes

---
+++ test/models/remote-set.test.js
@@ -0,0 +1,38 @@
+import RemoteSet from '../../lib/models/remote-set';
+import Remote from '../../lib/models/remote';
+
+describe('RemoteSet', function() {
+  const remotes = [
+    new Remote('origin', 'git@github.com:origin/repo.git'),
+    new Remote('upstream', 'git@github.com:upstream/repo.git'),
+  ];
+
+  it('creates an empty set', function() {
+    const set = new RemoteSet();
+    assert.isTrue(set.isEmpty());
+    assert.strictEqual(set.size(), 0);
+  });
+
+  it('creates a set containing one or more Remotes', function() {
+    const set = new RemoteSet(remotes);
+    assert.isFalse(set.isEmpty());
+    assert.strictEqual(set.size(), 2);
+  });
+
+  it('retrieves a Remote from the set by name', function() {
+    const set = new RemoteSet(remotes);
+    const remote = set.withName('upstream');
+    assert.strictEqual(remote, remotes[1]);
+  });
+
+  it('returns a nullRemote for unknown remote names', function() {
+    const set = new RemoteSet(remotes);
+    const remote = set.withName('unknown');
+    assert.isFalse(remote.isPresent());
+  });
+
+  it('iterates over the Remotes', function() {
+    const set = new RemoteSet(remotes);
+    assert.deepEqual(Array.from(set), remotes);
+  });
+});
