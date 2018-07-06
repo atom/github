ed219569ab575cf9317fecf11aa995c445f8b09d
Ash Wilson <smashwilson@github.com>
2018-07-06 09:00:34 -0400

    Extract protocol from remote URLs

--- test/models/remote.test.js
+++ test/models/remote.test.js
@@ -1,24 +1,29 @@
-import Remote from '../../lib/models/remote';
+import Remote, {nullRemote} from '../../lib/models/remote';
 
 describe('Remote', function() {
   it('detects and extracts information from GitHub repository URLs', function() {
     const urls = [
-      'git@github.com:atom/github.git',
-      'https://github.com/atom/github.git',
-      'https://git:pass@github.com/atom/github.git',
-      'ssh+https://github.com/atom/github.git',
-      'git://github.com/atom/github',
-      'ssh://git@github.com:atom/github.git',
+      ['git@github.com:atom/github.git', 'ssh'],
+      ['https://github.com/atom/github.git', 'https'],
+      ['https://git:pass@github.com/atom/github.git', 'https'],
+      ['ssh+https://github.com/atom/github.git', 'ssh+https'],
+      ['git://github.com/atom/github', 'git'],
+      ['ssh://git@github.com:atom/github.git', 'ssh'],
     ];
 
-    for (const url of urls) {
+    for (const [url, proto] of urls) {
       const remote = new Remote('origin', url);
 
-      assert.equal(remote.getName(), 'origin');
-      assert.equal(remote.getUrl(), url);
+      assert.isTrue(remote.isPresent());
+      assert.strictEqual(remote.getName(), 'origin');
+      assert.strictEqual(remote.getNameOr('else'), 'origin');
+      assert.strictEqual(remote.getUrl(), url);
       assert.isTrue(remote.isGithubRepo());
-      assert.equal(remote.getOwner(), 'atom');
-      assert.equal(remote.getRepo(), 'github');
+      assert.strictEqual(remote.getDomain(), 'github.com');
+      assert.strictEqual(remote.getProtocol(), proto);
+      assert.strictEqual(remote.getOwner(), 'atom');
+      assert.strictEqual(remote.getRepo(), 'github');
+      assert.strictEqual(remote.getSlug(), 'atom/github');
     }
   });
 
@@ -31,11 +36,42 @@ describe('Remote', function() {
     for (const url of urls) {
       const remote = new Remote('origin', url);
 
-      assert.equal(remote.getName(), 'origin');
-      assert.equal(remote.getUrl(), url);
+      assert.isTrue(remote.isPresent());
+      assert.strictEqual(remote.getName(), 'origin');
+      assert.strictEqual(remote.getNameOr('else'), 'origin');
+      assert.strictEqual(remote.getUrl(), url);
       assert.isFalse(remote.isGithubRepo());
+      assert.isNull(remote.getDomain());
       assert.isNull(remote.getOwner());
       assert.isNull(remote.getRepo());
+      assert.isNull(remote.getSlug());
     }
+  });
+
+  it('may be created without a URL', function() {
+    const remote = new Remote('origin');
+
+    assert.isTrue(remote.isPresent());
+    assert.strictEqual(remote.getName(), 'origin');
+    assert.strictEqual(remote.getNameOr('else'), 'origin');
+    assert.isNull(remote.getUrl());
+    assert.isFalse(remote.isGithubRepo());
+    assert.isNull(remote.getDomain());
+    assert.isNull(remote.getOwner());
+    assert.isNull(remote.getRepo());
+    assert.isNull(remote.getSlug());
+  });
+
+  it('has a corresponding null object', function() {
+    assert.isFalse(nullRemote.isPresent());
+    assert.strictEqual(nullRemote.getName(), '');
+    assert.strictEqual(nullRemote.getUrl(), '');
+    assert.isFalse(nullRemote.isGithubRepo());
+    assert.isNull(nullRemote.getDomain());
+    assert.isNull(nullRemote.getProtocol());
+    assert.isNull(nullRemote.getOwner());
+    assert.isNull(nullRemote.getRepo());
+    assert.isNull(nullRemote.getSlug());
+    assert.strictEqual(nullRemote.getNameOr('else'), 'else');
   });
 });
