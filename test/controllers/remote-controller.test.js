4840688bb18c908ddf18b2167f76bd3013722158
Tilde Ann Thurium <annthurium@github.com>
2018-07-05 18:12:46 -0700

    :fire: .only

--- test/controllers/remote-controller.test.js
+++ test/controllers/remote-controller.test.js
@@ -9,7 +9,7 @@ import {nullOperationStateObserver} from '../../lib/models/operation-state-obser
 import RemoteController from '../../lib/controllers/remote-controller';
 import * as reporterProxy from '../../lib/reporter-proxy';
 
-describe.only('RemoteController', async function() {
+describe('RemoteController', async function() {
   let atomEnv, remote, branchSet, currentBranch;
 
   beforeEach(function() {
