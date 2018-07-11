1a7bf63ad7b6c20db0fb4a4132ee1952e1da4cd8
Ash Wilson <smashwilson@github.com>
2018-07-11 09:18:16 -0400

    Match empty-valued query parameters
    Also :rocket: test coverage :rocket: for uri-pattern

--- test/atom/uri-pattern.test.js
+++ test/atom/uri-pattern.test.js
@@ -5,23 +5,38 @@ describe('URIPattern', function() {
     let exact;
 
     beforeEach(function() {
-      exact = new URIPattern('atom-github://exact');
+      exact = new URIPattern('atom-github://exact/match');
     });
 
     it('matches exact URIs', function() {
-      assert.isTrue(exact.matches('atom-github://exact').ok());
+      assert.isTrue(exact.matches('atom-github://exact/match').ok());
+      assert.isTrue(exact.matches('atom-github://exact/match/').ok());
     });
 
     it('does not match any other URIs', function() {
       assert.isFalse(exact.matches('atom-github://exactbutnot').ok());
+      assert.isFalse(exact.matches('atom-github://exact').ok());
       assert.isFalse(exact.matches('https://exact').ok());
       assert.isFalse(exact.matches('atom-github://exact/but/not').ok());
-      assert.isFalse(exact.matches('atom-github://exact?no=no').ok());
+      assert.isFalse(exact.matches('atom-github://exact/match?no=no').ok());
     });
 
     it('does not match undefined or null', function() {
       assert.isFalse(exact.matches(undefined).ok());
       assert.isFalse(exact.matches(null).ok());
+    });
+
+    it('matches username and password', function() {
+      const pattern = new URIPattern('proto://user:pass@host/some/path');
+      assert.isTrue(pattern.matches('proto://user:pass@host/some/path').ok());
+      assert.isFalse(pattern.matches('proto://other:pass@host/some/path').ok());
+      assert.isFalse(pattern.matches('proto://user:wrong@host/some/path').ok());
+    });
+
+    it('matches a hash', function() {
+      const pattern = new URIPattern('proto://host/foo#exact');
+      assert.isTrue(pattern.matches('proto://host/foo#exact').ok());
+      assert.isFalse(pattern.matches('proto://host/foo#nope').ok());
     });
   });
 
@@ -103,6 +118,12 @@ describe('URIPattern', function() {
       const m1 = pattern.matches('proto://host?p1=no&p0=yes');
       assert.isTrue(m1.ok());
       assert.deepEqual(m1.getParams(), {zero: 'yes', one: 'no'});
+
+      const m2 = pattern.matches('proto://host?p0=&p1=');
+      assert.isTrue(m2.ok());
+      assert.deepEqual(m2.getParams(), {zero: '', one: ''});
+
+      assert.isFalse(pattern.matches('proto://host?p0=no').ok());
     });
 
     it('does not match a single query string parameter against multiple occurrences', function() {
@@ -126,12 +147,68 @@ describe('URIPattern', function() {
       assert.deepEqual(m2.getParams(), {multi: ['0', '1', '2']});
     });
 
+    it('captures a hash', function() {
+      const pattern = new URIPattern('proto://host/root#{hash}');
+      const m = pattern.matches('proto://host/root#value');
+
+      assert.isTrue(m.ok());
+      assert.deepEqual(m.getParams(), {hash: 'value'});
+    });
+
     it('URI-decodes matched parameters', function() {
       const pattern = new URIPattern('proto://host/root/{child}?q={search}');
       const m = pattern.matches('proto://host/root/hooray%3E%20for%3C%20encodings?q=%3F%26%3F!');
 
       assert.isTrue(m.ok());
       assert.deepEqual(m.getParams(), {child: 'hooray> for< encodings', search: '?&?!'});
+    });
+
+    it('ignores the value of an empty capture', function() {
+      const pattern = new URIPattern('proto://host/root/{}?q={}#{}');
+      const m = pattern.matches('proto://host/root/anything?q=at#all');
+
+      assert.isTrue(m.ok());
+      assert.deepEqual(m.getParams(), {});
+    });
+  });
+
+  it('prints itself as a string for debugging', function() {
+    assert.strictEqual(
+      new URIPattern('proto://host/exact').toString(),
+      '<URIPattern proto://host/exact>',
+    );
+    assert.strictEqual(
+      new URIPattern('proto://host/{param}?q={value}').toString(),
+      '<URIPattern proto://host/{param}?q={value}>',
+    );
+  });
+
+  describe('match objects', function() {
+    it('prints itself as a string for debugging', function() {
+      const pattern = new URIPattern('proto://host/exact');
+      const m = pattern.matches('proto://host/exact');
+      assert.strictEqual(m.toString(), '<URIMatch ok>');
+    });
+
+    it('prints captured values', function() {
+      const pattern = new URIPattern('proto://host/{capture0}/{capture1}');
+      const m = pattern.matches('proto://host/first/and%20escaped');
+      assert.strictEqual(m.toString(), '<URIMatch ok capture0="first" capture1="and escaped">');
+    });
+
+    it('remembers the matched URI', function() {
+      const pattern = new URIPattern('proto://host/{capture0}/{capture1}');
+      const m = pattern.matches('proto://host/first/and%20escaped');
+      assert.strictEqual(m.getURI(), 'proto://host/first/and%20escaped');
+    });
+
+    it('behaves like a nonURIMatch', function() {
+      const pattern = new URIPattern('proto://yes');
+      const m = pattern.matches('proto://no');
+      assert.isFalse(m.ok());
+      assert.isUndefined(m.getURI());
+      assert.deepEqual(m.getParams(), {});
+      assert.strictEqual(m.toString(), '<nonURIMatch>');
     });
   });
 });
