71874c665e64fbd7665c30d762b84d7ec29907d6
Ash Wilson <smashwilson@github.com>
2018-07-20 09:32:26 -0400

    GitHubTabController coverage

--- test/fixtures/props/github-tab-props.js
+++ test/fixtures/props/github-tab-props.js
@@ -1,6 +1,11 @@
 import {InMemoryStrategy} from '../../../lib/shared/keytar-strategy';
 import GithubLoginModel from '../../../lib/models/github-login-model';
 import RefHolder from '../../../lib/models/ref-holder';
+import OperationStateObserver, {PUSH, PULL, FETCH} from '../../../lib/models/operation-state-observer';
+import RemoteSet from '../../../lib/models/remote-set';
+import {nullRemote} from '../../../lib/models/remote';
+import BranchSet from '../../../lib/models/branch-set';
+import {nullBranch} from '../../../lib/models/branch';
 
 export function gitHubTabItemProps(atomEnv, repository, overrides = {}) {
   return {
@@ -15,6 +20,19 @@ export function gitHubTabContainerProps(atomEnv, repository, overrides = {}) {
   return {
     ...gitHubTabItemProps(atomEnv, repository),
     rootHolder: new RefHolder(),
+    ...overrides,
+  };
+}
+
+export function gitHubTabControllerProps(atomEnv, repository, overrides = {}) {
+  return {
+    ...gitHubTabContainerProps(atomEnv, repository),
+    remoteOperationObserver: new OperationStateObserver(repository, PUSH, PULL, FETCH),
+    workingDirectory: repository.getWorkingDirectoryPath(),
+    allRemotes: new RemoteSet(),
+    branches: new BranchSet(),
+    aheadCount: 0,
+    pushInProgress: false,
     ...overrides,
   };
 }
