c5592df8677a6cc35c4163014ff8416f1b7f6a61
Tilde Ann Thurium <annthurium@github.com>
2018-08-15 14:42:44 -0700

    add some tests for getGitHubUser

--- test/github-user.test.js
+++ test/github-user.test.js
@@ -7,29 +7,57 @@ describe.only('getGitHubUser', function() {
   let login;
   const gitHubUser = 'annthurium';
   function useResult() {
-    return expectRelayQuery({name: 'GetGitHubUser'}, {
+    return expectRelayQuery({name: 'GetGitHubUser', variables: {}}, {
       viewer: {login: gitHubUser}
     });
   }
   beforeEach(function() {
     login = new GithubLoginModel(InMemoryStrategy);
-    sinon.stub(login, 'getScopes').returns(Promise.resolve(GithubLoginModel.REQUIRED_SCOPES));
-  })
-  it('returns the GitHub user if one exists', async function() {
-    await login.setToken('https://api.github.com', '1234');
-    useResult();
-    const user = await getGitHubUser();
-    assert.deepEqual(user, gitHubUser);
   });
-  // it('returns null if token has insufficient scope', function() {
-  //
-  // });
-  // it('handles errors gracefully', function() {
-  //
-  // });
-  // it('returns null if response does not contain viewer', function() {
-  //
-  // });
-
-
+  describe('sufficiently scoped tokens', async function() {
+    beforeEach(async function() {
+      sinon.stub(login, 'getScopes').returns(Promise.resolve(GithubLoginModel.REQUIRED_SCOPES));
+      await login.setToken('https://api.github.com', '1234');
+    });
+    it('returns the GitHub user if one exists', async function() {
+      const {resolve} = useResult();
+      resolve();
+      const user = await getGitHubUser(login);
+      await assert.async.deepEqual(user, gitHubUser);
+    });
+    it.only('handles graphql error gracefully', async function() {
+      // sinon.stub(login, 'getScopes').returns(Promise.resolve(GithubLoginModel.REQUIRED_SCOPES));
+      const {reject, promise} = useResult();
+      const e = new Error('wat');
+      e.rawStack = e.stack;
+      reject(e);
+      await promise.catch(() => {});
+      const user = await getGitHubUser(login);
+      await assert.async.isNull(user);
+    });
+    it('returns null if response does not contain viewer', async function() {
+      function useEmptyResult() {
+        return expectRelayQuery({name: 'GetGitHubUser', variables: {}}, {});
+      }
+      const {resolve} = useEmptyResult();
+      resolve();
+      const user = await getGitHubUser(login);
+      await assert.async.isNull(user);
+    });
+  });
+  it('returns null if token has insufficient scope', async function() {
+    sinon.stub(login, 'getScopes').resolves(['not-enough']);
+    const user = await getGitHubUser(login);
+    await assert.async.isNull(user);
+  });
+  it('handles graphql error gracefully', async function() {
+    sinon.stub(login, 'getScopes').returns(Promise.resolve(GithubLoginModel.REQUIRED_SCOPES));
+    const {reject, promise} = useResult();
+    const e = new Error('wat');
+    e.rawStack = e.stack;
+    reject(e);
+    await promise.catch(() => {});
+    const user = await getGitHubUser(login);
+    await assert.async.isNull(user);
+  });
 });
