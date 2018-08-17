e27b2dd7e8bb70b0ccea466371f0db515742824a
Ash Wilson <smashwilson@github.com>
2018-08-17 15:19:51 -0400

    Implement a nullFilePatch

--- test/models/patch/file-patch.test.js
+++ test/models/patch/file-patch.test.js
@@ -1,4 +1,4 @@
-import FilePatch from '../../../lib/models/patch/file-patch';
+import FilePatch, {nullFilePatch} from '../../../lib/models/patch/file-patch';
 import File, {nullFile} from '../../../lib/models/patch/file';
 import Patch from '../../../lib/models/patch/patch';
 import Hunk from '../../../lib/models/patch/hunk';
@@ -25,6 +25,8 @@ describe('FilePatch', function() {
     const oldFile = new File({path: 'a.txt', mode: '120000', symlink: 'dest.txt'});
     const newFile = new File({path: 'b.txt', mode: '100755'});
     const filePatch = new FilePatch(oldFile, newFile, patch);
+
+    assert.isTrue(filePatch.isPresent());
 
     assert.strictEqual(filePatch.getOldPath(), 'a.txt');
     assert.strictEqual(filePatch.getOldMode(), '120000');
@@ -688,5 +690,34 @@ describe('FilePatch', function() {
     });
   });
 
-  it('getByteSize() returns the size in bytes');
+  it('has a nullFilePatch that stubs all FilePatch methods', function() {
+    const rowRange = new IndexedRowRange({bufferRange: [[0, 0], [1, 0]], startOffset: 0, endOffset: 10});
+
+    assert.isFalse(nullFilePatch.isPresent());
+    assert.isFalse(nullFilePatch.getOldFile().isPresent());
+    assert.isFalse(nullFilePatch.getNewFile().isPresent());
+    assert.isFalse(nullFilePatch.getPatch().isPresent());
+    assert.isNull(nullFilePatch.getOldPath());
+    assert.isNull(nullFilePatch.getNewPath());
+    assert.isNull(nullFilePatch.getOldMode());
+    assert.isNull(nullFilePatch.getNewMode());
+    assert.isNull(nullFilePatch.getOldSymlink());
+    assert.isNull(nullFilePatch.getNewSymlink());
+    assert.strictEqual(nullFilePatch.getByteSize(), 0);
+    assert.strictEqual(nullFilePatch.getBufferText(), '');
+    assert.lengthOf(nullFilePatch.getAdditionRanges(), 0);
+    assert.lengthOf(nullFilePatch.getDeletionRanges(), 0);
+    assert.lengthOf(nullFilePatch.getNoNewlineRanges(), 0);
+    assert.isFalse(nullFilePatch.didChangeExecutableMode());
+    assert.isFalse(nullFilePatch.hasSymlink());
+    assert.isFalse(nullFilePatch.hasTypechange());
+    assert.isNull(nullFilePatch.getPath());
+    assert.isNull(nullFilePatch.getStatus());
+    assert.lengthOf(nullFilePatch.getHunks(), 0);
+    assert.isFalse(nullFilePatch.getStagePatchForLines(new Set([0])).isPresent());
+    assert.isFalse(nullFilePatch.getStagePatchForHunk(new Hunk({changes: [], rowRange})).isPresent());
+    assert.isFalse(nullFilePatch.getUnstagePatchForLines(new Set([0])).isPresent());
+    assert.isFalse(nullFilePatch.getUnstagePatchForHunk(new Hunk({changes: [], rowRange})).isPresent());
+    assert.strictEqual(nullFilePatch.toString(), '');
+  });
 });
