141f0ab6371a519945700d41ffc89e255a13c02c
Ash Wilson <smashwilson@github.com>
2018-09-17 21:11:32 -0400

    NullPatch needs a getHunkAt method

--- test/models/patch/patch.test.js
+++ test/models/patch/patch.test.js
@@ -874,6 +874,7 @@ describe('Patch', function() {
     assert.strictEqual(nullPatch.toString(), '');
     assert.strictEqual(nullPatch.getChangedLineCount(), 0);
     assert.strictEqual(nullPatch.getMaxLineNumberWidth(), 0);
+    assert.isUndefined(nullPatch.getHunkAt(0));
     assert.deepEqual(nullPatch.getFirstChangeRange(), [[0, 0], [0, 0]]);
     assert.deepEqual(nullPatch.getNextSelectionRange(), [[0, 0], [0, 0]]);
   });
