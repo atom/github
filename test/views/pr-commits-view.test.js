1542fe454e24d9c4fc247e8779c62a34cf9ec82b
Tilde Ann Thurium <annthurium@github.com>
2018-09-21 15:45:18 -0700

    add tests for PrCommitView
    Co-Authored-By: Katrina Uychaco <katrina@github.com>

---
+++ test/views/pr-commits-view.test.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+
+import PrCommitView from '../../lib/views/pr-commit-view';
+import {PrCommitsView} from '../../lib/views/pr-commits-view';
+
+const commitSpec = {
+  committer: {
+    avatarUrl: 'https://avatars3.githubusercontent.com/u/3781742',
+    name: 'Margaret Hamilton',
+    date: '2018-05-16T21:54:24.500Z',
+  },
+  messageHeadline: 'This one weird trick for getting to the moon will blow your mind 🚀',
+  abbreviatedOid: 'bad1dea',
+  url: 'https://github.com/atom/github/pull/1684/commits/bad1deaea3d816383721478fc631b5edd0c2b370',
+};
+
+describe.only('PrCommitsView', function() {
+  function buildApp(opts, overloadProps = {}) {
+    const o = {
+      relayHasMore: () => false,
+      relayLoadMore: () => {},
+      relayIsLoading: () => false,
+      commitSpecs: [],
+      commitStartCursor: 0,
+      ...opts,
+    };
+
+    if (o.commitTotal === undefined) {
+      o.commitTotal = o.commitSpecs.length;
+    }
+
+    const props = {
+      relay: {
+        hasMore: o.relayHasMore,
+        loadMore: o.relayLoadMore,
+        isLoading: o.relayIsLoading,
+      },
+      ...overloadProps,
+    };
+
+    const commits = {
+      edges: o.commitSpecs.map((spec, i) => ({
+        cursor: `result${i}`,
+        node: {
+          commit: spec,
+          id: i,
+        },
+      })),
+      pageInfo: {
+        startCursor: `result${o.commitStartCursor}`,
+        endCursor: `result${o.commitStartCursor + o.commitSpecs.length}`,
+        hasNextPage: o.commitStartCursor + o.commitSpecs.length < o.commitTotal,
+        hasPreviousPage: o.commitCursor !== 0,
+      },
+      totalCount: o.commitTotal,
+    };
+    props.pullRequest = {commits};
+
+    return <PrCommitsView {...props} />;
+  }
+
+  it('renders commits', function() {
+    const commitSpecs = [commitSpec, commitSpec];
+    const wrapper = shallow(buildApp({commitSpecs}));
+    assert.strictEqual(wrapper.find(PrCommitView).length, commitSpecs.length);
+  });
+
+  describe('load more button', function() {
+
+    it('is not rendered if there are no more commits', function() {
+
+    });
+
+    it('is rendered if there are more commits', function() {
+
+    });
+
+    it('loads more commits when load more button is clicked', function() {
+
+    });
+
+  });
+
+
+
+});
