f3a2c7dbac1f76c655b4682eb5257bf90001e59c
Ash Wilson <smashwilson@github.com>
2018-10-29 15:38:08 -0400

    Reported as #1698

--- test/git-strategies.test.js
+++ test/git-strategies.test.js
@@ -1457,6 +1457,8 @@ import * as reporterProxy from '../lib/reporter-proxy';
       }
 
       it('prompts for authentication data through Atom', async function() {
+        this.retries(5); // FLAKE
+
         let query = null;
         const git = await withHttpRemote({
           prompt: q => {
