0ac071487092b938b7f80961a03332456d3f9d88
Ash Wilson <smashwilson@github.com>
2018-10-30 09:55:54 -0400

    It helps if you import the right path

--- test/containers/commit-preview-container.test.js
+++ test/containers/commit-preview-container.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {shallow} from 'enzyme';
 
-import CommitPreviewContainer from '../lib/';
+import CommitPreviewContainer from '../lib/containers/commit-preview-container';
 
 describe('CommitPreviewContainer', function() {
   let atomEnv;
