7f0ec8798d9ff512ceb1ad87a9bfce183ab655df
Ash Wilson <smashwilson@github.com>
2018-10-31 16:36:10 -0400

    Tests for the CommitPreviewContainer

--- test/containers/commit-preview-container.test.js
+++ test/containers/commit-preview-container.test.js
@@ -31,4 +31,29 @@ describe('CommitPreviewContainer', function() {
     const wrapper = mount(buildApp());
     assert.isTrue(wrapper.find('LoadingView').exists());
   });
+
+  it('renders a loading spinner while the file patch is being loaded', async function() {
+    await repository.getLoadPromise();
+    const patchPromise = repository.getStagedChangesPatch();
+    let resolveDelayedPromise = () => {};
+    const delayedPromise = new Promise(resolve => {
+      resolveDelayedPromise = resolve;
+    });
+    sinon.stub(repository, 'getStagedChangesPatch').returns(delayedPromise);
+
+    const wrapper = mount(buildApp());
+
+    assert.isTrue(wrapper.find('LoadingView').exists());
+    resolveDelayedPromise(patchPromise);
+    await assert.async.isFalse(wrapper.update().find('LoadingView').exists());
+  });
+
+  it('renders a MultiFilePatchController once the file patch is loaded', async function() {
+    await repository.getLoadPromise();
+    const patch = await repository.getStagedChangesPatch();
+
+    const wrapper = mount(buildApp());
+    await assert.async.isTrue(wrapper.update().find('MultiFilePatchController').exists());
+    assert.strictEqual(wrapper.find('MultiFilePatchController').prop('multiFilePatch'), patch);
+  });
 });
