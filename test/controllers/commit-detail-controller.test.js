dbe16e2946a489fdbd7785b59f9a66e473c2026c
Vanessa Yuen <vanessa.yh.yuen@gmail.com>
2018-11-26 16:23:52 +0100

    add test for co-authored commits

--- test/controllers/commit-detail-controller.test.js
+++ test/controllers/commit-detail-controller.test.js
@@ -8,7 +8,7 @@ import CommitDetailController from '../../lib/controllers/commit-detail-controll
 import Commit from '../../lib/models/commit';
 import {multiFilePatchBuilder} from '../builder/patch';
 
-describe.only('CommitDetailController', function() {
+describe('CommitDetailController', function() {
 
   let atomEnv, repository, commit;
 
@@ -59,19 +59,41 @@ describe.only('CommitDetailController', function() {
       authorEmail: 'very@nice.com',
       authorDate: moment().subtract(2, 'days').unix(),
       messageSubject: 'subject',
-      messageBody: 'messageBody 🌙',
+      messageBody: 'messageBody',
     });
     const {multiFilePatch: mfp} = multiFilePatchBuilder().addFilePatch().build();
     sinon.stub(newCommit, 'getMultiFileDiff').returns(mfp);
     const wrapper = mount(buildApp({commit: newCommit}));
 
     assert.strictEqual(wrapper.find('.github-CommitDetailView-title').text(), 'subject');
-    assert.strictEqual(wrapper.find('.github-CommitDetailView-moreText').text(), 'messageBody 🌙');
+    assert.strictEqual(wrapper.find('.github-CommitDetailView-moreText').text(), 'messageBody');
     assert.strictEqual(wrapper.find('.github-CommitDetailView-metaText').text(), 'very@nice.com committed 2 days ago');
     assert.strictEqual(wrapper.find('.github-CommitDetailView-sha').text(), '420');
     /* TODO fix href test */
-    assert.strictEqual(wrapper.find('.github-CommitDetailView-sha a').prop('href'), '420');
+    // assert.strictEqual(wrapper.find('.github-CommitDetailView-sha a').prop('href'), '420');
+    assert.strictEqual(wrapper.find('img.github-RecentCommit-avatar').prop('src'), 'https://avatars.githubusercontent.com/u/e?email=very%40nice.com&s=32');
+  });
 
+  it('renders multiple avatars for co-authored commit', function() {
+    const newCommit = new Commit({
+      sha: '420',
+      authorEmail: 'very@nice.com',
+      authorDate: moment().subtract(2, 'days').unix(),
+      messageSubject: 'subject',
+      messageBody: 'messageBody',
+      coAuthors: [{name: 'two', email: 'two@coauthor.com'}, {name: 'three', email: 'three@coauthor.com'}],
+    });
+    const {multiFilePatch: mfp} = multiFilePatchBuilder().addFilePatch().build();
+    sinon.stub(newCommit, 'getMultiFileDiff').returns(mfp);
+    const wrapper = mount(buildApp({commit: newCommit}));
+    assert.deepEqual(
+      wrapper.find('img.github-RecentCommit-avatar').map(w => w.prop('src')),
+      [
+        'https://avatars.githubusercontent.com/u/e?email=very%40nice.com&s=32',
+        'https://avatars.githubusercontent.com/u/e?email=two%40coauthor.com&s=32',
+        'https://avatars.githubusercontent.com/u/e?email=three%40coauthor.com&s=32',
+      ],
+    );
   });
 
 });
