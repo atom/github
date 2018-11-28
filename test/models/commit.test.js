4d8737cbc321cddefddecd98ed34e6e8304613fa
Ash Wilson <smashwilson@github.com>
2018-11-28 15:48:33 -0500

    Failing tests and initial implementation of abbreviatedBody()

--- test/models/commit.test.js
+++ test/models/commit.test.js
@@ -38,4 +38,116 @@ describe('Commit', function() {
       assert.isFalse(nullCommit.isBodyLong());
     });
   });
+
+  describe('abbreviatedBody()', function() {
+    it('returns the message body as-is when the body is short', function() {
+      const commit = commitBuilder().messageBody('short').build();
+      assert.strictEqual(commit.abbreviatedBody(), 'short');
+    });
+
+    it('truncates the message body at the nearest paragraph boundary before the cutoff if one is nearby', function() {
+      // The | is at the 1000-character mark.
+      const body = dedent`
+        Lorem ipsum dolor sit amet, et his justo deleniti, omnium fastidii adversarium at has. Mazim alterum sea ea,
+        essent malorum persius ne mei. Nam ea tempor qualisque, modus doming te has. Affert dolore albucius te vis, eam
+        tantas nullam corrumpit ad, in oratio luptatum eleifend vim.
+
+        Ea salutatus contentiones eos. Eam in veniam facete volutpat, solum appetere adversarium ut quo. Vel cu appetere
+        urbanitas, usu ut aperiri mediocritatem, alia molestie urbanitas cu qui. Velit antiopam erroribus no eum,
+        scripta iudicabit ne nam, in duis clita commodo sit.
+
+        Assum sensibus oportere te vel, vis semper evertitur definiebas in. Tamquam feugiat comprehensam ut his, et eum
+        voluptua ullamcorper, ex mei debitis inciderint. Sit discere pertinax te, an mei liber putant. Ad doctus
+        tractatos ius, duo ad civibus alienum, nominati voluptaria sed an. Libris essent philosophia et vix. Nusquam
+        reprehendunt et mea. Ea eius omnes voluptua sit.
+
+        No cum illud verear efficiantur. Id altera imperdiet nec. Noster aud|iam accusamus mei at, no zril libris nemore
+        duo, ius ne rebum doctus fuisset. Legimus epicurei in sit, esse purto suscipit eu qui, oporteat deserunt
+        delicatissimi sea in. Est id putent accusata convenire, no tibique molestie accommodare quo, cu est fuisset
+        offendit evertitur.
+      `;
+
+      const commit = commitBuilder().messageBody(body).build();
+      assert.strictEqual(commit.abbreviatedBody(), dedent`
+        Lorem ipsum dolor sit amet, et his justo deleniti, omnium fastidii adversarium at has. Mazim alterum sea ea,
+        essent malorum persius ne mei. Nam ea tempor qualisque, modus doming te has. Affert dolore albucius te vis, eam
+        tantas nullam corrumpit ad, in oratio luptatum eleifend vim.
+
+        Ea salutatus contentiones eos. Eam in veniam facete volutpat, solum appetere adversarium ut quo. Vel cu appetere
+        urbanitas, usu ut aperiri mediocritatem, alia molestie urbanitas cu qui. Velit antiopam erroribus no eum,
+        scripta iudicabit ne nam, in duis clita commodo sit.
+
+        Assum sensibus oportere te vel, vis semper evertitur definiebas in. Tamquam feugiat comprehensam ut his, et eum
+        voluptua ullamcorper, ex mei debitis inciderint. Sit discere pertinax te, an mei liber putant. Ad doctus
+        tractatos ius, duo ad civibus alienum, nominati voluptaria sed an. Libris essent philosophia et vix. Nusquam
+        reprehendunt et mea. Ea eius omnes voluptua sit.
+      `);
+    });
+
+    it('truncates the message body at the nearest word boundary before the cutoff if one is nearby', function() {
+      // The | is at the 1000-character mark.
+      const body = dedent`
+        Lorem ipsum dolor sit amet, et his justo deleniti, omnium fastidii adversarium at has.
+
+        Mazim alterum sea ea, essent malorum persius ne mei. Nam ea tempor qualisque, modus doming te has. Affert dolore
+        albucius te vis, eam tantas nullam corrumpit ad, in oratio luptatum eleifend vim. Ea salutatus contentiones eos.
+        Eam in veniam facete volutpat, solum appetere adversarium ut quo. Vel cu appetere urbanitas, usu ut aperiri
+        mediocritatem, alia molestie urbanitas cu qui. Velit antiopam erroribus no eum, scripta iudicabit ne nam, in
+        duis clita commodo sit. Assum sensibus oportere te vel, vis semper evertitur definiebas in. Tamquam feugiat
+        comprehensam ut his, et eum voluptua ullamcorper, ex mei debitis inciderint. Sit discere pertinax te, an mei
+        liber putant. Ad doctus tractatos ius, duo ad civibus alienum, nominati voluptaria sed an. Libris essent
+        philosophia et vix. Nusquam reprehendunt et mea. Ea eius omnes voluptua sit. No cum illud verear efficiantur. Id
+        altera imperdiet nec. Noster audia|m accusamus mei at, no zril libris nemore duo, ius ne rebum doctus fuisset.
+        Legimus epicurei in sit, esse purto suscipit eu qui, oporteat deserunt delicatissimi sea in. Est id putent
+        accusata convenire, no tibique molestie accommodare quo, cu est fuisset offendit evertitur.
+      `;
+
+      const commit = commitBuilder().messageBody(body).build();
+      assert.strictEqual(commit.abbreviatedBody(), dedent`
+        Lorem ipsum dolor sit amet, et his justo deleniti, omnium fastidii adversarium at has.
+
+        Mazim alterum sea ea, essent malorum persius ne mei. Nam ea tempor qualisque, modus doming te has. Affert dolore
+        albucius te vis, eam tantas nullam corrumpit ad, in oratio luptatum eleifend vim. Ea salutatus contentiones eos.
+        Eam in veniam facete volutpat, solum appetere adversarium ut quo. Vel cu appetere urbanitas, usu ut aperiri
+        mediocritatem, alia molestie urbanitas cu qui. Velit antiopam erroribus no eum, scripta iudicabit ne nam, in
+        duis clita commodo sit. Assum sensibus oportere te vel, vis semper evertitur definiebas in. Tamquam feugiat
+        comprehensam ut his, et eum voluptua ullamcorper, ex mei debitis inciderint. Sit discere pertinax te, an mei
+        liber putant. Ad doctus tractatos ius, duo ad civibus alienum, nominati voluptaria sed an. Libris essent
+        philosophia et vix. Nusquam reprehendunt et mea. Ea eius omnes voluptua sit. No cum illud verear efficiantur. Id
+        altera imperdiet nec. Noster...
+      `);
+    });
+
+    it('truncates the message body at the character cutoff if no word or paragraph boundaries can be found', function() {
+      // The | is at the 1000-character mark.
+      const body = 'Loremipsumdolorsitamet,ethisjustodeleniti,omniumfastidiiadversariumathas.\n\n' +
+        'Mazim alterumseaea,essentmalorumpersiusnemei.Nameatemporqualisque,modusdomingtehas.Affertdolore' +
+        'albuciustevis,eamtantasnullamcorrumpitad,inoratioluptatumeleifendvim.Easalutatuscontentioneseos.' +
+        'Eaminveniamfacetevolutpat,solumappetereadversariumutquo.Velcuappetereurbanitas,usuutaperiri' +
+        'mediocritatem,aliamolestieurbanitascuqui.Velitantiopamerroribusnoeum,scriptaiudicabitnenam,in' +
+        'duisclitacommodosit.Assumsensibusoporteretevel,vissemperevertiturdefiniebasin.Tamquamfeugiat' +
+        'comprehensamuthis,eteumvoluptuaullamcorper,exmeidebitisinciderint.Sitdiscerepertinaxte,anmei' +
+        'liberputant.Addoctustractatosius,duoadcivibusalienum,nominativoluptariasedan.Librisessent' +
+        'philosophiaetvix.Nusquamreprehenduntetmea.Eaeiusomnesvoluptuasit.Nocumilludverearefficiantur.Id' +
+        'alteraimperdietnec.Nosteraudiamaccusamusmeiat,nozrillibrisnemoreduo,iusnerebumdoctusfuisset.' +
+        'Legimusepicureiinsit,essepurtosuscipiteuqui,oporteatdeseruntdelicatissimiseain.Estidputent' +
+        '|accusataconvenire,notibiquemolestieaccommodarequo,cuestfuissetoffenditevertitur.';
+
+      const commit = commitBuilder().messageBody(body).build();
+      assert.strictEqual(
+        commit.abbreviatedBody(),
+        'Loremipsumdolorsitamet,ethisjustodeleniti,omniumfastidiiadversariumathas.\n\n' +
+        'Mazim alterumseaea,essentmalorumpersiusnemei.Nameatemporqualisque,modusdomingtehas.Affertdolore' +
+        'albuciustevis,eamtantasnullamcorrumpitad,inoratioluptatumeleifendvim.Easalutatuscontentioneseos.' +
+        'Eaminveniamfacetevolutpat,solumappetereadversariumutquo.Velcuappetereurbanitas,usuutaperiri' +
+        'mediocritatem,aliamolestieurbanitascuqui.Velitantiopamerroribusnoeum,scriptaiudicabitnenam,in' +
+        'duisclitacommodosit.Assumsensibusoporteretevel,vissemperevertiturdefiniebasin.Tamquamfeugiat' +
+        'comprehensamuthis,eteumvoluptuaullamcorper,exmeidebitisinciderint.Sitdiscerepertinaxte,anmei' +
+        'liberputant.Addoctustractatosius,duoadcivibusalienum,nominativoluptariasedan.Librisessent' +
+        'philosophiaetvix.Nusquamreprehenduntetmea.Eaeiusomnesvoluptuasit.Nocumilludverearefficiantur.Id' +
+        'alteraimperdietnec.Nosteraudiamaccusamusmeiat,nozrillibrisnemoreduo,iusnerebumdoctusfuisset.' +
+        'Legimusepicureiinsit,essepurtosuscipiteuqui,oporteatdeseruntdelicatissimiseain.Estidput...',
+      );
+    });
+  });
 });
