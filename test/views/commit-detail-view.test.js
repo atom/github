1a28b7d3a7b3a62615690b605dc8ad54367e1a0d
Ash Wilson <smashwilson@github.com>
2018-11-28 14:56:59 -0500

    Ported CommitDetailView tests, all passing

--- test/views/commit-detail-view.test.js
+++ test/views/commit-detail-view.test.js
@@ -1,69 +1,90 @@
 import React from 'react';
 import {shallow} from 'enzyme';
+import moment from 'moment';
+
+import CommitDetailView from '../../lib/views/commit-detail-view';
+import CommitDetailItem from '../../lib/items/commit-detail-item';
+import {cloneRepository, buildRepository} from '../helpers';
+import {commitBuilder} from '../builder/commit';
 
 describe('CommitDetailView', function() {
-  it('has a MultiFilePatchController that its itemType set');
+  let repository, atomEnv;
 
-  it('passes unrecognized props to a MultiFilePatchController');
-
-  it('renders commit details properly');
-
-  it('renders multiple avatars for co-authored commit');
-});
-
-/*
-it('has a MultiFilePatchController that has `disableStageUnstage` flag set to true', function() {
-  const wrapper = mount(buildApp());
-  assert.isTrue(wrapper.find('MultiFilePatchController').exists());
-  assert.isTrue(wrapper.find('MultiFilePatchController').prop('disableStageUnstage'));
-});
-
-it('passes unrecognized props to a MultiFilePatchController', function() {
-  const extra = Symbol('extra');
-  const wrapper = shallow(buildApp({extra}));
-
-  assert.strictEqual(wrapper.find('MultiFilePatchController').prop('extra'), extra);
-});
-
-it('renders commit details properly', function() {
-  const newCommit = new Commit({
-    sha: '420',
-    authorEmail: 'very@nice.com',
-    authorDate: moment().subtract(2, 'days').unix(),
-    messageSubject: 'subject',
-    messageBody: 'messageBody',
+  beforeEach(async function() {
+    atomEnv = global.buildAtomEnvironment();
+    repository = await buildRepository(await cloneRepository('multiple-commits'));
   });
-  const {multiFilePatch: mfp} = multiFilePatchBuilder().addFilePatch().build();
-  sinon.stub(newCommit, 'getMultiFileDiff').returns(mfp);
-  const wrapper = mount(buildApp({commit: newCommit}));
 
-  assert.strictEqual(wrapper.find('.github-CommitDetailView-title').text(), 'subject');
-  assert.strictEqual(wrapper.find('.github-CommitDetailView-moreText').text(), 'messageBody');
-  assert.strictEqual(wrapper.find('.github-CommitDetailView-metaText').text(), 'very@nice.com committed 2 days ago');
-  assert.strictEqual(wrapper.find('.github-CommitDetailView-sha').text(), '420');
-  // assert.strictEqual(wrapper.find('.github-CommitDetailView-sha a').prop('href'), '420');
-  assert.strictEqual(wrapper.find('img.github-RecentCommit-avatar').prop('src'), 'https://avatars.githubusercontent.com/u/e?email=very%40nice.com&s=32');
-});
-
-it('renders multiple avatars for co-authored commit', function() {
-  const newCommit = new Commit({
-    sha: '420',
-    authorEmail: 'very@nice.com',
-    authorDate: moment().subtract(2, 'days').unix(),
-    messageSubject: 'subject',
-    messageBody: 'messageBody',
-    coAuthors: [{name: 'two', email: 'two@coauthor.com'}, {name: 'three', email: 'three@coauthor.com'}],
+  afterEach(function() {
+    atomEnv.destroy();
   });
-  const {multiFilePatch: mfp} = multiFilePatchBuilder().addFilePatch().build();
-  sinon.stub(newCommit, 'getMultiFileDiff').returns(mfp);
-  const wrapper = mount(buildApp({commit: newCommit}));
-  assert.deepEqual(
-    wrapper.find('img.github-RecentCommit-avatar').map(w => w.prop('src')),
-    [
+
+  function buildApp(override = {}) {
+    const props = {
+      repository,
+      commit: commitBuilder().build(),
+      itemType: CommitDetailItem,
+
+      workspace: atomEnv.workspace,
+      commands: atomEnv.commands,
+      keymaps: atomEnv.keymaps,
+      tooltips: atomEnv.tooltips,
+      config: atomEnv.config,
+
+      destroy: () => {},
+      ...override,
+    };
+
+    return <CommitDetailView {...props} />;
+  }
+
+  it('has a MultiFilePatchController that its itemType set', function() {
+    const wrapper = shallow(buildApp({itemType: CommitDetailItem}));
+    assert.strictEqual(wrapper.find('MultiFilePatchController').prop('itemType'), CommitDetailItem);
+  });
+
+  it('passes unrecognized props to a MultiFilePatchController', function() {
+    const extra = Symbol('extra');
+    const wrapper = shallow(buildApp({extra}));
+    assert.strictEqual(wrapper.find('MultiFilePatchController').prop('extra'), extra);
+  });
+
+  it('renders commit details properly', function() {
+    const commit = commitBuilder()
+      .sha('420')
+      .authorEmail('very@nice.com')
+      .authorDate(moment().subtract(2, 'days').unix())
+      .messageSubject('subject')
+      .messageBody('body')
+      .setMultiFileDiff()
+      .build();
+    const wrapper = shallow(buildApp({commit}));
+
+    assert.strictEqual(wrapper.find('.github-CommitDetailView-title').text(), 'subject');
+    assert.strictEqual(wrapper.find('.github-CommitDetailView-moreText').text(), 'body');
+    assert.strictEqual(wrapper.find('.github-CommitDetailView-metaText').text(), 'very@nice.com committed 2 days ago');
+    assert.strictEqual(wrapper.find('.github-CommitDetailView-sha').text(), '420');
+    // assert.strictEqual(wrapper.find('.github-CommitDetailView-sha a').prop('href'), '420');
+    assert.strictEqual(
+      wrapper.find('img.github-RecentCommit-avatar').prop('src'),
       'https://avatars.githubusercontent.com/u/e?email=very%40nice.com&s=32',
-      'https://avatars.githubusercontent.com/u/e?email=two%40coauthor.com&s=32',
-      'https://avatars.githubusercontent.com/u/e?email=three%40coauthor.com&s=32',
-    ],
-  );
+    );
+  });
+
+  it('renders multiple avatars for co-authored commit', function() {
+    const commit = commitBuilder()
+      .authorEmail('blaze@it.com')
+      .addCoAuthor('two', 'two@coauthor.com')
+      .addCoAuthor('three', 'three@coauthor.com')
+      .build();
+    const wrapper = shallow(buildApp({commit}));
+    assert.deepEqual(
+      wrapper.find('img.github-RecentCommit-avatar').map(w => w.prop('src')),
+      [
+        'https://avatars.githubusercontent.com/u/e?email=blaze%40it.com&s=32',
+        'https://avatars.githubusercontent.com/u/e?email=two%40coauthor.com&s=32',
+        'https://avatars.githubusercontent.com/u/e?email=three%40coauthor.com&s=32',
+      ],
+    );
+  });
 });
-*/
