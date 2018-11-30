d5efe90bd53e024e4deccf0c8ddf549a4ab7a7ec
Katrina Uychaco <kuychaco@gmail.com>
2018-11-29 17:57:14 -0800

    Add test for RecentCommitsController#openCommit, including event recording

--- test/controllers/recent-commits-controller.test.js
+++ test/controllers/recent-commits-controller.test.js
@@ -3,6 +3,8 @@ import {shallow} from 'enzyme';
 
 import RecentCommitsController from '../../lib/controllers/recent-commits-controller';
 import Commit from '../../lib/models/commit';
+import {cloneRepository, buildRepository} from '../helpers';
+import * as reporterProxy from '../../lib/reporter-proxy';
 
 describe('RecentCommitsController', function() {
   let app;
@@ -22,5 +24,47 @@ describe('RecentCommitsController', function() {
     app = React.cloneElement(app, {isLoading: true});
     const wrapper = shallow(app);
     assert.isTrue(wrapper.find('RecentCommitsView').prop('isLoading'));
+  });
+
+  describe('openCommit({sha})', function() {
+    let atomEnv, workdirPath, repository;
+
+    beforeEach(async function() {
+      atomEnv = global.buildAtomEnvironment();
+      workdirPath = await cloneRepository();
+      repository = await buildRepository(workdirPath);
+    });
+
+    afterEach(function() {
+      atomEnv.destroy();
+    });
+
+    it('opens a commit detail item', function() {
+      sinon.stub(atomEnv.workspace, 'open').resolves();
+
+      const sha = 'asdf1234';
+      const commits = [new Commit({sha})];
+      app = React.cloneElement(app, {commits, workspace: atomEnv.workspace, repository});
+      const wrapper = shallow(app);
+      wrapper.instance().openCommit({sha: 'asdf1234'});
+
+      assert.isTrue(atomEnv.workspace.open.calledWith(
+        `atom-github://commit-detail?workdir=${encodeURIComponent(workdirPath)}` +
+        `&sha=${encodeURIComponent(sha)}`,
+      ));
+    });
+
+    it('records an event', async function() {
+      sinon.stub(atomEnv.workspace, 'open').resolves();
+      sinon.stub(reporterProxy, 'addEvent');
+
+      const sha = 'asdf1234';
+      const commits = [new Commit({sha})];
+      app = React.cloneElement(app, {commits, workspace: atomEnv.workspace, repository});
+      const wrapper = shallow(app);
+
+      await wrapper.instance().openCommit({sha: 'asdf1234'});
+      assert.isTrue(reporterProxy.addEvent.calledWith('open-commit-in-pane', {package: 'github', from: RecentCommitsController.name}));
+    });
   });
 });
