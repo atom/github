54163061b9ec3852ea8ac7a6e8f2dd5a14574785
Katrina Uychaco <kuychaco@gmail.com>
2018-11-30 19:32:57 -0800

    Add OpenCommitDialog tests

---
+++ test/views/open-commit-dialog.test.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import {mount} from 'enzyme';
+import path from 'path';
+
+import OpenCommitDialog from '../../lib/views/open-commit-dialog';
+
+describe('OpenCommitDialog', function() {
+  let atomEnv, config, commandRegistry;
+  let app, wrapper, didAccept, didCancel;
+
+  beforeEach(function() {
+    atomEnv = global.buildAtomEnvironment();
+    config = atomEnv.config;
+    commandRegistry = atomEnv.commands;
+    sinon.stub(config, 'get').returns(path.join('home', 'me', 'codes'));
+
+    didAccept = sinon.stub();
+    didCancel = sinon.stub();
+
+    app = (
+      <OpenCommitDialog
+        config={config}
+        commandRegistry={commandRegistry}
+        didAccept={didAccept}
+        didCancel={didCancel}
+      />
+    );
+    wrapper = mount(app);
+  });
+
+  afterEach(function() {
+    atomEnv.destroy();
+  });
+
+  const setTextIn = function(selector, text) {
+    wrapper.find(selector).getDOMNode().getModel().setText(text);
+  };
+
+  describe('entering a commit sha', function() {
+    it("updates the project path automatically if it hasn't been modified", function() {
+      setTextIn('.github-CommitSha atom-text-editor', 'asdf1234');
+
+      assert.equal(wrapper.instance().getCommitSha(), 'asdf1234');
+    });
+
+    it('does update the sha if it was modified automatically', function() {
+      setTextIn('.github-CommitSha atom-text-editor', 'asdf1234');
+      assert.equal(wrapper.instance().getCommitSha(), 'asdf1234');
+
+      setTextIn('.github-CommitSha atom-text-editor', 'zxcv5678');
+      assert.equal(wrapper.instance().getCommitSha(), 'zxcv5678');
+    });
+  });
+
+  describe('open button enablement', function() {
+    it('disables the open button with no commit sha', function() {
+      setTextIn('.github-CommitSha atom-text-editor', '');
+      wrapper.update();
+
+      assert.isTrue(wrapper.find('button.icon-commit').prop('disabled'));
+    });
+
+    it('enables the open button when commit sha box is populated', function() {
+      setTextIn('.github-CommitSha atom-text-editor', 'asdf1234');
+      wrapper.update();
+
+      assert.isFalse(wrapper.find('button.icon-commit').prop('disabled'));
+    });
+  });
+
+  it('calls the acceptance callback', function() {
+    setTextIn('.github-CommitSha atom-text-editor', 'asdf1234');
+
+    wrapper.find('button.icon-commit').simulate('click');
+
+    assert.isTrue(didAccept.calledWith({sha: 'asdf1234'}));
+  });
+
+  it('calls the cancellation callback', function() {
+    wrapper.find('button.github-CancelButton').simulate('click');
+    assert.isTrue(didCancel.called);
+  });
+});
