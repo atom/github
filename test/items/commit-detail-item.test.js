f5ca14ca66d2e688a0c87d9b5279ca802c92610e
Ash Wilson <smashwilson@github.com>
2018-12-03 14:24:27 -0500

    Remove unused variables

--- test/items/commit-detail-item.test.js
+++ test/items/commit-detail-item.test.js
@@ -53,7 +53,7 @@ describe('CommitDetailItem', function() {
     );
   }
 
-  function open(wrapper, options = {}) {
+  function open(options = {}) {
     const opts = {
       workingDirectory: repository.getWorkingDirectoryPath(),
       sha: '18920c900bfa6e4844853e7e246607a31c3e2e8c',
@@ -65,7 +65,7 @@ describe('CommitDetailItem', function() {
 
   it('constructs and opens the correct URI', async function() {
     const wrapper = mount(buildPaneApp());
-    await open(wrapper);
+    await open();
 
     assert.isTrue(wrapper.update().find('CommitDetailItem').exists());
   });
@@ -73,20 +73,20 @@ describe('CommitDetailItem', function() {
   it('passes extra props to its container', async function() {
     const extra = Symbol('extra');
     const wrapper = mount(buildPaneApp({extra}));
-    await open(wrapper);
+    await open();
 
     assert.strictEqual(wrapper.update().find('CommitDetailItem').prop('extra'), extra);
   });
 
   it('serializes itself as a CommitDetailItem', async function() {
     const wrapper = mount(buildPaneApp());
-    const item0 = await open(wrapper, {workingDirectory: '/dir0', sha: '420'});
+    const item0 = await open({workingDirectory: '/dir0', sha: '420'});
     assert.deepEqual(item0.serialize(), {
       deserializer: 'CommitDetailStub',
       uri: 'atom-github://commit-detail?workdir=%2Fdir0&sha=420',
     });
 
-    const item1 = await open(wrapper, {workingDirectory: '/dir1', sha: '1337'});
+    const item1 = await open({workingDirectory: '/dir1', sha: '1337'});
     assert.deepEqual(item1.serialize(), {
       deserializer: 'CommitDetailStub',
       uri: 'atom-github://commit-detail?workdir=%2Fdir1&sha=1337',
@@ -95,30 +95,30 @@ describe('CommitDetailItem', function() {
 
   it('locates the repository from the context pool', async function() {
     const wrapper = mount(buildPaneApp());
-    await open(wrapper);
+    await open();
 
     assert.strictEqual(wrapper.update().find('CommitDetailContainer').prop('repository'), repository);
   });
 
   it('passes an absent repository if the working directory is unrecognized', async function() {
     const wrapper = mount(buildPaneApp());
-    await open(wrapper, {workingDirectory: '/nah'});
+    await open({workingDirectory: '/nah'});
 
     assert.isTrue(wrapper.update().find('CommitDetailContainer').prop('repository').isAbsent());
   });
 
   it('returns a fixed title and icon', async function() {
-    const wrapper = mount(buildPaneApp());
-    const item = await open(wrapper, {sha: '1337'});
+    mount(buildPaneApp());
+    const item = await open({sha: '1337'});
 
     assert.strictEqual(item.getTitle(), 'Commit: 1337');
     assert.strictEqual(item.getIconName(), 'git-commit');
   });
 
   it('terminates pending state', async function() {
-    const wrapper = mount(buildPaneApp());
+    mount(buildPaneApp());
 
-    const item = await open(wrapper);
+    const item = await open();
     const callback = sinon.spy();
     const sub = item.onDidTerminatePendingState(callback);
 
@@ -132,9 +132,9 @@ describe('CommitDetailItem', function() {
   });
 
   it('may be destroyed once', async function() {
-    const wrapper = mount(buildPaneApp());
+    mount(buildPaneApp());
 
-    const item = await open(wrapper);
+    const item = await open();
     const callback = sinon.spy();
     const sub = item.onDidDestroy(callback);
 
@@ -146,8 +146,8 @@ describe('CommitDetailItem', function() {
   });
 
   it('has an item-level accessor for the current working directory & sha', async function() {
-    const wrapper = mount(buildPaneApp());
-    const item = await open(wrapper, {workingDirectory: '/dir7', sha: '420'});
+    mount(buildPaneApp());
+    const item = await open({workingDirectory: '/dir7', sha: '420'});
     assert.strictEqual(item.getWorkingDirectory(), '/dir7');
     assert.strictEqual(item.getSha(), '420');
   });
