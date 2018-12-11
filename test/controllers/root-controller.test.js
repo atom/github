b329b413f4b63aa7faac72103668930f5a524729
Katrina Uychaco <kuychaco@gmail.com>
2018-12-10 17:45:53 -0800

    Add RootController tess for `isValidCommit`
    # 50-character subject line
    #
    # 72-character wrapped longer description. This should answer:
    #
    # * Why was this change necessary?
    # * How does it address the problem?
    # * Are there any side effects?
    #
    # Include a link to the ticket, if any.
    #
    # Don't forget the following emoji:
    #
    # * :art: `:art:` when improving the format/structure of the code
    # * :racehorse: `:racehorse:` when improving performance
    # * :non-potable_water: `:non-potable_water:` when plugging memory leaks
    # * :memo: `:memo:` when writing docs
    # * :penguin: `:penguin:` when fixing something on Linux
    # * :apple: `:apple:` when fixing something on Mac OS
    # * :checkered_flag: `:checkered_flag:` when fixing something on Windows
    # * :bug: `:bug:` when fixing a bug
    # * :fire: `:fire:` when removing code or files
    # * :green_heart: `:green_heart:` when fixing the CI build
    # * :white_check_mark: `:white_check_mark:` when adding tests
    # * :lock: `:lock:` when dealing with security
    # * :arrow_up: `:arrow_up:` when upgrading dependencies
    # * :arrow_down: `:arrow_down:` when downgrading dependencies
    # * :shirt: `:shirt:` when removing linter warnings

--- test/controllers/root-controller.test.js
+++ test/controllers/root-controller.test.js
@@ -326,7 +326,7 @@ describe('RootController', function() {
   });
 
   describe('github:open-commit', function() {
-    let workdirPath, wrapper, openCommitDetails, resolveOpenCommit;
+    let workdirPath, wrapper, openCommitDetails, resolveOpenCommit, repository;
 
     beforeEach(async function() {
       openCommitDetails = sinon.stub(atomEnv.workspace, 'open').returns(new Promise(resolve => {
@@ -334,7 +334,7 @@ describe('RootController', function() {
       }));
 
       workdirPath = await cloneRepository('multiple-commits');
-      const repository = await buildRepository(workdirPath);
+      repository = await buildRepository(workdirPath);
 
       app = React.cloneElement(app, {repository});
       wrapper = shallow(app);
@@ -377,6 +377,18 @@ describe('RootController', function() {
       assert.lengthOf(wrapper.find('OpenCommitDialog'), 0);
       assert.isFalse(openCommitDetails.called);
       assert.isFalse(wrapper.state('openCommitDialogActive'));
+    });
+
+    describe('isValidCommit', function() {
+      it('returns true if commit exists in repo, false if not', async function() {
+        assert.isTrue(await wrapper.instance().isValidCommit('HEAD'));
+        assert.isFalse(await wrapper.instance().isValidCommit('invalidCommitRef'));
+      });
+
+      it('re-throws exceptions encountered during validation check', async function() {
+        sinon.stub(repository, 'getCommit').throws(new Error('Oh shit'));
+        await assert.isRejected(wrapper.instance().isValidCommit('HEAD'), 'Oh shit');
+      });
     });
   });
 
