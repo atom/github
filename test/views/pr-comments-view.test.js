ffbf81a887cb56430c6cb74c1d4da4ca5b3a2730
Ash Wilson <smashwilson@github.com>
2018-12-21 13:50:53 -0500

    Use `getBufferRowForDiffPosition` to position review comments

--- test/views/pr-comments-view.test.js
+++ test/views/pr-comments-view.test.js
@@ -9,25 +9,32 @@ describe('PrCommentsView', function() {
   it('adjusts the position for comments after hunk headers', function() {
     const {multiFilePatch} = multiFilePatchBuilder()
       .addFilePatch(fp => {
-        fp.setOldFile(f => f.path('file.txt'));
-        fp.addHunk(h => h.oldRow(5).unchanged('1').added('2', '3', '4').unchanged('5'));
-        fp.addHunk(h => h.oldRow(20).unchanged('7').deleted('8', '9', '10').unchanged('11'));
-        fp.addHunk(h => h.oldRow(30).unchanged('13').added('14', '15').deleted('16').unchanged('17'));
+        fp.setOldFile(f => f.path('file0.txt'));
+        fp.addHunk(h => h.oldRow(5).unchanged('0 (1)').added('1 (2)', '2 (3)', '3 (4)').unchanged('4 (5)'));
+        fp.addHunk(h => h.oldRow(20).unchanged('5 (7)').deleted('6 (8)', '7 (9)', '8 (10)').unchanged('9 (11)'));
+        fp.addHunk(h => {
+          h.oldRow(30).unchanged('10 (13)').added('11 (14)', '12 (15)').deleted('13 (16)').unchanged('14 (17)');
+        });
+      })
+      .addFilePatch(fp => {
+        fp.setOldFile(f => f.path('file1.txt'));
+        fp.addHunk(h => h.oldRow(5).unchanged('15 (1)').added('16 (2)').unchanged('17 (3)'));
+        fp.addHunk(h => h.oldRow(20).unchanged('18 (5)').deleted('19 (6)', '20 (7)', '21 (8)').unchanged('22 (9)'));
       })
       .build();
 
     const pr = pullRequestBuilder()
       .addReview(r => {
-        r.addComment(c => c.id(0).path('file.txt').position(2).body('one'));
-        r.addComment(c => c.id(1).path('file.txt').position(9).body('two'));
-        r.addComment(c => c.id(2).path('file.txt').position(15).body('three'));
+        r.addComment(c => c.id(0).path('file0.txt').position(2).body('one'));
+        r.addComment(c => c.id(1).path('file0.txt').position(15).body('three'));
+        r.addComment(c => c.id(1).path('file1.txt').position(7).body('three'));
       })
       .build();
 
     const wrapper = shallow(<PrCommentsView multiFilePatch={multiFilePatch} reviews={pr.reviews} />);
 
     assert.deepEqual(wrapper.find('Marker').at(0).prop('bufferRange').serialize(), [[1, 0], [1, 0]]);
-    assert.deepEqual(wrapper.find('Marker').at(1).prop('bufferRange').serialize(), [[7, 0], [7, 0]]);
-    assert.deepEqual(wrapper.find('Marker').at(2).prop('bufferRange').serialize(), [[12, 0], [12, 0]]);
+    assert.deepEqual(wrapper.find('Marker').at(1).prop('bufferRange').serialize(), [[12, 0], [12, 0]]);
+    assert.deepEqual(wrapper.find('Marker').at(2).prop('bufferRange').serialize(), [[20, 0], [20, 0]]);
   });
 });
