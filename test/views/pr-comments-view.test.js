8055ee33d996db40c073bcca569a100ca757abc8
Tilde Ann Thurium <annthurium@github.com>
2018-12-27 11:46:57 -0800

    add test for comment with position: null

--- test/views/pr-comments-view.test.js
+++ test/views/pr-comments-view.test.js
@@ -37,4 +37,29 @@ describe('PrCommentsView', function() {
     assert.deepEqual(wrapper.find('Marker').at(1).prop('bufferRange').serialize(), [[12, 0], [12, 0]]);
     assert.deepEqual(wrapper.find('Marker').at(2).prop('bufferRange').serialize(), [[20, 0], [20, 0]]);
   });
+  it('does not render comment if position is null', function() {
+    const {multiFilePatch} = multiFilePatchBuilder()
+      .addFilePatch(fp => {
+        fp.setOldFile(f => f.path('file0.txt'));
+        fp.addHunk(h => h.oldRow(5).unchanged('0 (1)').added('1 (2)', '2 (3)', '3 (4)').unchanged('4 (5)'));
+        fp.addHunk(h => h.oldRow(20).unchanged('5 (7)').deleted('6 (8)', '7 (9)', '8 (10)').unchanged('9 (11)'));
+        fp.addHunk(h => {
+          h.oldRow(30).unchanged('10 (13)').added('11 (14)', '12 (15)').deleted('13 (16)').unchanged('14 (17)');
+        });
+      })
+      .build();
+
+    const pr = pullRequestBuilder()
+      .addReview(r => {
+        r.addComment(c => c.id(0).path('file0.txt').position(2).body('one'));
+        r.addComment(c => c.id(1).path('file0.txt').position(null).body('three'));
+      })
+      .build();
+
+    const wrapper = shallow(<PrCommentsView multiFilePatch={multiFilePatch} reviews={pr.reviews} />);
+
+    const comments = wrapper.find('PullRequestCommentView');
+    assert.lengthOf(comments, 1);
+    assert.strictEqual(comments.at(0).prop('comment').body, 'one');
+  });
 });
