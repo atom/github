0bfacb6bbf77398e4ef71ccc73531f4644e1aa1a
Ash Wilson <smashwilson@github.com>
2019-01-16 15:25:34 -0500

    Track changed renderStatus in CommitPreviewContainer

--- test/containers/commit-preview-container.test.js
+++ test/containers/commit-preview-container.test.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import {mount} from 'enzyme';
+import fs from 'fs-extra';
+import path from 'path';
 
 import CommitPreviewContainer from '../../lib/containers/commit-preview-container';
 import CommitPreviewItem from '../../lib/items/commit-preview-item';
+import {TOO_LARGE, EXPANDED} from '../../lib/models/patch/patch';
 import {cloneRepository, buildRepository} from '../helpers';
 
 describe('CommitPreviewContainer', function() {
@@ -70,5 +73,32 @@ describe('CommitPreviewContainer', function() {
     const wrapper = mount(buildApp());
     await assert.async.isTrue(wrapper.update().find('CommitPreviewController').exists());
     assert.strictEqual(wrapper.find('CommitPreviewController').prop('multiFilePatch'), patch);
+  });
+
+  it('remembers previously expanded large patches', async function() {
+    const wd = repository.getWorkingDirectoryPath();
+    await repository.getLoadPromise();
+
+    await fs.writeFile(path.join(wd, 'file-0.txt'), '0\n1\n2\n3\n4\n5\n', {encoding: 'utf8'});
+    await fs.writeFile(path.join(wd, 'file-1.txt'), '0\n1\n2\n', {encoding: 'utf8'});
+    await repository.stageFiles(['file-0.txt', 'file-1.txt']);
+
+    repository.refresh();
+
+    const wrapper = mount(buildApp({largeDiffThreshold: 3}));
+    await assert.async.isTrue(wrapper.update().exists('CommitPreviewController'));
+
+    const before = wrapper.find('CommitPreviewController').prop('multiFilePatch');
+    assert.strictEqual(before.getFilePatches()[0].getRenderStatus(), TOO_LARGE);
+    assert.strictEqual(before.getFilePatches()[1].getRenderStatus(), EXPANDED);
+
+    before.getFilePatches()[0].triggerDelayedRender();
+    repository.refresh();
+
+    await assert.async.notStrictEqual(wrapper.update().find('CommitPreviewController').prop('multiFilePatch'), before);
+    const after = wrapper.find('CommitPreviewController').prop('multiFilePatch');
+
+    assert.strictEqual(after.getFilePatches()[0].getRenderStatus(), EXPANDED);
+    assert.strictEqual(after.getFilePatches()[1].getRenderStatus(), EXPANDED);
   });
 });
