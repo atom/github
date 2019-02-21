662a1ddb259aee79cbd34d670d3fc6823a79f7d8
Ash Wilson <smashwilson@github.com>
2019-02-21 11:01:05 -0500

    Hunk tests :white_check_mark: + :100:

--- test/models/patch/hunk.test.js
+++ test/models/patch/hunk.test.js
@@ -193,6 +193,63 @@ describe('Hunk', function() {
     assert.strictEqual(h1.getMaxLineNumberWidth(), 5);
   });
 
+  it('updates markers from a marker map', function() {
+    const oMarker = buffer.markRange([[0, 0], [10, 4]]);
+
+    const h = new Hunk({
+      oldStartRow: 0,
+      newStartRow: 1,
+      oldRowCount: 2,
+      newRowCount: 3,
+      sectionHeading: 'sectionHeading',
+      marker: oMarker,
+      regions: [
+        new Addition(buffer.markRange([[1, 0], [2, 4]])),
+        new Deletion(buffer.markRange([[3, 0], [4, 4]])),
+        new Deletion(buffer.markRange([[5, 0], [6, 4]])),
+        new Unchanged(buffer.markRange([[7, 0], [10, 4]])),
+      ],
+    });
+
+    h.updateMarkers(new Map());
+    assert.strictEqual(h.getMarker(), oMarker);
+
+    const regionUpdateMaps = h.getRegions().map(r => sinon.spy(r, 'updateMarkers'));
+
+    const layer = buffer.addMarkerLayer();
+    const nMarker = layer.markRange([[0, 0], [10, 4]]);
+    const map = new Map([[oMarker, nMarker]]);
+
+    h.updateMarkers(map);
+
+    assert.strictEqual(h.getMarker(), nMarker);
+    assert.isTrue(regionUpdateMaps.every(spy => spy.calledWith(map)));
+  });
+
+  it('destroys all of its markers', function() {
+    const h = new Hunk({
+      oldStartRow: 0,
+      newStartRow: 1,
+      oldRowCount: 2,
+      newRowCount: 3,
+      sectionHeading: 'sectionHeading',
+      marker: buffer.markRange([[0, 0], [10, 4]]),
+      regions: [
+        new Addition(buffer.markRange([[1, 0], [2, 4]])),
+        new Deletion(buffer.markRange([[3, 0], [4, 4]])),
+        new Deletion(buffer.markRange([[5, 0], [6, 4]])),
+        new Unchanged(buffer.markRange([[7, 0], [10, 4]])),
+      ],
+    });
+
+    const allMarkers = [h.getMarker(), ...h.getRegions().map(r => r.getMarker())];
+    assert.isFalse(allMarkers.some(m => m.isDestroyed()));
+
+    h.destroyMarkers();
+
+    assert.isTrue(allMarkers.every(m => m.isDestroyed()));
+  });
+
   describe('toStringIn()', function() {
     it('prints its header', function() {
       const h = new Hunk({
