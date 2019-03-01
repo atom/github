2e4bb46819f229ab1251208541c701c6f1dd3fc7
annthurium <annthurium@github.com>
2019-03-01 14:11:11 -0800

    fix ReviewsFooterView tests

--- test/views/reviews-footer-view.test.js
+++ test/views/reviews-footer-view.test.js
@@ -18,6 +18,7 @@ describe('ReviewsFooterView', function() {
     const props = {
       commentsResolved: 3,
       totalComments: 4,
+      openReviews: () => {},
       ...override,
     };
 
@@ -27,17 +28,17 @@ describe('ReviewsFooterView', function() {
   it('renders the resolved and total comment counts', function() {
     const wrapper = shallow(buildApp({commentsResolved: 4, totalComments: 7}));
 
-    assert.strictEqual(wrapper.find('.github-PrDetailViewReviews-countNr').text(), '4');
-    assert.strictEqual(wrapper.find('.github-Reviews-countNr').text(), '7');
-    assert.strictEqual(wrapper.find('.github-PrDetailViewReviews-progessBar').prop('value'), 4);
-    assert.strictEqual(wrapper.find('.github-PrDetailViewReviews-progessBar').prop('max'), 7);
+    assert.strictEqual(wrapper.find('.github-ReviewsFooterView-commentsResolved').text(), '4');
+    assert.strictEqual(wrapper.find('.github-ReviewsFooterView-totalComments').text(), '7');
+    assert.strictEqual(wrapper.find('.github-ReviewsFooterView-progessBar').prop('value'), 4);
+    assert.strictEqual(wrapper.find('.github-ReviewsFooterView-progessBar').prop('max'), 7);
   });
 
   it('triggers openReviews on button click', function() {
     const openReviews = sinon.spy();
     const wrapper = shallow(buildApp({openReviews}));
 
-    wrapper.find('.github-PrDetailViewReviews-openReviewsButton').simulate('click');
+    wrapper.find('.github-ReviewsFooterView-openReviewsButton').simulate('click');
 
     assert.isTrue(openReviews.called);
   });
