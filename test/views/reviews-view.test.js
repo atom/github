5065385bf07117d7d00347ca719a322281de22f8
Vanessa Yuen <vanessa.yh.yuen@gmail.com>
2019-03-05 12:32:24 +0100

    got some tests started

--- test/views/reviews-view.test.js
+++ test/views/reviews-view.test.js
@@ -6,6 +6,14 @@ import ReviewsView from '../../lib/views/reviews-view';
 describe('ReviewsView', function() {
   let atomEnv;
 
+  const mockReview = {
+    state: 'APPROVED',
+    author: {avatarUrl: ''},
+    login: {login: 'Monkey King'},
+    submittedAt: new Date(),
+    body: 'wow such changes!',
+  };
+
   beforeEach(function() {
     atomEnv = global.buildAtomEnvironment();
   });
@@ -16,14 +24,35 @@ describe('ReviewsView', function() {
 
   function buildApp(override = {}) {
     const props = {
+      repository: {
+        pullRequest: {}
+      },
       ...override,
     };
 
     return <ReviewsView {...props} />;
   }
 
-  it('renders something', function() {
+  it('renders a PullRequestsReviewsContainer', function() {
     const wrapper = shallow(buildApp());
-    assert.isTrue(wrapper.exists('div'));
+    assert.lengthOf(wrapper.find('ForwardRef(Relay(PullRequestReviewsController))'), 1);
   });
+
+  it('does not render empty summary or comment sections', function() {
+    const wrapper = shallow(buildApp())
+      .find('ForwardRef(Relay(PullRequestReviewsController))')
+      .renderProp('children')({reviews: [], commentThreads: []});
+    assert.lengthOf(wrapper.find('.github-Reviews-section.summaries'), 0);
+    assert.lengthOf(wrapper.find('.github-Reviews-section.comments'), 0);
+  });
+
+  it('renders summary and comment sections', function() {
+    const wrapper = shallow(buildApp())
+      .find('ForwardRef(Relay(PullRequestReviewsController))')
+      .renderProp('children')({reviews: [mockReview], commentThreads: [{}]});
+    assert.lengthOf(wrapper.find('.github-Reviews-section.summaries'), 1);
+    assert.lengthOf(wrapper.find('.github-Reviews-section.comments'), 1);
+  });
+
+
 });
