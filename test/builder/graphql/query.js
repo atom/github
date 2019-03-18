c870edf082c8dd8dc60aa31da7637b60036f151a
Ash Wilson <smashwilson@github.com>
2019-03-18 09:05:17 -0400

    Search result query connection

--- test/builder/graphql/query.js
+++ test/builder/graphql/query.js
@@ -1,9 +1,35 @@
 import {createSpecBuilderClass} from './helpers';
 
 import {RepositoryBuilder} from './repository';
+import {PullRequestBuilder} from './pr';
+
+class SearchResultItemBuilder {
+  static resolve() { return this; }
+
+  constructor(...args) {
+    this.args = args;
+    this._value = null;
+  }
+
+  bePullRequest(block = () => {}) {
+    const b = new PullRequestBuilder(...this.args);
+    block(b);
+    this._value = b.build();
+  }
+
+  build() {
+    return this._value;
+  }
+}
+
+const SearchResultBuilder = createSpecBuilderClass('SearchResultItemCollection', {
+  issueCount: {default: 0},
+  nodes: {linked: SearchResultItemBuilder, plural: true, singularName: 'node'},
+});
 
 const QueryBuilder = createSpecBuilderClass('QueryBuilder', {
   repository: {linked: RepositoryBuilder},
+  search: {linked: SearchResultBuilder},
 });
 
 export function queryBuilder(...nodes) {
