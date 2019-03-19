96ebdc441c20fb25031fc47a2666a9d86b348c52
Ash Wilson <smashwilson@github.com>
2019-03-19 10:54:04 -0400

    Refactor out the union builder pattern

--- test/builder/graphql/issueish.js
+++ test/builder/graphql/issueish.js
@@ -1,33 +1,10 @@
 import {PullRequestBuilder} from './pr';
 import {IssueBuilder} from './issue';
 
-export class IssueishBuilder {
-  static resolve() { return this; }
+import {createUnionBuilderClass} from './union';
 
-  constructor(...args) {
-    this.args = args;
-    this._value = null;
-  }
-
-  beIssue(block = () => {}) {
-    const b = new IssueBuilder(...this.args);
-    block(b);
-    this._value = b.build();
-    return this;
-  }
-
-  bePullRequest(block = () => {}) {
-    const b = new PullRequestBuilder(...this.args);
-    block(b);
-    this._value = b.build();
-    return this;
-  }
-
-  build() {
-    if (this._value === null) {
-      this.beIssue();
-    }
-
-    return this._value;
-  }
-}
+export const IssueishBuilder = createUnionBuilderClass('Issueish', {
+  beIssue: IssueBuilder,
+  bePullRequest: PullRequestBuilder,
+  default: 'beIssue',
+});
