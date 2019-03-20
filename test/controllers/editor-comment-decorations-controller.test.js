91bd3e6314d0aad806f7897f2b11cb7b77ff3789
annthurium <annthurium@github.com>
2019-03-20 16:20:15 -0700

    add some tests for `EditorCommentDecorationsController`

---
+++ test/controllers/editor-comment-decorations-controller.test.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+
+import EditorCommentDecorationsController from '../../lib/controllers/editor-comment-decorations-controller';
+import Decoration from '../../lib/atom/decoration';
+
+describe('EditorCommentDecorationsController', function() {
+  let atomEnv, workspace, editor, wrapper;
+
+  function buildApp(opts = {}) {
+    const props = {
+      comments: [{position: 2}, {position: 3}],
+      ...opts,
+    };
+
+    return (
+      <EditorCommentDecorationsController {...props} />
+    );
+  }
+
+  beforeEach(async function() {
+    atomEnv = global.buildAtomEnvironment();
+    workspace = atomEnv.workspace;
+    editor = await workspace.open('zzz');
+  });
+
+  afterEach(function() {
+    atomEnv.destroy();
+  });
+
+  it('creates line decorations for lines with comments', function() {
+    wrapper = shallow(buildApp({editor}));
+    const decorations = wrapper.find(Decoration);
+    assert.lengthOf(decorations.findWhere(decoration => decoration.prop('type') === 'line'), 2);
+  });
+
+  it('creates gutter decorations for lines with comments', function() {
+    wrapper = shallow(buildApp({editor}));
+    const decorations = wrapper.find(Decoration);
+    assert.lengthOf(decorations.findWhere(decoration => decoration.prop('type') === 'gutter'), 2);
+  });
+
+  it('does not create decorations if no comments exist', function() {
+    wrapper = shallow(buildApp({editor, comments: []}));
+    assert.lengthOf(wrapper.find(Decoration), 0);
+  });
+
+  it('does not create decorations for minimized comments', function() {
+    wrapper = shallow(buildApp({editor, comments: [{isMinimized: true, position: 5}]}));
+    assert.lengthOf(wrapper.find(Decoration), 0);
+  });
+  it('does not create decorations for comments with null position', function() {
+    wrapper = shallow(buildApp({editor, comments: [{position: null}]}));
+    assert.lengthOf(wrapper.find(Decoration), 0);
+  });
+});
