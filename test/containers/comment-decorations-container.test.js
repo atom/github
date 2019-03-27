f579beebde6ccd3947955d1bcd7a8e8717900624
annthurium <annthurium@github.com>
2019-03-27 14:04:42 -0700

    unit test for `CommentDecorationsContainer` happy path
    Co-Authored-By: Vanessa Yuen <vanessayuenn@users.noreply.github.com>

--- test/containers/comment-decorations-container.test.js
+++ test/containers/comment-decorations-container.test.js
@@ -4,14 +4,15 @@ import {QueryRenderer} from 'react-relay';
 
 import {cloneRepository, buildRepository} from '../helpers';
 import {queryBuilder} from '../builder/graphql/query';
+import Branch from '../../lib/models/branch';
+import BranchSet from '../../lib/models/branch-set';
 import GithubLoginModel from '../../lib/models/github-login-model';
 import ObserveModel from '../../lib/views/observe-model';
 import Remote from '../../lib/models/remote';
-import Repository from '../../lib/models/repository';
+import RemoteSet from '../../lib/models/remote-set';
 import {InMemoryStrategy} from '../../lib/shared/keytar-strategy';
 import CommentDecorationsContainer from '../../lib/containers/comment-decorations-container';
 import repositoryQuery from '../../lib/containers/__generated__/commentDecorationsContainerQuery.graphql.js';
-import CommentDecorationsController from '../../lib/controllers/comment-decorations-controller';
 
 describe('CommentDecorationsContainer', function() {
   let atomEnv, workspace, localRepository;
@@ -21,7 +22,6 @@ describe('CommentDecorationsContainer', function() {
     workspace = atomEnv.workspace;
     localRepository = await buildRepository(await cloneRepository());
     await localRepository.getLoadPromise();
-    await localRepository.refresh();
   });
 
   afterEach(function() {
@@ -53,7 +53,31 @@ describe('CommentDecorationsContainer', function() {
   });
 
   describe('when GitHub remote exists', function() {
-    it('passes query results to controller', function() {
+    let localRepo, wrapper, localRepoWrapper;
+    beforeEach(async function() {
+      const loginModel = new GithubLoginModel(InMemoryStrategy);
+      await loginModel.setToken('https://api.github.com', '1234');
+
+      localRepo = await buildRepository(await cloneRepository());
+
+      wrapper = shallow(buildApp({localRepository: localRepo, loginModel}));
+
+      const origin = new Remote('origin', 'git@github.com:atom/github.git');
+      const upstreamBranch = Branch.createRemoteTracking('refs/remotes/origin/master', 'origin', 'refs/heads/master');
+      const branch = new Branch('master', upstreamBranch, upstreamBranch, true);
+
+      const repoData = {
+        branches: new BranchSet([branch]),
+        remotes: new RemoteSet([origin]),
+        currentRemote: origin,
+        workingDirectoryPath: 'path/path',
+      }
+      localRepoWrapper = wrapper.find(ObserveModel).renderProp('children')(repoData);
+
+    })
+    it('makes a relay query', function() {
+      const tokenWrapper = localRepoWrapper.find(ObserveModel).renderProp('children')({token: '1234'})
+      assert.lengthOf(tokenWrapper.find(QueryRenderer), 1);
     });
 
     it('renders nothing if query errors', function() {
