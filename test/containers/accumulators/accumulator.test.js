9e156ece251c5e12126c4e40071880e6658e9e97
Ash Wilson <smashwilson@github.com>
2019-03-28 10:23:14 -0400

    Accumulator tests :white_check_mark: + :100:
    Also correct and test some behavior related to unmounting an Accumulator 
    mid-update.

--- test/containers/accumulators/accumulator.test.js
+++ test/containers/accumulators/accumulator.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {shallow} from 'enzyme';
+import {Disposable} from 'event-kit';
 
 import Accumulator from '../../../lib/containers/accumulators/accumulator';
 
@@ -9,10 +10,15 @@ describe('Accumulator', function() {
       resultBatch: [],
       pageSize: 10,
       waitTimeMs: 0,
+      onDidRefetch: () => new Disposable(),
+      children: () => {},
       ...override,
       relay: {
         hasMore: () => false,
-        loadMore: cb => cb(null),
+        loadMore: (_pageSize, cb) => {
+          cb(null);
+          return new Disposable();
+        },
         isLoading: () => false,
         ...(override.relay || {}),
       },
@@ -48,6 +54,7 @@ describe('Accumulator', function() {
           loadMoreCallback = null;
           callback(null);
         };
+        return new Disposable();
       },
       isLoading: () => false,
     };
@@ -55,12 +62,12 @@ describe('Accumulator', function() {
     const wrapper = shallow(buildApp({relay, resultBatch: [1, 2, 3], children: fn}));
     assert.isTrue(fn.calledWith(null, [1, 2, 3], true));
 
-    wrapper.setProps({resultBatch: [4, 5, 6]});
+    wrapper.setProps({resultBatch: [1, 2, 3, 4, 5, 6]});
     loadMoreCallback();
     assert.isTrue(fn.calledWith(null, [1, 2, 3, 4, 5, 6], true));
 
     hasMore = false;
-    wrapper.setProps({resultBatch: [7, 8, 9]});
+    wrapper.setProps({resultBatch: [1, 2, 3, 4, 5, 6, 7, 8, 9]});
     loadMoreCallback();
     assert.isTrue(fn.calledWith(null, [1, 2, 3, 4, 5, 6, 7, 8, 9], false));
     assert.isNull(loadMoreCallback);
@@ -75,6 +82,7 @@ describe('Accumulator', function() {
       loadMore: (pageSize, callback) => {
         assert.strictEqual(pageSize, 10);
         callback(error);
+        return new Disposable();
       },
     };
 
@@ -82,33 +90,33 @@ describe('Accumulator', function() {
     assert.isTrue(fn.calledWith(error, [], true));
   });
 
-  it('reports results to a non-render callback prop', function() {
-    const fn = sinon.spy();
-    let hasMore = true;
-    let loadMoreCallback = null;
-
+  it('terminates a loadMore call when unmounting', function() {
+    const disposable = {dispose: sinon.spy()};
     const relay = {
-      hasMore: () => hasMore,
-      loadMore: (pageSize, callback) => {
-        assert.strictEqual(pageSize, 10);
-        loadMoreCallback = () => {
-          loadMoreCallback = null;
-          callback(null);
-        };
-      },
+      hasMore: () => true,
+      loadMore: sinon.stub().returns(disposable),
       isLoading: () => false,
     };
 
-    const wrapper = shallow(buildApp({relay, resultBatch: [1, 2, 3], handleResults: fn}));
-    assert.isTrue(fn.calledWith(null, [1, 2, 3], true));
+    const wrapper = shallow(buildApp({relay}));
+    assert.isTrue(relay.loadMore.called);
 
-    wrapper.setProps({resultBatch: [4, 5, 6]});
-    loadMoreCallback();
-    assert.isTrue(fn.calledWith(null, [1, 2, 3, 4, 5, 6], true));
+    wrapper.unmount();
+    assert.isTrue(disposable.dispose.called);
+  });
 
-    hasMore = false;
-    wrapper.setProps({resultBatch: [7, 8, 9]});
-    loadMoreCallback();
-    assert.isTrue(fn.calledWith(null, [1, 2, 3, 4, 5, 6, 7, 8, 9], false));
+  it('cancels a delayed accumulate call when unmounting', function() {
+    const setTimeoutStub = sinon.stub(window, 'setTimeout').returns(123);
+    const clearTimeoutStub = sinon.stub(window, 'clearTimeout');
+
+    const relay = {
+      hasMore: () => true,
+    };
+
+    const wrapper = shallow(buildApp({relay, waitTimeMs: 1000}));
+    assert.isTrue(setTimeoutStub.called);
+
+    wrapper.unmount();
+    assert.isTrue(clearTimeoutStub.calledWith(123));
   });
 });
