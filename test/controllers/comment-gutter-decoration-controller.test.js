694f0fba0c2451d243f5d0dfe57e70e5973e6d28
Vanessa Yuen <vanessa.yh.yuen@gmail.com>
2019-04-01 19:18:01 -0400

    weee more tests

--- test/controllers/comment-gutter-decoration-controller.test.js
+++ test/controllers/comment-gutter-decoration-controller.test.js
@@ -3,14 +3,24 @@ import {shallow} from 'enzyme';
 import CommentGutterDecorationController from '../../lib/controllers/comment-gutter-decoration-controller';
 import {getEndpoint} from '../../lib/models/endpoint';
 import {Range} from 'atom';
+import * as reporterProxy from '../../lib/reporter-proxy';
+import ReviewsItem from '../../lib/items/reviews-item'
 
-describe.only('CommentGutterDecorationController', function() {
-  let atomEnv, workspace, editor, wrapper;
+describe('CommentGutterDecorationController', function() {
+  let atomEnv, workspace, editor;
 
   function buildApp(opts = {}) {
     const props = {
       workspace,
       editor,
+      commentRow: 420,
+      threadId: 'my-thread-will-go-on',
+      extraClasses: ['celine', 'dion'],
+      endpoint: getEndpoint('github.com'),
+      owner: 'owner',
+      repo: 'repo',
+      number: 1337,
+      workdir: 'dir/path',
       ...opts,
     };
     return <CommentGutterDecorationController {...props} />;
@@ -20,17 +30,7 @@ describe.only('CommentGutterDecorationController', function() {
     atomEnv = global.buildAtomEnvironment();
     workspace = atomEnv.workspace;
     editor = await workspace.open(__filename);
-    wrapper = shallow(buildApp({
-      commentRow: 420,
-      threadId: 'my-thread-will-go-on',
-      extraClasses: ['celine', 'dion'],
-      endpoint: getEndpoint('github.com'),
-      owner: 'owner',
-      repo: 'repo',
-      number: 1337,
-      workdir: 'dir/path',
-    }));
-    editor.addGutter({name: 'github-comment-icon'});
+
   });
 
   afterEach(function() {
@@ -39,8 +39,11 @@ describe.only('CommentGutterDecorationController', function() {
 
 
   it('decorates the comment gutter', function() {
+    const wrapper = shallow(buildApp());
+    editor.addGutter({name: 'github-comment-icon'});
     const marker = wrapper.find('Marker');
     const decoration = marker.find('Decoration');
+
     assert.deepEqual(marker.prop('bufferRange'), new Range([420, 0], [420, 0]));
     assert.isTrue(decoration.hasClass('celine'));
     assert.isTrue(decoration.hasClass('dion'));
@@ -49,5 +52,21 @@ describe.only('CommentGutterDecorationController', function() {
 
   });
 
-  it('opens review dock and jumps to thread when clicked');
+  it('opens review dock and jumps to thread when clicked', async function() {
+    sinon.stub(reporterProxy, 'addEvent');
+    const jumpToThread = sinon.spy();
+    sinon.stub(atomEnv.workspace, 'open').resolves({jumpToThread});
+    const wrapper = shallow(buildApp());
+
+    wrapper.find('button.icon-comment').simulate('click');
+    assert.isTrue(atomEnv.workspace.open.calledWith(
+      ReviewsItem.buildURI('github.com', 'owner', 'repo', 1337, 'dir/path'),
+      {searchAllPanes: true},
+    ));
+    await assert.async.isTrue(jumpToThread.calledWith('my-thread-will-go-on'));
+    assert.isTrue(reporterProxy.addEvent.calledWith('open-review-thread', {
+      package: 'github',
+      from: 'CommentGutterDecorationController',
+    }));
+  });
 });
