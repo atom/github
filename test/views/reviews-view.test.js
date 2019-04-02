fc329ec94df9ecbfe9afe98ffee14975e80374d6
Ash Wilson <smashwilson@github.com>
2019-04-02 16:09:56 -0400

    Use global sinon sandbox

--- test/views/reviews-view.test.js
+++ test/views/reviews-view.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {shallow} from 'enzyme';
-import sinon from 'sinon';
 
 import {Command} from '../../lib/atom/commands';
 import ReviewsView from '../../lib/views/reviews-view';
