005883e132bb781e53f7575cac1cad5db545b6a2
Katrina Uychaco <kuychaco@gmail.com>
2019-04-23 13:14:35 -0700

    Test that `showActionsMenu` handler is called when octicon is clicked

--- test/views/issueish-list-view.test.js
+++ test/views/issueish-list-view.test.js
@@ -141,6 +141,20 @@ describe('IssueishListView', function() {
       wrapper.find('.github-IssueishList-more a').simulate('click');
       assert.isTrue(onMoreClick.called);
     });
+
+    it('calls its `showActionsMenu` handler when the menu icon is clicked', function() {
+      const issueishes = [allGreen, mixed, allRed];
+      const showActionsMenu = sinon.stub();
+      const wrapper = mount(buildApp({
+        isLoading: false,
+        total: 3,
+        issueishes,
+        showActionsMenu,
+      }));
+
+      wrapper.find('Octicon.github-IssueishList-item--menu').at(1).simulate('click');
+      assert.isTrue(showActionsMenu.calledWith(mixed));
+    });
   });
 
   it('renders review button only if needed', function() {
