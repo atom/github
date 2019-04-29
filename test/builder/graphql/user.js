a566a465cbb7108f7dca369409cf5c3999dbafc6
Ash Wilson <smashwilson@github.com>
2019-04-29 13:24:55 -0400

    Test coverage for UserMentionTooltipContainer

--- test/builder/graphql/user.js
+++ test/builder/graphql/user.js
@@ -1,5 +1,9 @@
-import {createSpecBuilderClass} from './base';
+import {createSpecBuilderClass, createConnectionBuilderClass, createUnionBuilderClass, defer} from './base';
 import {nextID} from '../id-sequence';
+
+const RepositoryBuilder = defer('../repository', 'RepositoryBuilder');
+
+export const RepositoryConnectionBuilder = createConnectionBuilderClass('Repository', RepositoryBuilder);
 
 export const UserBuilder = createSpecBuilderClass('User', {
   __typename: {default: 'User'},
@@ -10,8 +14,35 @@ export const UserBuilder = createSpecBuilderClass('User', {
     const login = f.login || 'login';
     return `https://github.com/${login}`;
   }},
+  company: {default: 'GitHub'},
+  repositories: {linked: RepositoryConnectionBuilder},
+},
+'Node & Actor & RegistryPackageOwner & RegistryPackageSearch & ProjectOwner ' +
+  '& RepositoryOwner & UniformResourceLocatable',
+);
+
+export const OrganizationMemberConnectionBuilder = createConnectionBuilderClass('OrganizationMember', UserBuilder);
+
+export const OrganizationBuilder = createSpecBuilderClass('Organization', {
+  login: {default: 'someone'},
+  avatarUrl: {default: 'https://avatars3.githubusercontent.com/u/17565?s=32&v=4'},
+  repositories: {linked: RepositoryConnectionBuilder},
+  membersWithRole: {linked: OrganizationMemberConnectionBuilder},
+},
+'Node & Actor & RegistryPackageOwner & RegistryPackageSearch & ProjectOwner ' +
+  '& RepositoryOwner & UniformResourceLocatable & MemberStatusable',
+);
+
+export const RepositoryOwnerBuilder = createUnionBuilderClass('RepositoryOwner', {
+  beUser: UserBuilder,
+  beOrganization: OrganizationBuilder,
+  default: 'beUser',
 });
 
 export function userBuilder(...nodes) {
   return UserBuilder.onFragmentQuery(nodes);
+}
+
+export function organizationBuilder(...nodes) {
+  return OrganizationBuilder.onFragmentQuery(nodes);
 }
