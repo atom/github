c46f68a31cda8c8f6206db69554f5bc03098a55d
Ash Wilson <smashwilson@gmail.com>
2019-10-02 19:54:52 -0400

    RemoteConfigurationView tests

--- test/views/remote-configuration-view.test.js
+++ test/views/remote-configuration-view.test.js
@@ -4,8 +4,19 @@ import {TextBuffer} from 'atom';
 
 import RemoteConfigurationView from '../../lib/views/remote-configuration-view';
 import TabGroup from '../../lib/tab-group';
+import {TabbableTextEditor} from '../../lib/views/tabbable';
 
 describe('RemoteConfigurationView', function() {
+  let atomEnv;
+
+  beforeEach(function() {
+    atomEnv = global.buildAtomEnvironment();
+  });
+
+  afterEach(function() {
+    atomEnv.destroy();
+  });
+
   function buildApp(override = {}) {
     const sourceRemoteBuffer = new TextBuffer();
     return (
@@ -14,6 +25,7 @@ describe('RemoteConfigurationView', function() {
         didChangeProtocol={() => {}}
         sourceRemoteBuffer={sourceRemoteBuffer}
         tabGroup={new TabGroup()}
+        commands={atomEnv.commands}
         {...override}
       />
     );
@@ -24,16 +36,16 @@ describe('RemoteConfigurationView', function() {
     const currentProtocol = 'ssh';
 
     const wrapper = shallow(buildApp({currentProtocol, sourceRemoteBuffer}));
-    assert.strictEqual(wrapper.find('AtomTextEditor').prop('buffer'), sourceRemoteBuffer);
-    assert.isFalse(wrapper.find('.github-RemoteConfiguration-protocolOption--https input.input-radio').prop('checked'));
-    assert.isTrue(wrapper.find('.github-RemoteConfiguration-protocolOption--ssh input.input-radio').prop('checked'));
+    assert.strictEqual(wrapper.find(TabbableTextEditor).prop('buffer'), sourceRemoteBuffer);
+    assert.isFalse(wrapper.find('.github-RemoteConfiguration-protocolOption--https .input-radio').prop('checked'));
+    assert.isTrue(wrapper.find('.github-RemoteConfiguration-protocolOption--ssh .input-radio').prop('checked'));
   });
 
   it('calls a callback when the protocol is changed', function() {
     const didChangeProtocol = sinon.spy();
 
     const wrapper = shallow(buildApp({didChangeProtocol}));
-    wrapper.find('.github-RemoteConfiguration-protocolOption--ssh input.input-radio')
+    wrapper.find('.github-RemoteConfiguration-protocolOption--ssh .input-radio')
       .prop('onChange')({target: {value: 'ssh'}});
 
     assert.isTrue(didChangeProtocol.calledWith('ssh'));
