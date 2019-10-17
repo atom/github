468d1fdd1814163b48a6f64fb2a49ecc18884c5e
Ash Wilson <smashwilson@gmail.com>
2019-10-17 09:16:24 -0400

    Use JS scripts for npm test-related tasks

---
+++ script/helpers/run-atom.js
@@ -0,0 +1,54 @@
+const {spawn} = require('child_process');
+
+function atomProcess(env, ...args) {
+  const atomBinPath = process.env.ATOM_SCRIPT_PATH || 'atom';
+  const atomEnv = {...process.env, ...env};
+
+  return new Promise((resolve, reject) => {
+    let settled = false;
+
+    const child = spawn(atomBinPath, args, {
+      env: atomEnv,
+      stdio: 'inherit',
+      shell: true,
+      windowsHide: true,
+    });
+
+    child.on('error', err => {
+      if (!settled) {
+        settled = true;
+        reject(err);
+      } else {
+        // eslint-disable-next-line no-console
+        console.error(`Unexpected subprocess error:\n${err.stack}`);
+      }
+    });
+
+    child.on('exit', (code, signal) => {
+      if (!settled) {
+        settled = true;
+        resolve({code, signal});
+      }
+    });
+  });
+}
+
+async function runAtom(...args) {
+  try {
+    const {code, signal} = await atomProcess(...args);
+    if (signal) {
+      // eslint-disable-next-line no-console
+      console.log(`Atom process killed with signal ${signal}.`);
+    }
+    process.exit(code !== null ? code : 1);
+  } catch (err) {
+    // eslint-disable-next-line no-console
+    console.error(err.stack);
+    process.exit(1);
+  }
+}
+
+module.exports = {
+  atomProcess,
+  runAtom,
+};
