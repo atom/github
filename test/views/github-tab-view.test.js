9ec4e755bc41d60bddb61d0efe98066ef23bf12e
wadeuk3 <wadeuk3@gmail.com>
2019-10-25 11:48:34 -0500

    lint

--- test/views/github-tab-view.test.js
+++ test/views/github-tab-view.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {shallow, mount} from 'enzyme';
+import {shallow} from 'enzyme';
 
 import {gitHubTabViewProps} from '../fixtures/props/github-tab-props';
 import Repository from '../../lib/models/repository';
