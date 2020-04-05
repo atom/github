3e4f4096273849626154c24f7beb951a97b522f8
Ash Wilson <smashwilson@github.com>
2020-04-05 08:47:34 -0400

    "Continue" button enablement

--- test/views/git-identity-view.test.js
+++ test/views/git-identity-view.test.js
@@ -30,9 +30,25 @@ describe('GitIdentityView', function() {
     assert.strictEqual(getEditor('email address').prop('buffer'), emailBuffer);
   });
 
+  it('disables the "Continue" button if the name is blank', function() {
+    const usernameBuffer = new TextBuffer();
+    const wrapper = mount(buildApp({usernameBuffer}));
+
+    assert.isTrue(wrapper.find('.btn').prop('disabled'));
+  });
+
+  it('disables the "Continue" button if the email is blank', function() {
+    const emailBuffer = new TextBuffer();
+    const wrapper = mount(buildApp({emailBuffer}));
+
+    assert.isTrue(wrapper.find('.btn').prop('disabled'));
+  });
+
   it('triggers a callback when "Continue" is clicked', function() {
+    const usernameBuffer = new TextBuffer({text: 'Me'});
+    const emailBuffer = new TextBuffer({text: 'me@email.com'});
     const close = sinon.spy();
-    const wrapper = mount(buildApp({close}));
+    const wrapper = mount(buildApp({usernameBuffer, emailBuffer, close}));
 
     wrapper.find('.btn').simulate('click');
 
